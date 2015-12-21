38b24dcf98afb72d17c57fab019d196a374187d9
kritsanasak kuntaros <roboconk@gmail.com>
2015-12-21 13:37:22 +0700

    - เพิ่ม รายชื่อคนไข้นัดตรวจ

---
+++ surasak3/ptappoiall.php
@@ -0,0 +1,271 @@
+<?php
+    
+session_start();
+/*
+if($_SESSION["sIdname"] != "bbm"){
+	echo "อยู่ระหว่างปรับปรุง";
+	exit();
+}
+*/
+$appd=$appdate.' '.$appmo.' '.$thiyr;
+    print "<font face='Angsana New'><b>รายชื่อคนไข้นัดตรวจ</b><br>";
+  
+  print "<b>นัดมาวันที่</b> $appd ";
+   
+ print ".........<input type=button onclick='history.back()' value=' << กลับไป '>";
+?>
+
+
+<?php
+    include("connect.inc");
+    $query="CREATE TEMPORARY TABLE appoint1 SELECT *, left( `doctor` , 5 ) AS codedoctor FROM appoint WHERE appdate = '$appd' ";
+	
+    $result = mysql_query($query) or die("Query failed,app");
+
+
+  print "จำนวนผู้ป่วยนัดแต่ละแพทย์ กดเลือกแพทย์ = รายชื่อผู้ป่วย<a target=_self  href='../nindex.htm'><<ไปเมนู</a><br> ";
+   $query="SELECT  codedoctor,COUNT(*) AS duplicate FROM appoint1 where codedoctor <> 'MD007' GROUP BY codedoctor HAVING duplicate > 0 ORDER BY doctor";
+   $result = mysql_query($query);
+     $n=0;
+ while (list ($codedoctor,$duplicate) = mysql_fetch_row ($result)) {
+            $n++;
+$num= $duplicate+$num;
+
+list($doctor) = mysql_fetch_row(mysql_query("Select name From doctor where name like '{$codedoctor}%' limit 1 "));
+            print (" <tr>\n".
+               "  <td BGCOLOR=66CDAA><font face='Angsana New'>$n&nbsp;&nbsp;</td>\n".
+              "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"ptappoiall2.php? doctor=$codedoctor&appd=$appd\">$doctor&nbsp;&nbsp;</a></td>\n".
+ 
+//    "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"checkidchk.php? idcard=$idcard\">$idcard&nbsp;&nbsp;</a></td>\n".
+             //  "  <td BGCOLOR=66CDAA><font face='Angsana New'>$detail&nbsp;&nbsp;</td>\n".
+        "  <td BGCOLOR=66CDAA><font face='Angsana New'>นัดจำนวน&nbsp; = &nbsp;$duplicate &nbsp;&nbsp;คน</td>\n".
+               " </tr>\n<br>");
+               }
+
+$query="SELECT  doctor,COUNT(*) AS duplicate FROM appoint1 where codedoctor = 'MD007' GROUP BY doctor HAVING duplicate > 0 ORDER BY doctor";
+   $result = mysql_query($query);
+
+ while (list ($doctor,$duplicate) = mysql_fetch_row ($result)) {
+            $n++;
+$num= $duplicate+$num;
+
+
+            print (" <tr>\n".
+               "  <td BGCOLOR=66CDAA><font face='Angsana New'>$n&nbsp;&nbsp;</td>\n".
+              "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"ptappoiall2.php? doctor=$doctor&appd=$appd\">$doctor&nbsp;&nbsp;</a></td>\n".
+ 
+//    "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"checkidchk.php? idcard=$idcard\">$idcard&nbsp;&nbsp;</a></td>\n".
+             //  "  <td BGCOLOR=66CDAA><font face='Angsana New'>$detail&nbsp;&nbsp;</td>\n".
+        "  <td BGCOLOR=66CDAA><font face='Angsana New'>นัดจำนวน&nbsp; = &nbsp;$duplicate &nbsp;&nbsp;คน</td>\n".
+               " </tr>\n<br>");
+               }
+ print "จำนวนผู้ป่วยทั้งหมด.... $num..คน</a><br> ";
+   include("unconnect.inc");
+?>
+
+
+<?php
+    include("connect.inc");
+    $query="CREATE TEMPORARY TABLE appoint1 SELECT * FROM appoint WHERE appdate = '$appd' ";
+    $result = mysql_query($query) or die("Query failed,app");
+
+
+  print "จำนวนผู้ป่วยนัด<a target=_self  href='../nindex.htm'><<ไปเมนู</a><br> ";
+   $query="SELECT  detail,COUNT(*) AS duplicate FROM appoint1 GROUP BY detail HAVING duplicate > 0 ORDER BY doctor";
+   $result = mysql_query($query);
+     $n=0;
+	 $num=0;
+ while (list ($detail,$duplicate) = mysql_fetch_row ($result)) {
+            $n++;
+
+$num= $duplicate+$num;
+            print (" <tr>\n".
+               "  <td BGCOLOR=66CDAA><font face='Angsana New'>$n&nbsp;&nbsp;</td>\n".
+              "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"ptappoiall3.php? detail=$detail&appd=$appd\">$detail&nbsp;&nbsp;</a></td>\n".
+ 
+//    "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK href=\"checkidchk.php? idcard=$idcard\">$idcard&nbsp;&nbsp;</a></td>\n".
+             //  "  <td BGCOLOR=66CDAA><font face='Angsana New'>$detail&nbsp;&nbsp;</td>\n".
+        "  <td BGCOLOR=66CDAA><font face='Angsana New'>นัดจำนวน&nbsp; = &nbsp;$duplicate &nbsp;&nbsp;คน</td>\n".
+               " </tr>\n<br>");
+               }
+ print "จำนวนผู้ป่วยทั้งหมด.... $num..คน</a><br> ";
+   include("unconnect.inc");
+?>
+
+
+<table>
+ <tr>
+  <th bgcolor=6495ED>#</th>
+ 
+ <th bgcolor=6495ED>เวลา</th>
+ 
+ <th bgcolor=6495ED>HN</th>
+ 
+ <th bgcolor=6495ED><font face='Angsana New'>ชื่อ</th>
+
+ <th bgcolor=6495ED><font face='Angsana New'>แพทย์</th>
+ 
+ <th bgcolor=6495ED><font face='Angsana New'>แผนก</th>
+   
+ <th bgcolor=6495ED><font face='Angsana New'>เจ้าหน้าที่</th>
+ 
+ <th bgcolor=6495ED>มา?</th>
+<th bgcolor=6495ED>วันเวลาออกใบนัด</th>
+
+ <th bgcolor=6495ED>สิทธิหลัก</th>
+ <th bgcolor=6495ED>สิทธิรอง</th>
+  <th bgcolor=6495ED>มีสิทธิ</th>
+ 
+ </tr>
+
+<?php
+    include("connect.inc");
+    $query = "SELECT hn,ptname,apptime,came,row_id,age,doctor,depcode,officer,date FROM appoint WHERE appdate = '$appd' ORDER BY row_id ASC    ";
+  
+ $result = mysql_query($query)
+        or die("Query failed");
+  
+  $num=0;
+    while (list ($hn,$ptname,$apptime,$came,$row_id,$age,$doctor,$depcode,$officer,$date) = mysql_fetch_row ($result)) 
+{
+     
+	$sql = "Select ptright,ptright1,idcard From opcard where hn = '$hn'  limit 1 ";
+	$result1 = Mysql_Query($sql);
+	list($ptright,$ptright1,$idcard) = Mysql_fetch_row($result1);
+
+
+
+if(substr($ptright1,0,3)=='R07'){
+		$sql = "Select id From ssodata where id LIKE '$idcard%' limit 1 ";
+
+		if(Mysql_num_rows(Mysql_Query($sql)) > 0){
+			$color = "#66CDAA";
+		
+		}else{
+			$color = "#FF0000";
+		
+		}
+}else if(substr($ptright1,0,3)=='R03'){
+		$sql = "Select hn, status From cscddata where hn = '$hn' AND ( status like '%U%' OR status = '\r' OR status like '%V%' )  limit 1 ";
+
+		if(Mysql_num_rows(Mysql_Query($sql)) > 0){
+			$color = "#66CDAA";
+		
+		}else{
+			$color = "#FF0000";
+		
+		}
+}
+
+/*
+
+else  if(substr($ptright1,0,3)=='R33'){
+		$sql = "Select flag From optdata where hn = '$hn'  order by row_id desc limit 1 ";
+
+	$result = Mysql_Query($sql);
+	list($flag) = Mysql_fetch_row($result);
+	
+					if($flag==""){
+									$color = "#66CDAA";
+		
+										}else{
+									$color = "#FF0000";
+		
+										}
+										
+}
+
+
+*/
+
+
+else if(substr($ptright1,0,3)=='R33'){
+		$sql = "Select hn,flag From optdata where hn = '$hn' AND  flag !='E' limit 1 ";
+		if(Mysql_num_rows(Mysql_Query($sql)) > 0){
+			$color = "#66CDAA";
+		
+		}else{
+			$color = "#FF0000";
+		
+		}
+}
+else{
+
+
+		$color = "#66CDAA";
+}
+
+
+
+
+
+if(!empty($idcard)){
+	$sql2 = "Select id From ssodata where id LIKE '$idcard%' limit 1 ";
+	if(Mysql_num_rows(Mysql_Query($sql2)) > 0){
+		$ptright2='R07 ประกันสังคม';
+	}else{
+		$ptright2='';
+	}
+}else{
+	$ptright2='ไม่มีเลขบัตร';
+}
+
+
+if(!empty($hn)){
+	$sql3 = "Select hn, status From cscddata where hn = '$hn' AND ( status like '%U%' OR status = '\r' OR status like '%V%')  limit 1 ";			
+	if(Mysql_num_rows(Mysql_Query($sql3)) > 0){
+		$ptright3='R03 โครงการเบิกจ่ายตรง';
+	}else{
+		$ptright3='';
+	}
+}else{
+	$ptright3='ไม่มีHN';
+}
+
+
+$num++;
+   
+$ptrightall=$ptright2.$ptright3;
+//$ptrightall2=substr($ptright2.$ptright3,0,3);
+	if(substr($ptright1,0,3)==substr($ptright2.$ptright3,0,3)){$color2="#66CDAA";}else{$color2="#FF0099";};
+
+	if(substr($ptright1,0,3)==substr($ptright2.$ptright3,0,3)&&substr($ptright1,0,3)==substr($ptright,0,3)){$color2="#66CDAA";}else{$color2="#FF0099";};
+	
+     print (" <tr>\n".
+         
+		 "  <td BGCOLOR=$color><font face='Angsana New'>$num</td>\n".
+        
+   "  <td BGCOLOR=$color><font face='Angsana New'>$apptime</td>\n".
+     
+      "  <td BGCOLOR=$color><font face='Angsana New'><a href=\"printpt.php? cHn=$hn&cPtname=$ptname&cIdcard=$idcard&cPtright1=$ptright1\" target=\"_blank\"  >$hn</a></td>\n".
+      
+     "  <td BGCOLOR=$color><font face='Angsana New'>$ptname</td>\n".
+     
+   "  <td BGCOLOR=$color><font face='Angsana New'>$doctor</td>\n".
+	
+  "  <td BGCOLOR=$color><font face='Angsana New'>$depcode</td>\n".
+			
+  "  <td BGCOLOR=$color><font face='Angsana New'>$officer</td>\n".
+      
+     "  <td BGCOLOR=$color><font face='Angsana New'>$came</td>\n".
+
+	  "  <td BGCOLOR=$color><font face='Angsana New'>$date</td>\n".
+	     "  <td BGCOLOR=$color2><font face='Angsana New'>$ptright1</td>\n".
+		  "  <td BGCOLOR=$color2><font face='Angsana New'>$ptright</td>\n".
+		   "  <td BGCOLOR=$color2><font face='Angsana New'>$ptrightall</td>\n".
+
+
+
+  // "  <td BGCOLOR=66CDAA><font face='Angsana New'><a target=_BLANK  href=\"apprxfrm.php? cPtname=$ptname&cHn=$hn&cAge=$age&nRow=$row_id\">พิมพ์</a></td>\n".
+           " </tr>\n");
+  
+     }
+  
+  include("unconnect.inc");
+?>
+
+</table>
+
+<div ><a href="ptappoiall_chk.php?appd=<?=$appd;?>" target="_blank">พิมพ์ใบตรวจสอบสิทธิ์</a></div>
+
+
---
+++ surasak3/ptappoiall2.php
@@ -0,0 +1,245 @@
+<?php
+session_start();
+set_time_limit(5);
+include("connect.inc");
+  $Thaidate=date("d-m-").(date("Y")+543)."  ".date("H:i:s");
+    print "<font face='Angsana New'><b>รายชื่อคนไข้นัดตรวจ</b><br>";
+	
+	if(strlen($doctor) == 5){
+		$sql = "Select name From doctor where name like '".$doctor."%' limit 1";
+		list($dc) = mysql_fetch_row(mysql_query($sql));
+		print "<b>แพทย์:</b> $dc <br>"; 
+	}else{
+		print "<b>แพทย์:</b> $doctor <br>"; 
+	}
+   print "<b>นัดมาวันที่</b> $appd<br> ";
+   print "วัน/เวลาทำการตรวจสอบ....$Thaidate"; 
+
+
+
+?>
+<style type="text/css">
+<!--
+@media print{
+#no_print{display:none;}
+}
+
+.theBlocktoPrint 
+{ 
+background-color: #000; 
+color: #FFF; 
+} 
+-->
+</style>
+<br />
+<div id="no_print" >
+<A HREF="ptapsort.php?doctor=<?php echo $_GET["doctor"];?>&appd=<?php echo $_GET["appd"];?>" target="_blank">ผู้ป่วยไตเทียม</A> 
+&nbsp;&nbsp;<A HREF="ptapsort1.php?doctor=<?php echo $_GET["doctor"];?>&appd=<?php echo $_GET["appd"];?>" target="_blank">ผู้ป่วยOPD</A>&nbsp;&nbsp;<a href="ptapsort3.php?doctor=<?php echo $_GET["doctor"];?>&appd=<?php echo $_GET["appd"];?>" target="_blank">คลีนิกฝังเข็ม</a>&nbsp;&nbsp;<a href="vnprintday.php?nat=<?=$_GET["appd"];?>&detail=<?=$dc;?>&doctor">พิมพ์ใบตรวจโรค</a>&nbsp;&nbsp;<a href="opdcard_vnprintday.php?nat=<?=$_GET["appd"];?>&amp;detail=<?=$dc;?>&doctor">พิมพ์ใบต่อรายวัน</a></div>
+<table>
+ <tr>
+  <th bgcolor=6495ED>#</th>
+
+  <th bgcolor=6495ED width='80'>HN</th>
+  <th bgcolor=6495ED><font face='Angsana New'>ชื่อ</font></th>
+  <th bgcolor=6495ED><font face='Angsana New'><A HREF="<?php echo $_SERVER["PHP_SELF"];?>?doctor=<?php echo $_GET["doctor"];?>&appd=<?php echo $_GET["appd"];?>&sortby=time">เวลานัด</A></font></th>
+  <th bgcolor=6495ED><font face='Angsana New'>นัดเพื่อ</font></th>
+  <? // if($doctor=="MD041"){?>
+  <th bgcolor=6495ED>อื่นๆ</th>
+<? // } ?>
+  <th bgcolor=6495ED>diag</th>
+  <th bgcolor=6495ED>ซ้ำ</th>
+  <th bgcolor=6495ED>ยื่นบัตร</th>
+  <th bgcolor=6495ED>Admit</th>
+ 
+  <!-- <th bgcolor=6495ED>วันที่นัด</th> -->
+  </tr>
+
+<?php
+
+	$sql = "Select menucode From inputm where idname = '".$_SESSION["sIdname"]."' limit 1 ";
+	//echo $sql."<br>";
+	$result = Mysql_Query($sql);
+	list($menucode) = Mysql_fetch_row($result);
+	
+		if(strlen($doctor) == 5){
+			$doctor2 = " doctor like '".$doctor."%' ";
+			$doctor3 = "AND left(doctor,5) <> '".$doctor."' ";
+
+		}else{
+			$doctor2 = " doctor = '".$doctor."' ";
+			$doctor3 = " AND doctor <> '".$doctor."' ";
+		}
+
+		$query = "SELECT count( hn ) , hn, doctor   FROM `appoint` WHERE appdate = '$appd' ".$doctor3." GROUP BY hn HAVING count( hn ) >= 1 ";
+		//echo $query."<br>";
+		$result = mysql_query($query);
+		
+		while($arr = Mysql_fetch_assoc($result)){
+			$name_dc = substr($arr["doctor"],5);
+			if(substr($arr["doctor"],0,5) != "MD007"){
+				$arr["doctor"] = substr($arr["doctor"],0,5);
+			}			
+			
+			$listhn[$arr["hn"]] .= "<A HREF=\"ptappoiall2.php?doctor=".urlencode($arr["doctor"])."&appd=".urlencode($appd)."\" target='_blank'>".$name_dc."</A> &nbsp; ";
+		}
+		
+	
+
+/*	if(isset($_GET["sortby"]) && $_GET["sortby"] != ""){
+		$sort = " apptime ASC ,detail asc";
+	}else{
+		$sort = " detail asc  ASC";
+}*/
+	
+	if(strlen($doctor) == 5)
+		$doctor2 = "doctor like '".$doctor."%' ";
+	else
+		$doctor2 = "doctor = '".$doctor."' ";
+	if($_GET["sortby"]=="time"){
+	 $query1 = "SELECT hn,ptname,apptime,detail,came,row_id,age,date_format(date,'%d-%m-%Y'),officer, left(apptime,5),diag ,other,room FROM appoint WHERE appdate = '$appd' and (".$doctor2.")   order by apptime asc";
+	}else{
+    $query1 = "SELECT hn,ptname,apptime,detail,came,row_id,age,date_format(date,'%d-%m-%Y'),officer, left(apptime,5),diag ,other,room FROM appoint WHERE appdate = '$appd' and (".$doctor2.")   order by detail desc";
+	}
+	//echo $query1."<br>";
+    $result = mysql_query($query1)or die("Query failed");
+	
+    $num=0;
+	$j[0]=0;
+	$j[1]=0;
+	
+	$title_array = array();
+	$title_array2 = array();
+	$detail_array = array();
+
+	$date_now = date("d-m-").(date("Y")+543);
+    while (list ($hn,$ptname,$apptime,$detail,$came,$row_id,$age,$date,$officer,$left5,$diag,$other,$room) = mysql_fetch_row ($result)) {
+
+        $num++;
+		$left5 = str_replace(".",":",$left5);
+		if($left5 >= "07:00" && $left5 <= "14:00"){
+			$x=0;
+		}else{
+			$x=1;
+			
+		}
+		if($date_now == $date){
+			$bgcolor = "FFA8A8";
+		}else{
+			$bgcolor = "66CDAA";
+		}
+		
+		if($menucode == 'ADMOPD'){
+			$detail = substr($detail,4);
+		}
+		
+		list($firstyear,$count_number) = explode("-",$hn);
+		$title_array[$x][$j[$x]] = $firstyear;
+		$title_array[$x][$j[$x]] = $title_array[$x][$j[$x]]*1;
+		$title_array2[$x][$j[$x]] = $count_number;
+		$title_array2[$x][$j[$x]] = $title_array2[$x][$j[$x]]*1;
+
+        $detail_array[$x][$j[$x]] = " <tr>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>{#ii}</td>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:20px;'><font face='Angsana New'><a href='opdcard_vnprintday.php?act=show1&hn=$hn&nat=$_GET[appd]&amp;detail=$dc&amp;doctor' target='_blank'>$hn</a></td>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:20px;'><font face='Angsana New'>$ptname</td>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>$apptime</td>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>$detail</td>\n".
+			"  <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>$other</td>\n".
+			 "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>$diag</td>\n";
+			//    "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>ค้นพบ////ไม่พบ</td>\n"
+			
+	if(isset($listhn[$hn])){
+		 $detail_array[$x][$j[$x]] .= " <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>".$listhn[$hn]."</td>\n";
+	 }else if(empty($listhn[$hn])){
+		 $detail_array[$x][$j[$x]] .= " <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>&nbsp;</td>\n";
+	 }
+	 if($room=="แผนกทะเบียน"){
+		$detail_array[$x][$j[$x]] .= "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>$room</td>\n";
+	}else{
+		$detail_array[$x][$j[$x]] .= "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>&nbsp;</td>\n";
+	}
+	// print " <td BGCOLOR=$bgcolor style='font-size:18px;'><font face='Angsana New'>$date</td>\n";
+		//$sql5 = "select * from ipcard where hn='$hn' and dcdate = '0000-00-00 00:00:00' and days is null and dcnumber ='' and ptname is not null ";
+	$sql5 = "select * from bed where hn='$hn' ";
+	//echo $sql5;
+	$row5 = mysql_query($sql5);
+	$rep5 = mysql_num_rows($row5);
+	if($rep5>0){
+		$detail_array[$x][$j[$x]] .= "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>Admit</td>\n";
+	}else{
+		$detail_array[$x][$j[$x]] .= "  <td BGCOLOR=66CDAA style='font-size:18px;'><font face='Angsana New'>&nbsp;</td>\n";
+	}
+	
+           $detail_array[$x][$j[$x]] .= " </tr>\n";
+
+		   $j[$x]++;
+}
+
+$x=0;
+
+for($one=1;$one<$j[$x];$one++){
+
+	for($two=$one;$two>0;$two--){
+		
+		if(($title_array[$x][$two] < $title_array[$x][$two-1]) ||  ($title_array[$x][$two] == $title_array[$x][$two-1] &&  $title_array2[$x][$two] < $title_array2[$x][$two-1])){
+
+			$xxx = $title_array[$x][$two];
+			$title_array[$x][$two] = $title_array[$x][$two-1];
+			$title_array[$x][$two-1] = $xxx;
+
+			$xxx = $title_array2[$x][$two];
+			$title_array2[$x][$two] = $title_array2[$x][$two-1];
+			$title_array2[$x][$two-1] = $xxx;
+
+			$xxx = $detail_array[$x][$two];
+			$detail_array[$x][$two] = $detail_array[$x][$two-1];
+			$detail_array[$x][$two-1] = $xxx;
+
+		}
+
+	}
+}
+
+$x=1;
+for($one=1;$one<$j[$x];$one++){
+
+	for($two=$one;$two>0;$two--){
+		
+		if(($title_array[$x][$two] < $title_array[$x][$two-1]) ||  ($title_array[$x][$two] == $title_array[$x][$two-1] &&  $title_array2[$x][$two] < $title_array2[$x][$two-1])){
+
+			$xxx = $title_array[$x][$two];
+			$title_array[$x][$two] = $title_array[$x][$two-1];
+			$title_array[$x][$two-1] = $xxx;
+
+			$xxx = $title_array2[$x][$two];
+			$title_array2[$x][$two] = $title_array2[$x][$two-1];
+			$title_array2[$x][$two-1] = $xxx;
+
+			$xxx = $detail_array[$x][$two];
+			$detail_array[$x][$two] = $detail_array[$x][$two-1];
+			$detail_array[$x][$two-1] = $xxx;
+
+		}
+
+	}
+}
+
+$x=0;
+$y=0;
+for($i=0;$i<$j[$x];$i++){
+	
+	$detail_array[$x][$i] = str_replace("{#ii}",$i+1,$detail_array[$x][$i]);
+	echo $detail_array[$x][$i];
+$y++;
+}
+
+$x=1;
+for($i=0;$i<$j[$x];$i++){
+	
+	$detail_array[$x][$i] = str_replace("{#ii}",$i+1+$y,$detail_array[$x][$i]);
+	echo "",$detail_array[$x][$i];
+
+}
+    include("unconnect.inc");
+?>
+</table>
