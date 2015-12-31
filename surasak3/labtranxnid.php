9077cf6184bb5850a1896469fb30dd40d9aaf34d
kritsanasak kuntaros <roboconk@gmail.com>
2015-12-31 11:17:53 +0700

    - แก้ไข ปรับรูปแบบใบรับรองแพทย์ โดยเพิ่ม วันที่ ในใบรับรองแบบย่อ และ เพิ่ม ตำแหน่งแพทย์แผนจีนด้านล่างชื่อ

--- surasak3/labtranxnid.php
+++ surasak3/labtranxnid.php
@@ -51,21 +51,21 @@ $today = date("Y-m-d");
    $query = "INSERT INTO depart(chktranx,date,ptname,hn,an,doctor,depart,item,detail,price,sumyprice,sumnprice,paid, idname,diag,accno,tvn,ptright,lab)VALUES('$nRunno','$Thidate','$cPtname','$cHn','$cAn','$cDoctor','$cDepart','$item','$aDetail',
                     '$Netprice','$aSumYprice','$aSumNprice','','$sOfficer','$cDiag','$cAccno','$tvn','$cPtright','$nLab');";
 
-       $result = mysql_query($query) or 
-                die("**เตือน ! เมื่อพบหน้าต่างนี้แสดงว่าได้บันทึกข้อมูลไปก่อนแล้ว หรือการบันทึกล้มเหลว<br>
-	*โปรดตรวจสอบว่ามีรายการในเมนู [ดูการจ่ายเงิน] หรือไม่<br>
-	*ถ้ามีแสดงว่า ได้บันทึกไปก่อนแล้ว<br>
-	*ถ้าไม่มีแสดงว่า  การบันทึกล้มเหลว<br><br>
-                -------- รายการ ---------<br> 
-	$Thaidate<br>
-	$cPtname HN:$cHn AN:$cAn VN:$tvn<br>
-                สิทธิ: $cPtright<br>
-                โรค:$cDiag<br>
-                แพทย์:$cDoctor<br>
-                $aDetail<br>
-               จำนวน $item รายการ<br>
-               ราคารวม $Netprice บาท<br>
-               จนท. $sOfficer<br>");
+    //    $result = mysql_query($query) or 
+    //             die("**เตือน ! เมื่อพบหน้าต่างนี้แสดงว่าได้บันทึกข้อมูลไปก่อนแล้ว หรือการบันทึกล้มเหลว<br>
+	// *โปรดตรวจสอบว่ามีรายการในเมนู [ดูการจ่ายเงิน] หรือไม่<br>
+	// *ถ้ามีแสดงว่า ได้บันทึกไปก่อนแล้ว<br>
+	// *ถ้าไม่มีแสดงว่า  การบันทึกล้มเหลว<br><br>
+    //             -------- รายการ ---------<br> 
+	// $Thaidate<br>
+	// $cPtname HN:$cHn AN:$cAn VN:$tvn<br>
+    //             สิทธิ: $cPtright<br>
+    //             โรค:$cDiag<br>
+    //             แพทย์:$cDoctor<br>
+    //             $aDetail<br>
+    //            จำนวน $item รายการ<br>
+    //            ราคารวม $Netprice บาท<br>
+    //            จนท. $sOfficer<br>");
 
 //test 9/4/47 to find the last row
 //printf ("Last inserted record has id %d\n",mysql_insert_id());
@@ -79,7 +79,7 @@ $today = date("Y-m-d");
                 $query = "INSERT INTO patdata(date,hn,an,ptname,doctor,item,code,detail,amount,price,yprice,nprice,depart,part,idno,ptright)
                                  VALUES('$Thidate','$cHn','$cAn','$cPtname','$cDoctor','$item','$aDgcode[$n]','$aTrade[$n]','$aAmount[$n]',
                                  '$aMoney[$n]','$aYprice[$n]','$aNprice[$n]','$cDepart','$aPart[$n]','$idno','$cPtright');";
-                $result = mysql_query($query) or die("Query failed,cannot insert into patdata");
+                // $result = mysql_query($query) or die("Query failed,cannot insert into patdata");
         }
         }
 
@@ -215,16 +215,35 @@ if( $cDoctor2 === 'MD115' ){
     $position = 'แพทย์';
 }
 
-	  print "<font face='Angsana New' size ='4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<CENTER><B>ใบรับรองการตรวจร่างกายของแพทย์</B> โรงพยาบาลค่ายสุรศักดิ์มนตรี ลำปาง<BR></CENTER></font>"; 
-	  print "ข้าพเจ้า <B>$yot$cDoctor1</B> ตำแหน่ง "; 
-      print $position;
-      print "ประจำโรงพยาบาลค่ายสุรศักดิ์มนตรี<BR> ";
-	  print "ใบอนุญาตประกอบอาชีพเวชกรรมเลขที่ &nbsp;&nbsp;&nbsp;$doctorcode<BR>"; 
-	  print "ได้ทำการตรวจร่างกาย &nbsp;<B>$cPtname</B> &nbsp;HN:$cHn  &nbsp;&nbsp;เป็นโรค:&nbsp;&nbsp;$cDiag<BR>"; 
-	  print "เห็นสมควรให้บริการรักษาด้วยการฝังเข็ม&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;ครั้ง&nbsp;&nbsp;ตั้งแต่เวลา........................ถึง........................น.<BR>"; 
-	  print "<CENTER>&nbsp;$yot&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แพทย์ผู้ตรวจ<BR></CENTER>";
-	  print "<CENTER>($cDoctor1)</CENTER>"; 
+print "<font face='Angsana New' size ='4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<CENTER><B>ใบรับรองการตรวจร่างกายของแพทย์</B> โรงพยาบาลค่ายสุรศักดิ์มนตรี ลำปาง<BR></CENTER></font>"; 
+if( $cDoctor2 === 'MD115' ){
+    list($nid_date, $nid_time) = explode(' ', $Thaidate);
+    print "<CENTER>วันที่ <b>$nid_date</b></CENTER>"; 
+}
 
+print "ข้าพเจ้า <B>$yot$cDoctor1</B> ตำแหน่ง "; 
+print $position;
+print "ประจำโรงพยาบาลค่ายสุรศักดิ์มนตรี<BR> ";
+print "ใบอนุญาตประกอบอาชีพเวชกรรมเลขที่ &nbsp;$doctorcode<BR>"; 
+print "ได้ทำการตรวจร่างกาย &nbsp;<B>$cPtname</B> &nbsp;HN:$cHn  &nbsp;&nbsp;เป็นโรค:&nbsp;&nbsp;$cDiag<BR>"; 
+print "เห็นสมควรให้บริการรักษาด้วยการฝังเข็ม&nbsp;";
+$diag_list = array('อัมพฤกษ์','อัมพาต','CVA');
+if( $cDoctor2 === 'MD115' ){
+    if( in_array($cDiag, $diag_list) === true ){
+        print 'เพื่อ ฟื้นฟูสมรรถภาพ';
+    }else{
+        print 'เพื่อ การรักษา';
+    }
+}else{
+    print "&nbsp;&nbsp;1&nbsp;&nbsp;ครั้ง&nbsp;&nbsp;ตั้งแต่เวลา........................ถึง........................น."; 
+}
+
+print "<br>";
+print "<CENTER>&nbsp;$yot&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แพทย์ผู้ตรวจ<BR></CENTER>";
+print "<CENTER>($cDoctor1)</CENTER>"; 
+if( $cDoctor2 === 'MD115' ){
+    print "<CENTER>$position</CENTER>"; 
+}
 
 	    // print "<B>นำใบแจ้งหนี้ไปชำระเงินที่ห้องเก็บเงิน</B>";  
 //จบใบแจ้งหนี้
