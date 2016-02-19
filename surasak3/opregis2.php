eb1ea426fa8d8b68739a462a1ed99408bca7f1c0
kritsanasak kuntaros <roboconk@gmail.com>
2016-02-19 14:38:40 +0700

    - แก้ไข บล๊อกการลงทะเบียนในกรณียังไม่ได้ DC - แก้ไข ใบ Sticker ใหม่

--- surasak3/opregis2.php
+++ surasak3/opregis2.php
@@ -1,69 +1,73 @@
 <?php
-   session_start();
-    session_unregister("cHn");  
-    session_unregister("cPtname");
-    session_unregister("cPtright");
-    session_unregister("nVn");  
-    session_unregister("cAge");  
-    session_unregister("nRunno");  
-    session_unregister("vAN");
-    session_unregister("thdatehn");  
-    session_unregister("cNote");  
-	 session_unregister("Ptright1");
+session_start();
+session_unregister("cHn");  
+session_unregister("cPtname");
+session_unregister("cPtright");
+session_unregister("nVn");  
+session_unregister("cAge");  
+session_unregister("nRunno");  
+session_unregister("vAN");
+session_unregister("thdatehn");  
+session_unregister("cNote");  
+session_unregister("Ptright1");
 //    session_destroy();
 ?>
-  <p>ระบบลงทะเบียนผู้ป่วย</p>
+
+<script type="text/javascript" src="templates/classic/main.js"></script>
+<script type="text/javascript" src="assets/js/json2.js"></script>
+
+<p>ระบบลงทะเบียนผู้ป่วย</p>
 <script type="text/javascript">
-document.getElementById('hn_value').focus();
+	document.getElementById('hn_value').focus();
 </script>
 <FORM Name="f1" METHOD=POST ACTION="" Onsubmit = "return checkForm();">
 	<Table border="0" align="center">
-    <TR><TD align="right">ID&nbsp;:&nbsp;</TD><TD>
-	<input type="textbox" name="id_value" maxlength="13" onkeypress="return Numberonly(event)" onclick="readonlyall(document.f1.id_value,'');  document.f1.id_value.focus(); document.f1.type[2].checked = true;">
-	</TD></TR>
-	<TR>
+		<TR><TD align="right">ID&nbsp;:&nbsp;</TD><TD>
+		<input type="textbox" name="id_value" maxlength="13" onkeypress="return Numberonly(event)" onclick="readonlyall(document.f1.id_value,'');  document.f1.id_value.focus(); document.f1.type[2].checked = true;">
+		</TD></TR>
+		<TR>
 		<TD align="right">HN&nbsp;:&nbsp;</TD><TD>
-	<input type="textbox" name="hn_value" onkeypress="return Hnonly(event)" onclick="readonlyall(document.f1.hn_value,''); document.f1.hn_value.focus(); document.f1.type[0].checked = true;"> 
+		<input type="textbox" name="hn_value" onkeypress="return Hnonly(event)" onclick="readonlyall(document.f1.hn_value,''); document.f1.hn_value.focus(); document.f1.type[0].checked = true;"> 
 		</TD>
-
-	</TR>
-<TR>
-	<TD align="right">ชื่อ&nbsp;:&nbsp;</TD><TD>
-	<input type="textbox" name="firstname" onclick="readonlyall(document.f1.firstname,document.f1.lastname); document.f1.firstname.focus();document.f1.type[1].checked = true;">
-	&nbsp;&nbsp;
-	สกุล&nbsp;:&nbsp;
-	<input type="textbox" name="lastname" onclick="readonlyall(document.f1.firstname,document.f1.lastname); document.f1.type[1].checked = true;" >
-	</TD>
-</TR>
-
-	<TR><TD align="center"  colspan="3">
-	<INPUT TYPE="submit" name="submit" value="ตกลง">&nbsp;&nbsp;<INPUT TYPE="reset" value="ยกเลิก">
-	</TD></TR>
-</Table>
+		
+		</TR>
+		<TR>
+		<TD align="right">ชื่อ&nbsp;:&nbsp;</TD><TD>
+		<input type="textbox" name="firstname" onclick="readonlyall(document.f1.firstname,document.f1.lastname); document.f1.firstname.focus();document.f1.type[1].checked = true;">
+		&nbsp;&nbsp;
+		สกุล&nbsp;:&nbsp;
+		<input type="textbox" name="lastname" onclick="readonlyall(document.f1.firstname,document.f1.lastname); document.f1.type[1].checked = true;" >
+		</TD>
+		</TR>
+		
+		<TR><TD align="center"  colspan="3">
+		<INPUT TYPE="submit" name="submit" value="ตกลง">&nbsp;&nbsp;<INPUT TYPE="reset" value="ยกเลิก">
+		</TD></TR>
+	</Table>
 </FORM>
 
 
 <table>
- <tr>
-  <th height="22" bgcolor=6495ED>HN</th>
-  <th bgcolor=6495ED width="70">ยศ</th>
-  <th bgcolor=6495ED>ชื่อ</th>
-   
-  <th bgcolor=6495ED>สกุล</th>
-  <th bgcolor=6495ED>ใบต่อแบบใหม่</th>
-<th bgcolor=6495ED>มา รพ.</th>
-  <th bgcolor=6495ED>ตรวจนอน</th>
- <th bgcolor=6495ED>ใบต่อ</th>
-<!-- <th bgcolor=6495ED>ใบยานอก</th>
- <th bgcolor=6495ED>ใบสั่งยา</th>
- <th bgcolor=6495ED>ใบตรวจโรค</th>-->
-  <th bgcolor=6495ED colspan="4">ใบตรวจโรค</th>
-  <th bgcolor=6495ED >สถานะ ward</th>
-  </tr>
+	<tr bgcolor="6495ED">
+		<th height="22">HN</th>
+		<th width="70">ยศ</th>
+		<th>ชื่อ</th>
+		
+		<th>สกุล</th>
+		<th>ใบต่อแบบใหม่</th>
+		<th>มา รพ.</th>
+		<th>ตรวจนอน</th>
+		<th>ใบต่อ</th>
+		<!-- <th>ใบยานอก</th>
+		<th>ใบสั่งยา</th>
+		<th>ใบตรวจโรค</th>-->
+		<th colspan="4">ใบตรวจโรค</th>
+		<th>สถานะ ward</th>
+	</tr>
 
 <?php
- if(!empty($hn_value)||!empty($firstname)||!empty($lastname)||!empty($id_value)){
-    include("connect.inc");
+if(!empty($hn_value)||!empty($firstname)||!empty($lastname)||!empty($id_value)){
+	include("connect.inc");
 	if($_POST["hn_value"] != ""){
 		$where = " Where hn = '".$_POST["hn_value"]."' ";
 	}else if($_POST["firstname"] != ""||$_POST["lastname"]!=""){
@@ -75,111 +79,77 @@ document.getElementById('hn_value').focus();
 	$result_chkname = mysql_query($sql_chkname);
 	while($dbarr= mysql_fetch_array($result_chkname)){
 	
-    global $hn;
+	global $hn;
 	$hn=$dbarr['hn'];
-    $query = "SELECT hn,yot,name,surname,ptright,ptright1,idcard FROM opcard WHERE hn = '$hn'";
-    $result = mysql_query($query)or die("Query failed");
-
+	$query = "SELECT hn,yot,name,surname,ptright,ptright1,idcard FROM opcard WHERE hn = '$hn'";
+	$result = mysql_query($query)or die("Query failed");
 	
-	
-    while (list ($hn,$yot,$name,$surname,$ptright,$ptright1,$idcard) = mysql_fetch_row ($result)) {
+		while (list ($hn,$yot,$name,$surname,$ptright,$ptright1,$idcard) = mysql_fetch_row ($result)) {
 		
-		$alert_msg = '-';
-
-		if(substr($ptright,0,3)=='R07' && !empty($idcard)){
-			$sql = "Select id From ssodata where id LIKE '$idcard%' limit 1 ";
-
-			if(Mysql_num_rows(Mysql_Query($sql)) > 0){
-				$color = "#CCFF00";
+			$alert_msg = '-';
+			
+			if(substr($ptright,0,3)=='R07' && !empty($idcard)){
+				$sql = "Select id From ssodata where id LIKE '$idcard%' limit 1 ";
+				
+				if(Mysql_num_rows(Mysql_Query($sql)) > 0){
+					$color = "#CCFF00";
+				}else{
+					$color = "FF8C8C";
+				}
+			}else if(substr($ptright,0,3)=='R03'){
+				$sql = "Select hn, status From cscddata where hn = '$hn' AND ( status like '%U%' OR status = '\r' OR status like '%V%')  limit 1 ";
+				
+				if(Mysql_num_rows(Mysql_Query($sql)) > 0){
+					$color = "99CC00";
+				}else{
+					$color = "FF8C8C";
+				}
 			}else{
-				$color = "FF8C8C";
+				$color = "66CDAA";
 			}
-		}else if(substr($ptright,0,3)=='R03'){
-			$sql = "Select hn, status From cscddata where hn = '$hn' AND ( status like '%U%' OR status = '\r' OR status like '%V%')  limit 1 ";
-
-			if(Mysql_num_rows(Mysql_Query($sql)) > 0){
-				$color = "99CC00";
-			}else{
-				$color = "FF8C8C";
-			}
-		}else{
-			$color = "66CDAA";
-		}
-
-
-	/*if(!empty($idcard)){
-$sql = "Select id From ssodata where id LIKE '$idcard%' limit 1 ";
-			if(Mysql_num_rows(Mysql_Query($sql)) > 0){
-				echo"<FONT SIZE='' COLOR='#FF0033'>ผู้ป่วยมีสิทธิประกันสังคม</FONT>";
-			}else{
-				echo"";
-			}
-	}else{
-			echo"<FONT SIZE='' COLOR='#FF0033'>ผู้ป่วยไม่มีเลขประจำตัวประชาชน</FONT>";
-		}
-*/
-
-/*if(!empty($hn)){
-$sql = "Select hn, status From cscddata where hn = '$hn' AND ( status like '%U%' OR status = '\r' OR status like '%V%')  limit 1 ";			
-if(Mysql_num_rows(Mysql_Query($sql)) > 0){
-				echo"<FONT SIZE='' COLOR='#FF0033'>ผู้ป่วยมีสิทธิจ่ายตรง</FONT>";
-			}else{
-				echo"";
-			}
-	}else{
-			echo"<FONT SIZE='' COLOR='#FF0033'>ผู้ป่วยไม่มี HN</FONT>";
-		}*/
-		
-		$sql = sprintf("SELECT `hn`,`dcdate`, `my_ward`
-		FROM `ipcard` 
-		WHERE `hn` = '%s' 
-		ORDER BY `row_id` DESC LIMIT 1", $hn);
-		$query = mysql_query($sql);
-		$item = mysql_fetch_assoc($query);
-		
-		if($item != false && $item['dcdate'] == '0000-00-00 00:00:00'){
-			$alert_msg = $item['my_ward'];
-		}
 		
 		
-
-        print (" <tr>\n".
-           "  <td BGCOLOR=".$color."><a target=_BLANK  href=\"opedit.php? cHn=$hn & cName=$name &cSurname=$surname\">$hn</a></td>\n".
-           "  <td BGCOLOR=".$color.">$yot</td>\n".
-           "  <td BGCOLOR=".$color.">$name</td>\n".
-           "  <td BGCOLOR=".$color.">$surname</td>\n".
-			   "  <td BGCOLOR=".$color."><a target=_BLANK  href=\"opdcard_opregis.php?cHn=$hn\">$ptright</a></td>\n".
- "  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"hndaycheck.php?hn=$hn\">มา รพ.</td>\n".
-         "  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"appdaycheck.php?hn=$hn\">ตรวจนัด</td>\n".
-   // "  <td BGCOLOR=".$color."><a target= _BLANK href=\"ancheck.php?hn=$hn\">ตรวจนอน</td>\n".
-"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"opdprint2.php?cHn=$hn\">ใบต่อ</td>\n".
-	/*"  <td BGCOLOR=".$color."><a target= _BLANK href=\"edprint.php?cHn=$hn\">ใบยานอก</td>\n".
-	"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint.php?cHn=$hn\">ผู้ป่วยนัด</td>\n".
-"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint1.php?cHn=$hn\">ใบตรวจโรค</td>\n".*/
-	"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointhdvn.php?cHn=$hn\">ไต</td>\n".
-  "  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointdenvn.php?cHn=$hn\">ฟัน</td>\n".
-  "  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointeyevn.php?cHn=$hn\">ตา</td>\n".
-  "  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appoint.php?cHn=$hn\">ผป.นัด</td>\n".
-  
+		
+		
+			$sql = sprintf("SELECT `hn`,`dcdate`, `my_ward`
+			FROM `ipcard` 
+			WHERE `hn` = '%s' 
+			ORDER BY `row_id` DESC LIMIT 1", $hn);
+			$query = mysql_query($sql);
+			$item = mysql_fetch_assoc($query);
+			
+			if($item != false && $item['dcdate'] == '0000-00-00 00:00:00'){
+				$alert_msg = $item['my_ward'];
+			}
+		
+		
+		
+			print (" <tr>\n".
+			"  <td BGCOLOR=".$color."><a target=_BLANK onclick=\"checkIpd(this, event, '$hn')\" href=\"opedit.php? cHn=$hn & cName=$name &cSurname=$surname\">$hn</a></td>\n".
+			"  <td BGCOLOR=".$color.">$yot</td>\n".
+			"  <td BGCOLOR=".$color.">$name</td>\n".
+			"  <td BGCOLOR=".$color.">$surname</td>\n".
+			"  <td BGCOLOR=".$color."><a target=_BLANK  href=\"opdcard_opregis.php?cHn=$hn\">$ptright</a></td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"hndaycheck.php?hn=$hn\">มา รพ.</td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"appdaycheck.php?hn=$hn\">ตรวจนัด</td>\n".
+			// "  <td BGCOLOR=".$color."><a target= _BLANK href=\"ancheck.php?hn=$hn\">ตรวจนอน</td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"opdprint2.php?cHn=$hn\">ใบต่อ</td>\n".
+			/*"  <td BGCOLOR=".$color."><a target= _BLANK href=\"edprint.php?cHn=$hn\">ใบยานอก</td>\n".
+			"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint.php?cHn=$hn\">ผู้ป่วยนัด</td>\n".
+			"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint1.php?cHn=$hn\">ใบตรวจโรค</td>\n".*/
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointhdvn.php?cHn=$hn\">ไต</td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointdenvn.php?cHn=$hn\">ฟัน</td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appointeyevn.php?cHn=$hn\">ตา</td>\n".
+			"  <td BGCOLOR=".$color." align='center'><a target= _BLANK href=\"rg_appoint.php?cHn=$hn\">ผป.นัด</td>\n".
+			
 			"<td BGCOLOR=".$color." align='center'>".$alert_msg."</td>".
-
-           " </tr>\n");
-		  
-		   
+			
+			" </tr>\n");
+		}
 	}
-}
 	
-
-/*	$sql1="SELECT  * FROM opcard
-where name='".$_SESSION['name']."' and surname='".$_SESSION['surname']."' and hn !='". $_SESSION['hn']."' ";
-	$result1 = mysql_query($sql1);
-	$rows1=mysql_num_rows($result1);
-	if($rows1){
-	 echo "<font color='#FF0000'>ซ้ำ</font>";
-	}*/
-
 	
-// ตรวจสอบและเปลี่ยน HN AN ตอนขึ้นปีใหม่
+	// ตรวจสอบและเปลี่ยน HN AN ตอนขึ้นปีใหม่
 	$sql = "Select left(prefix,2) From runno where title = 'HN' ";
 	list($title_hn) = Mysql_fetch_row(Mysql_Query($sql));
 	$year_now = substr(date("Y")+543,2);
@@ -187,6 +157,7 @@ where name='".$_SESSION['name']."' and surname='".$_SESSION['surname']."' and hn
 		$sql = "Update runno set prefix = '".$year_now."-', runno = 0 where  title = 'HN' limit 1;";
 		$result = mysql_Query($sql);
 	}
+	
 	$sql = "Select left(prefix,2) From runno where title = 'AN' ";
 	list($title_an) = Mysql_fetch_row(Mysql_Query($sql));
 	$year_now = substr(date("Y")+543,2);
@@ -195,93 +166,56 @@ where name='".$_SESSION['name']."' and surname='".$_SESSION['surname']."' and hn
 		$result = mysql_Query($sql);
 	}
 	// END
-
-?>
-</table>
-<FONT SIZE="2" COLOR="#990000">***คำอธิบาย***</FONT> <BR>
-<FONT SIZE="" COLOR="66CDAA">สีเขียว คือ ยังไม่ได้ทำการตรวจสิทธิการรักษา</FONT><BR>
-<FONT SIZE="" COLOR="#CCFF00">สีเขียวอ่อน คือ ตรวจสอบแล้ว มีสิทธิประกันสังคม</FONT><BR>
-<FONT SIZE="" COLOR="#99CC00">สีเขียวอ่อน คือ ตรวจสอบแล้ว มีสิทธิจ่ายตรง</FONT><BR>
-<FONT SIZE="" COLOR="#FF0033">สีแดง คือ ไม่มีสิทธิ</FONT><BR>
-<?php
-
-// var_dump($_POST['id_value']);
-// var_dump($_POST['hn_value']);
-// var_dump($_POST['firstname']);
-// var_dump($_POST['lastname']);
-
-$id_value = isset($_POST['id_value']) ? $_POST['id_value'] : '' ;
-$hn_value = isset($_POST['hn_value']) ? $_POST['hn_value'] : '' ;
-$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '' ;
-$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '' ;
-
-if($id_value != ''){
-	$where = '';
-}else if($hn_value != ''){
-	$where = '';
-}else{
-	$where = '';
-}
-
-
-
-
-?>
-<hr />
-<?
-	/*$sql_chkname="SELECT  * FROM opcard
-where name='".$_SESSION['name']."' and surname='".$_SESSION['surname']."' and hn !='". $_SESSION['hn']."' ";
-	$result_chkname = mysql_query($sql_chkname);
-	$rows=mysql_num_rows($result_chkname);
 	
-	if($rows){	
-?>
-
-
-<h2><font color="#FF0000">คำเตือน</font></h2>
-<h3>มีผู้ป่วย ขื่อ  <?= $_SESSION['name']?> <?=$_SESSION['surname'];?>  ซ้ำ ในระบบทะเบียน</h3>
-<h3>กรุณาตรวจสอบผู้ป่วย</h3>
-<table>
- <tr>
-  <th bgcolor=6495ED>HN</th>
-  <th bgcolor=6495ED>ยศ</th>
-  <th bgcolor=6495ED>ชื่อ</th>
-  <th bgcolor=6495ED>สกุล</th>
-  <th bgcolor=6495ED>สิทธิ</th>
- <th bgcolor=6495ED>มา รพ.</th>
-  <th bgcolor=6495ED>ตรวจนอน</th>
- <th bgcolor=6495ED>ใบต่อ</th>
- <th bgcolor=6495ED>ใบยานอก</th>
- <th bgcolor=6495ED>ใบสั่งยา</th>
- <th bgcolor=6495ED>ใบตรวจโรค</th>
- </tr>
-<?
-	while($dbarr= mysql_fetch_array($result_chkname)){
-
-	print (" <tr>\n".
-           "  <td BGCOLOR=".$color."><a target=_BLANK  href=\"opedit.php? cHn=$dbarr[hn] & cName=$dbarr[name] &cSurname=$dbarr[surname]\">$dbarr[hn]</a></td>\n".
-           "  <td BGCOLOR=".$color.">$dbarr[yot]</td>\n".
-           "  <td BGCOLOR=".$color.">$dbarr[name]</td>\n".
-           "  <td BGCOLOR=".$color.">$dbarr[surname]</td>\n".
-			   "  <td BGCOLOR=".$color.">$dbarr[ptright]</td>\n".
- "  <td BGCOLOR=".$color."><a target= _BLANK href=\"hndaycheck.php?hn=$dbarr[hn]\">มา รพ.</td>\n".
-         "  <td BGCOLOR=".$color."><a target= _BLANK href=\"appdaycheck.php?hn=$dbarr[hn]\">ตรวจนัด</td>\n".
-   // "  <td BGCOLOR=".$color."><a target= _BLANK href=\"ancheck.php?hn=$hn\">ตรวจนอน</td>\n".
-"  <td BGCOLOR=".$color."><a target= _BLANK href=\"opdprint2.php?cHn=$dbarr[hn]\">ใบต่อ</td>\n".
-	"  <td BGCOLOR=".$color."><a target= _BLANK href=\"edprint.php?cHn=$dbarr[hn]\">ใบยานอก</td>\n".
-	"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint.php?cHn=$dbarr[hn]\">ผู้ป่วยนัด</td>\n".
-"  <td BGCOLOR=".$color."><a target= _BLANK href=\"rg_appoint1.php?cHn=$dbarr[hn]\">ใบตรวจโรค</td>\n".
- 
-           " </tr>\n");  
+	?>
+	</table>
+	<FONT SIZE="2" COLOR="#990000">***คำอธิบาย***</FONT> <BR>
+	<FONT SIZE="" COLOR="66CDAA">สีเขียว คือ ยังไม่ได้ทำการตรวจสิทธิการรักษา</FONT><BR>
+	<FONT SIZE="" COLOR="#CCFF00">สีเขียวอ่อน คือ ตรวจสอบแล้ว มีสิทธิประกันสังคม</FONT><BR>
+	<FONT SIZE="" COLOR="#99CC00">สีเขียวอ่อน คือ ตรวจสอบแล้ว มีสิทธิจ่ายตรง</FONT><BR>
+	<FONT SIZE="" COLOR="#FF0033">สีแดง คือ ไม่มีสิทธิ</FONT><BR>
+	<?php
+	
+	$id_value = isset($_POST['id_value']) ? $_POST['id_value'] : '' ;
+	$hn_value = isset($_POST['hn_value']) ? $_POST['hn_value'] : '' ;
+	$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '' ;
+	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '' ;
+	
+	if($id_value != ''){
+		$where = '';
+	}else if($hn_value != ''){
+		$where = '';
+	}else{
+		$where = '';
 	}
-}*/
-/*session_unregister("hn");
-session_unregister("name");
-session_unregister("surname");*/
-
-
-include("unconnect.inc");
-       }
+	
+	?>
+	<hr />
+	<?
+	include("unconnect.inc");
+}
 ?>
 </table>
-
+<script type="text/javascript">
+	/* checkIpd */
+	function checkIpd(link, ev, hn){
+		// SmPreventDefault(ev);
+		// var href = this.href;
+		var newSm = new SmHttp();
+		newSm.ajax(
+			'templates/regis/checkIpd.php',
+			{ id: hn },
+			function(res){
+				var txt = JSON.parse(res);
+				if( txt.state === 400 ){
+					alert('สถานะของผู้ป่วยยังอยู่ '+txt.msg+' กรุณาติดต่อที่ Ward เพื่อ Discharge');
+					SmPreventDefault(ev);
+				}else{
+					// window.open(link.href, '_blank');
+				}
+			},
+			false // true is Syncronous and false is Assyncronous (Default by true)
+		);
+		
+	}
+</script>
--- surasak3/stk_basic_opd.php
+++ surasak3/stk_basic_opd.php
@@ -1,19 +1,127 @@
 <?php
-require("fpdf/fpdf.php");
-require("fpdf/pdf.php");
 
+require("fpdf_thai/fpdf_thai.php");
 include("connect.php");
 
-$sql = "Select date_format(thidate,'%d-%m-%Y %H:%i:%s'), hn, ptname , temperature , pause , rate , weight , height , bp1 , bp2 , drugreact , congenital_disease , type , organ , doctor, clinic ,age  From opd where thdatehn = '".$_GET["dthn"]."' limit 1 ";
+class PDF extends FPDF_Thai{}
+
+function calcage($birth){
+
+	$today = getdate();   
+	$nY = $today['year']; 
+	$nM = $today['mon'] ;
+	$bY = substr($birth,0,4)-543;
+	$bM = substr($birth,5,2);
+	$ageY = $nY-$bY;
+	$ageM = $nM-$bM;
+
+	if ($ageM <0 ) {
+		$ageY = $ageY-1;
+		$ageM = 12+$ageM;
+	}
+
+	if ($ageM == 0){
+		$pAge = "$ageY ปี";
+	}else{
+		$pAge = "$ageY ปี $ageM เดือน";
+	}
+
+	return $pAge;
+}
+
+$sql = "Select thidate, hn, ptname , temperature , pause , rate , weight , height , bp1 , bp2 , drugreact , congenital_disease , type , organ , doctor, clinic, cigarette,alcohol,painscore,age From opd where thdatehn = '".$_GET["dthn"]."' limit 1 ";
 $result_dt_hn = Mysql_Query($sql);
-list($thidate, $hn, $ptname , $temperature , $pause , $rate , $weight , $height , $bp1 , $bp2 , $drugreact , $congenital_disease , $type , $organ , $doctor, $clinic,$age) = Mysql_fetch_row($result_dt_hn);
+list($thidate, $hn, $ptname , $temperature , $pause , $rate , $weight , $height , $bp1 , $bp2 , $drugreact , $congenital_disease , $type , $organ , $doctor, $clinic, $cigarette, $alcohol,$painscore,$age) = Mysql_fetch_row($result_dt_hn);
 
 
-		 $ht = $height/100;
-		 $bmi=number_format($weight /($ht*$ht),2);
-		 
+$ht = $height/100;
+$bmi=number_format($weight /($ht*$ht),2);
+
+if( empty($painscore) ){
+	$painscore = '-';
+}
+
+$thidate = substr($thidate,8,2)."-".substr($thidate,5,2)."-".substr($thidate,0,4)." ".substr($thidate,10);
+if($cigarette==0){
+	$cigarette='ไม่สูบ';
+}else if($cigarette==1){
+	$cigarette='สูบ';
+}else{
+	$cigarette='เคยสูบ';
+}
+
+if($alcohol==0){
+	$alcohol='ไม่ดื่ม';
+}else if($alcohol==1){
+	$alcohol='ดื่ม';
+}else{
+	$alcohol='เคยดื่ม';
+}
+
+if($drugreact == 0){
+	$congenital_disease .=" , ผู้ป่วยไม่แพ้ยา";
+}else{
+	$i=0;
+	$list = array();
+	$sql = "Select  tradname From drugreact  where hn = '".$hn."' ";
+	$result = Mysql_Query($sql);
+	while($arr = Mysql_fetch_assoc($result)){
+		array_push($list ,$arr["tradname"]);
+	}
+	$list_drug = implode(", ",$list);
+	$congenital_disease .= " , แพ้ยา : ".$list_drug;
+}
+
+if( empty($weight) ){
+	$weight = 0;
+}
+if( empty($height) ){
+	$height = 0;
+	$ht = 0;
+}else{
+	$ht = $height/100;
+}
+
+$bmi = number_format(($weight / ( $ht * $ht)), 2);
+
+$sql111 = "Select dbirth From opcard where hn='$hn' ";
+$result111 = Mysql_Query($sql111);
+list($dbirth) = Mysql_fetch_row($result111);
+$cAge = calcage($dbirth);
+
+
+$pdf = new PDF("L", 'mm', array( 50, 80));
+
+// $pdf->AddFont('THSarabunPSK', '', 'THSarabun.php');
+// $pdf->SetFont('THSarabunPSK', '', 14);
+$pdf->AddFont('AngsanaNew', '', 'angsa.php');
+$pdf->SetFont('AngsanaNew', '', 14);
+
+
+$pdf->SetAutoPageBreak(true, 2);
+$pdf->SetMargins(2, 2);
+// $pdf->SetLineWidth(80);
+$pdf->AddPage();
+
+$pdf->Cell(0, 6, "HN: $hn, $thidate, $cAge");
+$pdf->Ln();
+$pdf->Cell(0, 6, "T: $temperature C, P: $pause ครั้ง/นาที, R: $rate ครั้ง/นาที");
+$pdf->Ln();
+$pdf->Cell(0, 6, "BP: $bp1 / $bp2 mmHg, นน: $weight กก., สส: $height ซม.");
+$pdf->Ln();
+$pdf->Cell(0, 6, "บุหรี่: $cigarette, สุรา: $alcohol, bmi: $bmi, PS: $painscore");
+$pdf->Ln();
+$pdf->Cell(0, 6, "ลักษณะ: $type, คลินิก: ".(substr($clinic,3)));
+$pdf->Ln();
+$pdf->Cell(0, 6, "โรคประจำตัว: ".trim($congenital_disease));
+$pdf->Ln();
+$pdf->MultiCell(0, 6, "อาการ: ".trim($organ));
+$pdf->Ln();
+
+$pdf->Output();
+
+/*
 $ll = "P";
-
 $pdf = new PDF($ll,'mm',array( 80,50 ));
 $pdf->SetThaiFont();
 $pdf->SetAutoPageBreak(false,0);
@@ -51,5 +159,5 @@ $pdf->Cell(0,5,"B : ".$congenital_disease,0,0);
 
 
 $pdf->Output();
-
+*/
 ?>
