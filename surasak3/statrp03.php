2c839f9079a53c3e5a6db89477572fdf4dae9bcb
kritsanasak kuntaros <roboconk@gmail.com>
2016-03-02 18:44:34 +0700

    - แก้ไข หน้าสถิติของฝังเข็มเปลี่ยนจาก pdf เป็นหน้าเว็บ

--- surasak3/statrp03.php
+++ surasak3/statrp03.php
@@ -31,38 +31,11 @@ $time_zone = explode("-",$_GET["time"]);
 
 if($_GET["code"] == "58001" OR $_GET["code"] == "58000" ){ //ฝังเข็ม
 
-	$pdf = new PDF('P' ,'mm','A4');
-	
-	$pdf->SetThaiFont();
-	
-	$pdf->SetMargins(10, 10);
-	
-	$pdf->AddPage();
-	
-	$pdf->SetFont('AngsanaNew', '', 14);
-
-
-	/*
-	$sql = "
-	SELECT DISTINCT a.hn, a.ptname
-	FROM (
-	
-	SELECT hn, ptname, date
-	FROM patdata
-	WHERE (
-	date
-	BETWEEN '".$_GET["year"]."-".$_GET["month"]."-".$_GET["day"]." ".$time_zone[0]."' AND '".$_GET["year"]."-".$_GET["month"]."-".$_GET["day"]." ".$time_zone[1]."'
-	) AND doctor = '".$_GET["doctor"]."' ".$where."
-	) AS a
-	INNER JOIN (
-	
-	SELECT hn
-	FROM opacc
-	WHERE date
-	LIKE '".$_GET["year"]."-".$_GET["month"]."-".$_GET["day"]."%' AND depart = 'NID'
-	) AS b ON a.hn = b.hn
-	Order by a.date ASC limit 70 
-	";*/
+	// $pdf = new PDF('P' ,'mm','A4');
+	// $pdf->SetThaiFont();
+	// $pdf->SetMargins(10, 10);
+	// $pdf->AddPage();
+	// $pdf->SetFont('AngsanaNew', '', 14);
 
 	$sql = "SELECT hn, ptname, idno   
 	FROM patdata 
@@ -74,85 +47,163 @@ if($_GET["code"] == "58001" OR $_GET["code"] == "58000" ){ //ฝังเข็ม
 	limit 70 ";
 
 	file_put_contents('logs/mysql-query.log', $sql, FILE_APPEND);
-	//echo $sql;
+	
 	$result2  = Mysql_Query($sql) or die( mysql_error() );
 	
-	
 	$txt = "คลินิกนอกเวลาราชการ (ฝังเข็ม) เวลา ";
-
 	switch($_GET["time"]){
 		case "07:30:00-12:30:00" : $txt .= "08.00 - 12.00"; break;
 		case "16:20:00-21:00:00" : $txt .= "16.30 - 20.30"; break;
-		
 		case "08.00:00-16:00:00" : $txt .= "08.00 - 16.00"; break;
-		/*			case "07:30:00-16:00:00" : $txt .= "08.00 - 16.00"; break;*/
 		case "16:00:01-20:30:00" : $txt .= "16.00 - 20.00"; break;
 	
 	}
 
-	$pdf->Cell(0,7,$txt,0,0,'C');
-	$pdf->Ln();
-	
-	
-	$pdf->Cell(0,7,"วันที่ ".$_GET["day"]." ".$month_[$_GET["month"]]." ".$_GET["year"],0);
-	$pdf->Ln();
-	
-	$pdf->Cell(10,7,"ลำดับ",1,0,'C');
-	
-	$pdf->Cell(100,7,"ชื่อ - สกุล ผู้รับบริการ",1,0,'C');
-	
-	$pdf->Cell(30,7,"HN",1,0,'C');
-	
-	$pdf->Cell(30,7,"โรค",1,0,'C');
-	
-	$pdf->Ln();
-	
-	
+	// $pdf->Cell(0,7,$txt,0,0,'C');
+	// $pdf->Ln();
+	// $pdf->Cell(0,7,"วันที่ ".$_GET["day"]." ".$month_[$_GET["month"]]." ".$_GET["year"],0);
+	// $pdf->Ln();
+	// $pdf->Cell(10,7,"ลำดับ",1,0,'C');
+	// $pdf->Cell(100,7,"ชื่อ - สกุล ผู้รับบริการ",1,0,'C');
+	// $pdf->Cell(30,7,"HN",1,0,'C');
+	// $pdf->Cell(30,7,"โรค",1,0,'C');
+	// $pdf->Ln();
+	?>
+	<style type="text/css">
+		body, th, td{
+			font-family: 'TH SarabunPSK';
+			font-size: 20px;
+		}
+		.main-contain{
+			width: 80%;
+		}
+		@media screen and (max-width: 992px){
+			.main-contain{
+				width: 100%;
+			}
+		}
+		@media print{
+			.main-contain{
+				width: 100%;
+			}
+			.fix-head, .fix-bottom{
+				display: table-header-group;
+			}
+		}
+	</style>
+	<div class="main-contain">
+		<div class="fix-head">
+			<div style="text-align: center;">
+				<p><?=$txt;?></p>
+			</div>
+			<div>
+				<p><?php echo "วันที่ ".$_GET["day"]." ".$month_[$_GET["month"]]." ".$_GET["year"];?></p>
+			</div>
+		</div>
+		<div>
+			<table width="100%" border="1" cellspacing="0" cellpadding="3"  bordercolor="#000000" style="border-collapse:collapse">
+				<thead>
+					<tr>
+						<th>ลำดับ</th>
+						<th>ชื่อ - สกุล ผู้รับบริการ</th>
+						<th>HN</th>
+						<th>โรค</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					
+					$i=1;
+					while(list($hn,$ptname, $idno) = mysql_fetch_row($result2)){	
+						
+						$sql = "SELECT `diag` 
+						FROM `depart` 
+						WHERE `row_id` = '$idno'";
+						$query = mysql_query($sql) or die( mysql_error() );
+						$res = mysql_fetch_assoc($query);
+					
+					?>
+					<tr>
+						<td align="center"><?=$i;?></td>
+						<td><?=$ptname;?></td>
+						<td align="center"><?=$hn;?></td>
+						<td align="center"><?=$res['diag'];?></td>
+					</tr>
+					<?php
+						$i++;
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
+		<div style="padding: 0.5em;"></div>
+		<div class="fix-bottom">
+			<table width="100%" border="0" cellspacing="0" cellpadding="3"  bordercolor="#000000" style="border-collapse:collapse">
+				<tbody>
+					<tr>
+						<td width="50%">ผู้บันทึก</td>
+						<td width="50%">นาย</td>
+					</tr>
+					<tr>
+						<td><span style="padding-left: 50px;"></span>(<span style="padding: 0 80px;"></span>)</td>
+						<td><span style="padding-left: 50px;"></span>(<span style="padding: 0 80px;"></span>)</td>
+					</tr>
+					<tr>
+						<td><span style="padding-left: 65px;"></span>เจ้าหน้าที่คลินิกฝังเข็ม</td>
+						<td><span style="padding-left: 95px;"></span>แพทย์ผู้รักษา</td>
+					</tr>
+					<tr>
+						<td><span style="padding-left: 65px;"></span>........../........../..........</td>
+						<td><span style="padding-left: 80px;">........../........../..........</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<?php
+	exit;
 	$i=1;
-	while(list($hn,$ptname, $idno) = Mysql_fetch_row($result2)){	
+	while(list($hn,$ptname,$idno) = mysql_fetch_row($result2)){	
 		
 		$sql = "SELECT `diag` 
-FROM `depart` 
-WHERE `row_id` = '$idno'";
-	$query = mysql_query($sql) or die( mysql_error() );
-	$res = mysql_fetch_assoc($query);
-	// echo "<pre>";
-	// var_dump($res['diag']);
-	// exit;
+		FROM `depart` 
+		WHERE `row_id` = '$idno'";
+		$query = mysql_query($sql) or die( mysql_error() );
+		$res = mysql_fetch_assoc($query);
 	
-		$pdf->Cell(10,7,$i,1,0,'C');
+		// $pdf->Cell(10,7,$i,1,0,'C');
+		// $pdf->Cell(100,7,$ptname,1,0);
+		// $pdf->Cell(30,7,$hn,1,0,'C');
 		
-		$pdf->Cell(100,7,$ptname,1,0);
-		
-		$pdf->Cell(30,7,$hn,1,0,'C');
-		
-		$pdf->Cell(30,7,$res['diag'],1,0,'C');
-		$pdf->Ln();
+		// $pdf->Cell(30,7,$res['diag'],1,0,'C');
+		// $pdf->Ln();
 		$i++;
 	}
 	
-	$pdf->Ln();
+	// $pdf->Ln();
 	
-	$pdf->Cell(20,7,"ผู้บันทึก",0,0,'C');
-	if($_GET["doctor"]=="MD115"){
-		$pdf->Cell(100,7,"นาย",0,0,'R');
-	}else{
-		$pdf->Cell(100,7,"พ.อ.",0,0,'R');
-	}
-	$pdf->Ln();
+	// $pdf->Cell(20,7,"ผู้บันทึก",0,0,'C');
+	// if($_GET["doctor"]=="MD115"){
+	// 	$pdf->Cell(100,7,"นาย",0,0,'R');
+	// }else{
+	// 	$pdf->Cell(100,7,"พ.อ.",0,0,'R');
+	// }
+	// $pdf->Ln();
 	
-	$pdf->Cell(65,7,"(                                          )",0,0,'C');
-	$pdf->Cell(90,7,"(                                          )",0,0,'R');
-	$pdf->Ln();
+	// $pdf->Cell(65,7,"(                                          )",0,0,'C');
+	// $pdf->Cell(90,7,"(                                          )",0,0,'R');
+	// $pdf->Ln();
 	
-	$pdf->Cell(66,7,"เจ้าหน้าที่คลินิกฝังเข็ม",0,0,'C');
-	$pdf->Cell(140,7,"แพทย์ผู้รักษา",0,0,'C');
-	$pdf->Ln();
+	// $pdf->Cell(66,7,"เจ้าหน้าที่คลินิกฝังเข็ม",0,0,'C');
+	// $pdf->Cell(140,7,"แพทย์ผู้รักษา",0,0,'C');
+	// $pdf->Ln();
 	
-	$pdf->Cell(65,7,"........../........../..........",0,0,'C');
-	$pdf->Cell(140,7,"........../........../..........",0,0,'C');
-	$pdf->Ln();
+	// $pdf->Cell(65,7,"........../........../..........",0,0,'C');
+	// $pdf->Cell(140,7,"........../........../..........",0,0,'C');
+	// $pdf->Ln();
 	//ฝังเข็ม
+	
+	
 }else{
 	
 	
@@ -292,12 +343,12 @@ $pdf->Ln();
 $pdf->Cell(65,7,"........../........../..........",0,0,'C');
 $pdf->Cell(140,7,"........../........../..........",0,0,'C');
 $pdf->Ln();
-
+$pdf->Output();
 }//นวดแผนไทย
 
 
 
 require("unconnect.php");
-$pdf->Output();
+
 
 ?>
