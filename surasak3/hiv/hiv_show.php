b9482494eb73aa608772835f1b3c13e8c763303d
kritsanasak kuntaros <roboconk@gmail.com>
2016-04-04 16:05:40 +0700

    - แก้ไข เมนูหลักบนเบราเซอร์รุ่นใหม่

--- surasak3/hiv/hiv_show.php
+++ surasak3/hiv/hiv_show.php
@@ -81,21 +81,7 @@ div#copyright a:hover { color:#fff; }*/
   
 
 <div id="no_print">
-<div id="menu">
-  <ul class="menu"> 
-        <li><a href="http://192.168.1.2/sm3/nindex.htm" class="parent"><span>โปรแกรมโรงพยาบาล</span></a></li>
-        <li><a href="hiv_form.php"><span>ลงทะเบียนผู้ป่วย HIV</span></a></li>
-     	 <li><a href="hiv_edit.php"><span>แก้ไขข้อมูลผู้ป่วย HIV</span></a></li>
-         <li><a href="#"><span>รายชื่อผู้ป่วย HIV</span></a></li>
-     
-     <ul>
-	  	<li class="last"><a href="hiv_show.php?detail=vo"><span>สิทธิเบิกทั่วไป</span></a></li>
-	  	<li class="last"><a href="hiv_show.php?detail=vp"><span>สิทธิเบิกประกันสังคม</span></a></li>
-        <li class="last"><a href="hiv_show.php?detail=nhso"><span>สิทธิเบิก สปสช.</span></a></li>
-        
-       	</ul>
-    </ul>
-</div>
+<?php include 'main_menu.php';?>
 <div style="visibility: hidden">
  <br />
  <a href="http://apycom.com/">a</a><br />
---
+++ surasak3/hiv/main_menu.php
@@ -0,0 +1,15 @@
+<div id="menu">
+	<ul class="menu"> 
+		<li><a href="http://192.168.1.2/sm3/nindex.htm" class="parent"><span>โปรแกรมโรงพยาบาล</span></a></li>
+		<li><a href="hiv_form.php"><span>ลงทะเบียนผู้ป่วย HIV</span></a></li>
+		<li><a href="hiv_edit.php"><span>แก้ไขข้อมูลผู้ป่วย HIV</span></a></li>
+		<li>
+			<a href="#"><span>รายชื่อผู้ป่วย HIV</span></a>
+			<ul>
+				<li class="last"><a href="hiv_show.php?detail=vo"><span>สิทธิเบิกทั่วไป</span></a></li>
+				<li class="last"><a href="hiv_show.php?detail=vp"><span>สิทธิเบิกประกันสังคม</span></a></li>
+				<li class="last"><a href="hiv_show.php?detail=nhso"><span>สิทธิเบิก สปสช.</span></a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
