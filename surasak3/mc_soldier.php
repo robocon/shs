2397565f8d445c83b97200a4e4793cd8bc6e0057
kritsanasak kuntaros <roboconk@gmail.com>
2016-11-14 14:38:45 +0700

    แก้ไข การแสดงผลโดยตัด hn ที่ซ้ำซ้อนออก

--- surasak3/mc_soldier.php
+++ surasak3/mc_soldier.php
@@ -1,73 +1,24 @@
 <?php
- include("connect.inc");   
-	$month["01"] = "มกราคม";
-    $month["02"] = "กุมภาพันธ์";
-    $month["03"] = "มีนาคม";
-    $month["04"] = "เมษายน";
-    $month["05"] = "พฤษภาคม";
-    $month["06"] = "มิถุนายน";
-    $month["07"] = "กรกฏาคม";
-    $month["08"] = "สิงหาคม";
-    $month["09"] = "กันยายน";
-    $month["10"] = "ตุลาคม";
-    $month["11"] = "พฤศจิกายน";
-    $month["12"] = "ธันวาคม";
+include 'connect.inc';  
+include 'includes/functions.php';
+
+$month["01"] = "มกราคม";
+$month["02"] = "กุมภาพันธ์";
+$month["03"] = "มีนาคม";
+$month["04"] = "เมษายน";
+$month["05"] = "พฤษภาคม";
+$month["06"] = "มิถุนายน";
+$month["07"] = "กรกฏาคม";
+$month["08"] = "สิงหาคม";
+$month["09"] = "กันยายน";
+$month["10"] = "ตุลาคม";
+$month["11"] = "พฤศจิกายน";
+$month["12"] = "ธันวาคม";
 ?>
-
-
-<style>
-	.font_tr{ font-family:"Angsana New"; font-size:20px; background-color:"#F5DEB3"; }
-	.font_hd{ font-family:"Angsana New"; font-size:20px; background-color:"#CD853F"; }
-</style>
-
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;รายชื่อผู้มาขอใบรับรองงดเกณฑ์ทหาร</p>
-<form name="ff1" method="post" action="<?php echo $PHP_SELF ?>">
-
-<TABLE>
-
-
-<TR id="row2" >
-	<TD align="right">ตั้งแต่วันที่ :</TD>
-	<TD>
-		<INPUT TYPE="text" NAME="start_day" value="<?php if(isset($_POST["start_day"])) echo $_POST["start_day"]; else echo date("d");?>" size="2" maxlength="2"> / 
-		<SELECT NAME="start_month">
-		<?php
-		foreach($month as $value => $index){
-			echo "<OPTION VALUE=\"",$value,"\" ";
-			if($_POST["start_month"] == $value){ echo " Selected ";}
-			 else if( !isset($_POST["start_month"]) && date("m") == $value){ echo " Selected ";}
-			echo ">",$index;
-			
-			}	?>
-			
-		</SELECT> / 
-		<INPUT TYPE="text" NAME="start_year" value="<?php if(isset($_POST["start_year"])) echo $_POST["start_year"]; else echo date("Y")+543;?>"  size="4" maxlength="4">
-	</TD>
-</TR>
-<TR id="row3">
-	<TD align="right">ถึงวันที่ :</TD>
-	<TD><INPUT TYPE="text" NAME="end_day" value="<?php if(isset($_POST["end_day"])) echo $_POST["end_day"]; else echo date("d");?>" size="2" maxlength="2"> / 
-		<SELECT NAME="end_month">
-		<?php
-		foreach($month as $value => $index){
-			echo "<OPTION VALUE=\"",$value,"\" ";
-			 if($_POST["end_month"] == $value){ echo " Selected ";}
-			 else if( !isset($_POST["end_month"]) && date("m") == $value) echo " Selected ";
-			echo ">",$index;
-			
-			}	?>
-			
-		</SELECT> / 
-		<INPUT TYPE="text" NAME="end_year" value="<?php if(isset($_POST["end_year"])) echo $_POST["end_year"]; else echo date("Y")+543;?>"  size="4" maxlength="4"></TD>
-</TR>
-</TABLE>
- <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
- 
- <input type="submit" value="      ตกลง      " name="B1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
- <INPUT TYPE="button" value="Print" Onclick="window.open('mc_soldier_print.php?sd='+document.ff1.start_year.value+'-'+document.ff1.start_month.value+'-'+document.ff1.start_day.value+'&ed='+document.ff1.end_year.value+'-'+document.ff1.end_month.value+'-'+document.ff1.end_day.value+'');">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
- <a target=_self  href='../nindex.htm'><<ไปเมนู</a></p>
-</form>
 <style type="text/css">
+	.font_tr{ font-family:"TH SarabunPSK"; font-size:20px; background-color:"#F5DEB3"; }
+	.font_hd{ font-family:"TH SarabunPSK"; font-size:20px; background-color:"#CD853F"; }
+
 	table, tr, td, th{
 		border: 1px solid black;
 	}
@@ -75,48 +26,141 @@
 		border-collapse: collapse; 
 		border-spacing: 0; 
 	}
-	
+	#soldier_header,
+	#soldier_header tr,
+	#soldier_header td{
+		border: 0;
+	}
+	@media print{
+		.disappear{
+			display: none;
+		}
+		#soldier_header{
+			display: block!important;
+		}
+	}
 </style>
-<table style="">
-	<tr class="font_hd">
-		<th width="2%">ลำดับ</th>
-		<th width="15%">ชื่อ-สกุล</th>
-		<th>โรคที่ตรวจพบ</th>
-		<th width="15%">ตามกฏทรวงฉบับที่ ๗๔ พ.ศ. ๒๕๔๐<br />
-		และฉบับแก้ไขที่ ๗๖ พ.ศ. ๒๕๕๕</th>
-		<th width="12%">คณะแพทย์ผู้ตรวจ</th>
-		<th width="15%">ภูมิลำเนาทหาร</th>
-		<th width="5%">ว.ด.ป. ที่รับการตรวจ</th>
-		<th>เพิ่ม/แก้ไขข้อมูล</th>
-	</tr>
+
+<p class="disappear">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;รายชื่อผู้มาขอใบรับรองงดเกณฑ์ทหาร</p>
+<form name="ff1" method="post" action="mc_soldier.php" class="disappear">
+	<TABLE>
+	<TR id="row2" >
+		<TD align="right">ตั้งแต่วันที่ :</TD>
+		<TD>
+			<INPUT TYPE="text" NAME="start_day" value="<?php if(isset($_POST["start_day"])) echo $_POST["start_day"]; else echo date("d");?>" size="2" maxlength="2"> / 
+			<SELECT NAME="start_month">
+				<?php
+				foreach($month as $value => $index){
+					echo "<OPTION VALUE=\"",$value,"\" ";
+					if($_POST["start_month"] == $value){ echo " Selected ";}
+					else if( !isset($_POST["start_month"]) && date("m") == $value){ echo " Selected ";}
+					echo ">",$index;
+				}
+				?>
+			</SELECT> / 
+			<INPUT TYPE="text" NAME="start_year" value="<?php if(isset($_POST["start_year"])) echo $_POST["start_year"]; else echo date("Y")+543;?>"  size="4" maxlength="4">
+		</TD>
+	</TR>
+	<TR id="row3">
+	<TD align="right">ถึงวันที่ :</TD>
+	<TD><INPUT TYPE="text" NAME="end_day" value="<?php if(isset($_POST["end_day"])) echo $_POST["end_day"]; else echo date("d");?>" size="2" maxlength="2"> / 
+	<SELECT NAME="end_month">
+	<?php
+	foreach($month as $value => $index){
+		echo "<OPTION VALUE=\"",$value,"\" ";
+		if($_POST["end_month"] == $value){ echo " Selected ";}
+		else if( !isset($_POST["end_month"]) && date("m") == $value) echo " Selected ";
+		echo ">",$index;
+
+	}
+	?>
+	</SELECT> / 
+	<INPUT TYPE="text" NAME="end_year" value="<?php if(isset($_POST["end_year"])) echo $_POST["end_year"]; else echo date("Y")+543;?>"  size="4" maxlength="4"></TD>
+	</TR>
+	</TABLE>
+
+ 	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ 
+	<input type="submit" value="      ตกลง      " name="B1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<INPUT TYPE="button" value="Print" Onclick="window.open('mc_soldier_print.php?sd='+document.ff1.start_year.value+'-'+document.ff1.start_month.value+'-'+document.ff1.start_day.value+'&ed='+document.ff1.end_year.value+'-'+document.ff1.end_month.value+'-'+document.ff1.end_day.value+'');">
+	
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ 	<a target=_self  href='../nindex.htm'><<ไปเมนู</a></p>
+</form>
+
 <?php
-$num=0;
+
 if( !empty($B1) ){
-	include("connect.inc");
+	
 	$ymd_start = $_POST["start_year"]."-".$_POST["start_month"]."-".$_POST["start_day"];
 	$ymd_end = $_POST["end_year"]."-".$_POST["end_month"]."-".$_POST["end_day"];
-	
-	$where = " AND (a.thidate between '$ymd_start 00:00:00' AND '$ymd_end 23:59:59' ) ";
-	$sql = "
-		SELECT a.row_id, date_format(a.thidate,'%d-%m-%Y'), a.hn, a.ptname, a.organ, a.dx_mc_soldier, a.dr1_mc_soldier, a.dr2_mc_soldier, a.dr3_mc_soldier, a.address, a.thdatehn, a.rule 
-		, b.idcard
-	FROM opd AS a
-	LEFT JOIN opcard AS b ON b.hn = a.hn 
-	WHERE (
-		( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%ทหาร%' AND a.organ LIKE '%เกณ%' ) 
-		OR ( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%เลือกทหาร%' ) 
-		OR a.toborow like 'EX30%'
-	) 
-	".$where." ORDER BY thidate ASC ";
-	
-	$result = mysql_query($sql) or die("Query failed ".mysql_error());
-	
+
+	$sql = "SELECT b.`row_id`,b.`hn`,b.`ptname`,b.`thdatehn`,b.`organ`,b.`dx_mc_soldier`,b.`dr1_mc_soldier`,b.`dr2_mc_soldier`,b.`dr3_mc_soldier`,b.`rule`,
+	CONCAT(c.`address`,' ',c.`tambol`,' ',c.`ampur`,' ',c.`changwat`) AS `address`,
+	CONCAT(SUBSTRING(b.`thidate`,9,2),'-',SUBSTRING(b.`thidate`,6,2),'-',SUBSTRING(b.`thidate`,1,4)) AS `date`,
+	c.`idcard`
+	FROM 
+	(
+		SELECT MAX(`row_id`) AS `opd_id`
+		FROM `opd` 
+		WHERE `thidate` >= '$ymd_start' AND `thidate` <= '$ymd_end' 
+		AND (
+			( `organ` LIKE '%รับรอง%' AND `organ` LIKE '%งดเกณ%' )
+			OR `toborow` LIKE 'EX30%' 
+		)
+		GROUP BY `hn`
+	) AS a 
+	LEFT JOIN `opd` AS b ON b.`row_id` = a.`opd_id` 
+	LEFT JOIN `opcard` AS c ON c.`hn` = b.`hn`";
+
+
 	$notPassed = 0;
-	while (list ($row_id, $date,$hn,$ptname,$organ, $dx_mc_soldier, $dr1_mc_soldier, $dr2_mc_soldier, $dr3_mc_soldier,$address1,$thdatehn,$rule,$idcard) = mysql_fetch_row ($result)) 
+	$num = 0;
+	$result = mysql_query($sql) or die("Query failed: ".mysql_error());
+	?>
+	<table style="">
+		<thead>
+			<tr class="font_hd">
+				<th width="2%">ลำดับ</th>
+				<th width="15%">ชื่อ-สกุล</th>
+				<th>โรคที่ตรวจพบ</th>
+				<th width="15%">ตามกฏทรวงฉบับที่ ๗๔ พ.ศ. ๒๕๔๐<br />
+				และฉบับแก้ไขที่ ๗๖ พ.ศ. ๒๕๕๕</th>
+				<th width="12%">คณะแพทย์ผู้ตรวจ</th>
+				<th width="15%">ภูมิลำเนาทหาร</th>
+				<th width="5%">ว.ด.ป. ที่รับการตรวจ</th>
+				<th class="disappear">เพิ่ม/แก้ไขข้อมูล</th>
+			</tr>
+		</thead>
+		<tbody>
+	<?php
+
+
+	while ( $item = mysql_fetch_assoc($result)) 
 	{
+		$row_id = $item['row_id'];
+		$date = $item['date'];
+		$hn = $item['hn'];
+		$ptname = $item['ptname'];
+		$organ = $item['organ'];
+		$dx_mc_soldier = $item['dx_mc_soldier'];
+		$dr1_mc_soldier = $item['dr1_mc_soldier'];
+		$dr2_mc_soldier = $item['dr2_mc_soldier'];
+		$dr3_mc_soldier = $item['dr3_mc_soldier'];
+		$address = $item['address'];
+		$thdatehn = $item['thdatehn'];
+		$rule = $item['rule'];
+		$idcard = $item['idcard'];
+
 		$Total = $Total+$amount; 
-		list($address) = mysql_fetch_row(mysql_query("Select concat(address,' ', tambol,' ',  ampur,' ',  changwat  ) From opcard where hn = '".$hn."' limit 0,1 "));
-		$thdatehn = substr($thdatehn,0,10);
+		// $sql = "Select concat(address,' ',tambol,' ',ampur,' ',changwat) 
+		// From opcard 
+		// where hn = '".$hn."' 
+		// limit 0,1 ";
+		// list($address) = mysql_fetch_row(mysql_query($sql));
+
+
+		// $thdatehn = substr($thdatehn,0,10);
 		$num++;
 		
 		$dr1 = preg_replace('/MD\d+\s/', '', $dr1_mc_soldier);
@@ -127,30 +171,51 @@ if( !empty($B1) ){
 			$notPassed++;
 		}
 		
+		$style = '';
+		if( empty($row_id) OR empty($dx_mc_soldier) ){
+			$style = 'disappear';
+		}
 		?>
-		<tr class="font_tr">
-			<td align="center"><?php echo $num;?></td>
-			<td><?php echo $ptname;?><br><?php echo $idcard;?></td>
+		<tr class="font_tr <?=$style;?>">
+			<td align="center"><?=to_thai_number($num);?></td>
+			<td><?php echo $ptname;?><br><?=to_thai_number($idcard);?></td>
 			<td><?php echo $dx_mc_soldier;?></td>
 			<td><?php echo $rule;?></td>
 			<td><?php echo $dr1."<br>".$dr2."<br>".$dr3;?></td>
-			<td><?php echo $address1;?></td>
-			<td><?php echo $thdatehn;?></td>
-			<td><a href="edit_report_mc.php?id=<?php echo $row_id;?>" target="_blank">เพิ่ม/แก้ไขข้อมูล</a></td>
+			<td><?=to_thai_number($address);?></td>
+			<td><?=to_thai_number($date);?></td>
+			<td class="disappear">
+				<?php
+				if( !empty($row_id) ){
+					?>
+					<a href="edit_report_mc.php?id=<?php echo $row_id;?>" target="_blank">เพิ่ม/แก้ไขข้อมูล</a>
+					<?php
+				}else{
+					?>
+					<p>ไม่มีข้อมูลซักประวัติ</p>
+					<?php
+				}
+				?>
+			</td>
 		</tr>
 		<?php
 	}
-	include("unconnect.inc");
-}
-?>
-</table>
-<?php
-if( $num > 0 ){
-$passed = $num - $notPassed;
-?>
-<h3>สรุปยอด</h3>
-<p>จำนวนผู้ที่ได้รับยกเว้นเกฑณ์ทหาร  <?php echo $passed;?> คน</p>
-<p>จำนวนผู้ที่ไม่ได้รับยกเว้นเกฑณ์ทหาร  <?php echo $notPassed;?> คน</p>
-<?php
+	
+
+	?>
+		</tbody>
+	</table>
+	<?php
+	if( $num > 0 ){
+		$passed = $num - $notPassed;
+		?>
+		<div class="disappear">
+			<h3>สรุปยอด</h3>
+			<p>จำนวนผู้ที่ได้รับยกเว้นเกฑณ์ทหาร <?php echo $passed;?> คน</p>
+			<p>จำนวนผู้ที่ไม่ได้รับยกเว้นเกฑณ์ทหาร <?php echo $notPassed;?> คน</p>
+		</div>
+		<?php
+	}
+
 }
 ?>
--- surasak3/mc_soldier_print.php
+++ surasak3/mc_soldier_print.php
@@ -1,6 +1,5 @@
 <?php
 include("connect.inc");  
-
 $month["01"] = "มกราคม";
 $month["02"] = "กุมภาพันธ์";
 $month["03"] = "มีนาคม";
@@ -21,11 +20,10 @@ function thaiNumber($str){
 }
 ?>
 <style>
-.font_tr{ font-family:"THSarabunPSK"; font-size:20px; background-color:"#FFFFFF"; }
-.font_hd{ font-family:"THSarabunPSK"; font-size:20px; background-color:"#FFFFFF"; }
-.font_hd1{ font-family:"THSarabunPSK"; font-size:23px; background-color:"#FFFFFF"; }
+.font_tr{ font-family:"TH SarabunPSK"; font-size:20px; background-color:"#FFFFFF"; }
+.font_hd{ font-family:"TH SarabunPSK"; font-size:20px; background-color:"#FFFFFF"; }
+.font_hd1{ font-family:"TH SarabunPSK"; font-size:23px; background-color:"#FFFFFF"; }
 </style>
-
 <table width="100%" border="0" bordercolor="#000000" style='BORDER-COLLAPSE: collapse'>
 	<TR class="font_hd1" align='center'>
 		<TD>รายงานการตรวจโรคชายไทยก่อนเกณฑ์ทหาร (พี่พบความผิดปกติ)</TD>
@@ -37,65 +35,109 @@ function thaiNumber($str){
 		<TD>โรงพยาบาลค่ายสุรศักดิ์มนตรี  ทภ.๓</TD>
 	</TR>
 </table>
-
 <table width="100%" border="1" bordercolor="#000000" style='BORDER-COLLAPSE: collapse'>
-	<tr class="font_hd">
-		<th width="2%">ลำดับ</th>
-		<th width="15%">ชื่อ-สกุล</th>
-		<th>โรคที่ตรวจพบ</th>
-		<th width="14%">ตามกฏทรวงฉบับที่ ๗๔ พ.ศ. ๒๕๔๐ และฉบับแก้ไขที่ ๗๖ พ.ศ. ๒๕๕๕</th>
-		<th width="14%">คณะแพทย์ผู้ตรวจ</th>
-		<th width="15%">ภูมิลำเนาทหาร</th>
-		<th width="8%">ว.ด.ป. ที่รับการตรวจ</th>
-	</tr>
-<?php
-
-$num=0;
-$where = " AND (thidate between '".$_GET["sd"]." 00:00:00' AND '".$_GET["ed"]." 23:59:00' ) ";
-
-// $sql = "SELECT row_id, date_format(thidate,'%d-%m-%Y'), hn, ptname, organ, dx_mc_soldier, dr1_mc_soldier, dr2_mc_soldier, dr3_mc_soldier,address,thdatehn,rule FROM opd WHERE (dx_mc_soldier is not null AND dx_mc_soldier != '' ) ".$where." Order by  thidate ASC ";
-$sql = "
-SELECT a.row_id, date_format(a.thidate,'%d-%m-%Y'), a.hn, a.ptname, a.organ, a.dx_mc_soldier, a.dr1_mc_soldier, a.dr2_mc_soldier, a.dr3_mc_soldier, a.address, a.thdatehn, a.rule 
-, b.idcard
-FROM opd AS a
-LEFT JOIN opcard AS b ON b.hn = a.hn 
-WHERE (
-	( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%ทหาร%' AND a.organ LIKE '%เกณ%' ) 
-	OR ( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%เลือกทหาร%' ) 
-	OR a.toborow like 'EX30%'
-) 
-AND dx_mc_soldier IS NOT NULL 
-AND dx_mc_soldier != ''
-".$where." 
-ORDER BY thidate ASC ";
-$result = mysql_query($sql) or die("Query failed ".mysql_error());
-
-
-while (list ($row_id, $date,$hn,$ptname,$organ, $dx_mc_soldier, $dr1_mc_soldier, $dr2_mc_soldier, $dr3_mc_soldier,$address1,$thdatehn,$rule,$idcard) = mysql_fetch_row ($result)) 
-{
-	$Total =$Total+$amount; 
-	list($address) = mysql_fetch_row(mysql_query("Select concat(address,' ', tambol,' ',  ampur,' ',  changwat  ) From opcard where hn = '".$hn."' limit 0,1 "));
-	$thdatehn=substr($thdatehn,0,10);
-	
-	$num++;
-	$dr1 = preg_replace('/MD\d+\s/', '', $dr1_mc_soldier);
-	$dr2 = preg_replace('/MD\d+\s/', '', $dr2_mc_soldier);
-	$dr3 = preg_replace('/MD\d+\s/', '', $dr3_mc_soldier);
-	
-	?>
-	<tr class="font_tr">
-		<td align="center"><?php echo $num;?></td>
-		<td><?php echo $ptname;?><br><?php echo thaiNumber($idcard);?></td>
-		<td><?php echo thaiNumber($dx_mc_soldier);?></td>
-		<td><?php echo thaiNumber($rule);?></td>
-		<td><?php echo $dr1."<br>".$dr2."<br>".$dr3;?></td>
-		<td><?php echo thaiNumber($address1);?></td>
-		<td><?php echo thaiNumber($thdatehn);?></td>
-	</tr>
+	<thead>
+		<tr class="font_hd">
+			<th width="2%">ลำดับ</th>
+			<th width="15%">ชื่อ-สกุล</th>
+			<th>โรคที่ตรวจพบ</th>
+			<th width="14%">ตามกฏทรวงฉบับที่ ๗๔ พ.ศ. ๒๕๔๐ และฉบับแก้ไขที่ ๗๖ พ.ศ. ๒๕๕๕</th>
+			<th width="14%">คณะแพทย์ผู้ตรวจ</th>
+			<th width="15%">ภูมิลำเนาทหาร</th>
+			<th width="8%">ว.ด.ป. ที่รับการตรวจ</th>
+		</tr>
+	</thead>
+	<tbody>
 	<?php
-}
+	$num=0;
+	$where = " AND (thidate between '".$_GET["sd"]." 00:00:00' AND '".$_GET["ed"]." 23:59:00' ) ";
 
-include("unconnect.inc");
+	$ymd_start = $_GET["sd"];
+	$ymd_end = $_GET["ed"];
 
-?>
+	// $sql = "SELECT row_id, date_format(thidate,'%d-%m-%Y'), hn, ptname, organ, dx_mc_soldier, dr1_mc_soldier, dr2_mc_soldier, dr3_mc_soldier,address,thdatehn,rule FROM opd WHERE (dx_mc_soldier is not null AND dx_mc_soldier != '' ) ".$where." Order by  thidate ASC ";
+	// $sql = "
+	// SELECT a.row_id, date_format(a.thidate,'%d-%m-%Y'), a.hn, a.ptname, a.organ, a.dx_mc_soldier, a.dr1_mc_soldier, a.dr2_mc_soldier, a.dr3_mc_soldier, a.address, a.thdatehn, a.rule 
+	// , b.idcard
+	// FROM opd AS a
+	// LEFT JOIN opcard AS b ON b.hn = a.hn 
+	// WHERE (
+	// 	( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%ทหาร%' AND a.organ LIKE '%เกณ%' ) 
+	// 	OR ( a.organ LIKE '%รับรอง%' AND a.organ LIKE '%เลือกทหาร%' ) 
+	// 	OR a.toborow like 'EX30%'
+	// ) 
+	// AND dx_mc_soldier IS NOT NULL 
+	// AND dx_mc_soldier != ''
+	// ".$where." 
+	// ORDER BY thidate ASC ";
+
+	$sql = "SELECT b.`row_id`,b.`hn`,b.`ptname`,b.`thdatehn`,b.`organ`,b.`dx_mc_soldier`,b.`dr1_mc_soldier`,b.`dr2_mc_soldier`,b.`dr3_mc_soldier`,b.`rule`,
+	CONCAT(c.`address`,' ',c.`tambol`,' ',c.`ampur`,' ',c.`changwat`) AS `address`,
+	CONCAT(SUBSTRING(b.`thidate`,9,2),'-',SUBSTRING(b.`thidate`,6,2),'-',SUBSTRING(b.`thidate`,1,4)) AS `date`,
+	c.`idcard`
+	FROM 
+	(
+		SELECT MAX(`row_id`) AS `opd_id`
+		FROM `opd` 
+		WHERE `thidate` >= '$ymd_start' AND `thidate` <= '$ymd_end' 
+		AND (
+			( `organ` LIKE '%รับรอง%' AND `organ` LIKE '%งดเกณ%' )
+			OR `toborow` LIKE 'EX30%' 
+		)
+		GROUP BY `hn`
+	) AS a 
+	LEFT JOIN `opd` AS b ON b.`row_id` = a.`opd_id` 
+	LEFT JOIN `opcard` AS c ON c.`hn` = b.`hn`";
+
+	$result = mysql_query($sql) or die("Query failed ".mysql_error());
+	while ( $item = mysql_fetch_assoc($result)) 
+	{
+		$row_id = $item['opd_id'];
+		$date = $item['date'];
+		$hn = $item['hn'];
+		$ptname = $item['ptname'];
+		$organ = $item['organ'];
+		$dx_mc_soldier = $item['dx_mc_soldier'];
+		$dr1_mc_soldier = $item['dr1_mc_soldier'];
+		$dr2_mc_soldier = $item['dr2_mc_soldier'];
+		$dr3_mc_soldier = $item['dr3_mc_soldier'];
+		$address1 = $item['address'];
+		$thdatehn = $item['thdatehn'];
+		$rule = $item['rule'];
+		$idcard = $item['idcard'];
+
+		if( empty($dx_mc_soldier) ){
+			continue;
+		}
+
+		$Total =$Total+$amount; 
+		list($address) = mysql_fetch_row(mysql_query("Select concat(address,' ', tambol,' ',  ampur,' ',  changwat  ) From opcard where hn = '".$hn."' limit 0,1 "));
+		$thdatehn=substr($thdatehn,0,10);
+		
+		$num++;
+		$dr1 = preg_replace('/MD\d+\s/', '', $dr1_mc_soldier);
+		$dr2 = preg_replace('/MD\d+\s/', '', $dr2_mc_soldier);
+		$dr3 = preg_replace('/MD\d+\s/', '', $dr3_mc_soldier);
+		
+		?>
+		<tr class="font_tr">
+			<td align="center"><?php echo $num;?></td>
+			<td><?php echo $ptname;?><br><?php echo thaiNumber($idcard);?></td>
+			<td>
+				<?php 
+				echo thaiNumber($dx_mc_soldier);
+				?>
+			</td>
+			<td><?php echo thaiNumber($rule);?></td>
+			<td><?php echo $dr1."<br>".$dr2."<br>".$dr3;?></td>
+			<td><?php echo thaiNumber($address1);?></td>
+			<td><?php echo thaiNumber($thdatehn);?></td>
+		</tr>
+		<?php
+	}
+
+	include("unconnect.inc");
+
+	?>
+</tbody>
 </table>
