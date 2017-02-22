a4668c7421a7280ea1036a5e3e7b192952f00ab8
kritsanasak kuntaros <roboconk@gmail.com>
2017-02-22 20:35:52 +0700

    naaaaa

--- surasak3/Backup-Code-2015/06-04/nameseek.php
+++
@@ -1,4 +0,0 @@
-<frameset rows="0%,100%">
-  <frame name="top" src="" scrolling="no">
-  <frame name="bottom" src="opname.php" scrolling="auto">
-</frameset>
--- surasak3/Backup-Code-2015/06-04/namesname.php
+++
@@ -1,4 +0,0 @@
-<frameset rows="0%,100%">
-  <frame name="top" src="" scrolling="no">
-  <frame name="bottom" src="opnameops.php" scrolling="auto">
-</frameset>
