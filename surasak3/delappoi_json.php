bcd7d0cb581f64b22a72d1f33110977228903ec8
kritsanasak kuntaros <roboconk@gmail.com>
2017-05-25 15:46:39 +0700

    update

--- surasak3/delappoi_json.php
+++ surasak3/delappoi_json.php
@@ -2,7 +2,13 @@
 include_once 'includes/JSON.php';
 $json = new Services_JSON();
 
-$appoint_delete = array('id' => $cRow, 'apptime' => urlencode('ยกเลิกการนัด'));
+$sql = "SELECT `hn` FROM `appoint` WHERE `row_id` = '$cRow' ";
+$q = mysql_query($sql);
+$item = mysql_fetch_assoc($q);
+
+$cHn = $item['hn'];
+
+$appoint_delete = array('id' => $cRow, 'hn' => $cHn, 'apptime' => urlencode('ยกเลิกการนัด'));
 $output = $json->encode($appoint_delete);
 
 file_put_contents('syncfile/appoint/update/'.$cHn.'.json', $output);
