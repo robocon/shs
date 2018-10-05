3553d195dd0e435264829e53c3b1c12d97397702
kritsanasak kuntaros <roboconk@gmail.com>
2018-10-05 16:08:14 +0700

    ลองขยายช่อง

--- surasak3/med_record_print_pdf.php
+++ surasak3/med_record_print_pdf.php
@@ -52,10 +52,10 @@ class MedSHS extends SHSPdf{
         $this->Cell(110, 6, '', 0, 1);
 
         $this->SetFont('THSarabun','B',14);
-        $this->SetXY(20, ( $header_y + 6 ));
+        $this->SetXY(10, ( $header_y + 6 ));
         $this->Cell(52, 12, 'ชื่อยา ขนาด วิธีใช้', 1, 1, 'C');
         
-        $this->SetXY(72, ( $header_y + 6 ));
+        $this->SetXY(62, ( $header_y + 6 ));
         $this->Cell(15, 12, 'เวลา', 1, 1, 'C');
 
         // 
@@ -188,7 +188,7 @@ foreach ($drug_lists as $drug_code) {
         $drug_y = $get_header_y;
     }
 
-    $pdf->SetX(20);
+    $pdf->SetX(10);
     
     // หาความสูงของตัว Multicel ที่เป็นข้อความ
     $message = $d['tradname'].'('.$d['slcode'].')'."\n\r".$detail_txt."$drug_y   $tr_height";
@@ -203,7 +203,7 @@ foreach ($drug_lists as $drug_code) {
 
     // x y w h
     // เป็นกรอบข้อความยา
-    $pdf->Rect(20, $drug_y, 52, ($tr_height * 6));
+    $pdf->Rect(10, $drug_y, 52, ($tr_height * 6));
     $pdf->MultiCell(52, 6, $d['tradname'].'('.$d['slcode'].')'."\n\r".$detail_txt, 0);
 
     ////// 
@@ -239,14 +239,14 @@ if( $footer_h > 240 ){
     $footer_h = $get_header_y;
 }
 
-$pdf->SetXY(20, $footer_h);
+$pdf->SetXY(10, $footer_h);
 $pdf->Cell(52, 12, 'Recheck order', 1, 1, 'C');
-$pdf->SetXY(72, $footer_h);
+$pdf->SetXY(62, $footer_h);
 $pdf->Cell(123, 12, 'ผู้ตรวจสอบ', 1, 1, 'C');
 
-$pdf->SetXY(20, ($footer_h + 12));
+$pdf->SetXY(10, ($footer_h + 12));
 $pdf->Cell(52, 6, 'เวรเช้า', 1, 1, 'C');
-$pdf->Rect(72, ($footer_h + 12), 15, 6);
+$pdf->Rect(62, ($footer_h + 12), 15, 6);
 $pdf->Rect(87, ($footer_h + 12), 18, 6);
 $pdf->Rect(105, ($footer_h + 12), 18, 6);
 $pdf->Rect(123, ($footer_h + 12), 18, 6);
@@ -254,9 +254,9 @@ $pdf->Rect(141, ($footer_h + 12), 18, 6);
 $pdf->Rect(159, ($footer_h + 12), 18, 6);
 $pdf->Rect(177, ($footer_h + 12), 18, 6);
 
-$pdf->SetXY(20, ($footer_h + 18));
+$pdf->SetXY(10, ($footer_h + 18));
 $pdf->Cell(52, 6, 'เวรบ่าย', 1, 1, 'C');
-$pdf->Rect(72, ($footer_h + 18), 15, 6);
+$pdf->Rect(62, ($footer_h + 18), 15, 6);
 $pdf->Rect(87, ($footer_h + 18), 18, 6);
 $pdf->Rect(105, ($footer_h + 18), 18, 6);
 $pdf->Rect(123, ($footer_h + 18), 18, 6);
@@ -264,9 +264,9 @@ $pdf->Rect(141, ($footer_h + 18), 18, 6);
 $pdf->Rect(159, ($footer_h + 18), 18, 6);
 $pdf->Rect(177, ($footer_h + 18), 18, 6);
 
-$pdf->SetXY(20, ($footer_h + 24));
+$pdf->SetXY(10, ($footer_h + 24));
 $pdf->Cell(52, 6, 'เวรดึก', 1, 1, 'C');
-$pdf->Rect(72, ($footer_h + 24), 15, 6);
+$pdf->Rect(62, ($footer_h + 24), 15, 6);
 $pdf->Rect(87, ($footer_h + 24), 18, 6);
 $pdf->Rect(105, ($footer_h + 24), 18, 6);
 $pdf->Rect(123, ($footer_h + 24), 18, 6);
@@ -276,249 +276,3 @@ $pdf->Rect(177, ($footer_h + 24), 18, 6);
 
 // $pdf->AutoPrint(true);
 $pdf->Output();
-
-exit;
-
-
-
-?>
-<style>
-*{
-    margin: 0;
-}
-.clearfix::after{
-    content: "";
-    clear: both;
-    display: table;
-}
-
-/* ตาราง */
-.chk_table{
-    border-collapse: collapse;
-}
-
-.chk_table, th, td{
-    border: 1px solid black;
-}
-.chk_table th{
-    /* font-size: 16pt; */
-}
-.chk_table th,
-.chk_table td{
-    padding: 3px;
-}
-
-#main_page{
-    font-family: TH Sarabun New, TH SarabunPSK;
-    font-size: 16pt;
-    /* padding: 15mm; */
-}
-.time-get th{
-    width: 10mm;
-    height: 7mm;
-}
-.td_detail{
-    height: 7mm;
-    font-size: 16pt;
-}
-
-@media print{
-    .page_header{
-        height: auto; 
-        /* position: fixed;  */
-        width: 100%;
-    }
-    .page_body,
-    .new_table_page{
-        
-    }
-}
-
-</style>
-<!-- width: 8.3in; height: 11.7in; -->
-<div id="main_page" style="">
-
-    <div class="page_header" style="" class="clearfix">
-        <div style="width: 35%; height: 45mm; float: left; text-align: center;">
-            <p style="font-size: 20pt;"><b>โรงพยาบาลค่ายสุรศักดิ์มนตรี</b></p>
-            <p><b>แบบบันทึกการให้ยา</b></p>
-            <p style="font-size: 18pt;"><b><u><?=$_POST['type'];?></u></b></p>
-        </div>
-        <div style="width: 65%; float: right;">
-            <b>ชื่อ/สกุล ผู้ป่วย: </b><?=$user['ptname'];?> <b>อายุ: </b><?=$user['age'];?><br>
-            <b>HN: </b><?=$user['hn'];?> <b>AN: </b><?=$user['an'];?> <b>WARD: </b><?=$ward_name;?><br>
-            <b>ROOM/BED: </b><?=$user['bed'];?> <b>Dx: </b><?=$user['diagnos'];?><br>
-            <b>สิทธ์: </b><?=$user['ptright'];?> <b>แพทย์: </b><?=$user['doctor'];?><br>
-            <?php
-            if( $react_txt !== '' ){
-                ?>
-                <span style="color: red;">
-                    <b><u>แพ้ยา:</u> </b><?=$react_txt;?>
-                </span>
-                <?php
-            }
-            ?>
-        </div>
-
-
-    </div>
-
-    <!-- clear fix for IE 8 -->
-    <div style="clear: both;"></div>
-
-    <div class="page_body" style="">
-        <table class="chk_table" width="100%">
-            <tr>
-                <th rowspan="2" style="font-size: 20px;">ชื่อยา ขนาด วิธีให้</th>
-
-                <?php
-                $current = strtotime($_POST['date_set']);
-
-                for ($i=1; $i <= 7; $i++) { 
-
-                    $year = date('Y', $current) + 543;
-                    $month = date('m', $current);
-                    $date = date('d', $current);
-                    ?>
-                    <th width="10%"><?=$date;?> <?=$def_month_th[$month];?> <?=$year;?></th>
-                    <?php
-                    $current = strtotime('+1 day', $current);
-                }
-                ?>
-                
-            </tr>
-            <tr class="time-get">
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-                <th>เวลา/ผู้ให้</th>
-            </tr>
-
-            <?php
-            $ii = 1;
-            foreach ($drug_lists as $drug_code) {
-
-                if ( $ii == 6 ) {
-                    
-                    ?>
-                    </table>
-                    </div>
-
-                    <div style="page-break-before: always;"></div>
-
-                    <div class="page_body">
-                    <table class="chk_table" width="100%" style="padding-top: 45mm;">
-                    <?php
-                    
-                }
-                
-                $ii++;
-
-                $sql = "SELECT `drugcode`,`date`,`tradname`,`unit`,`slcode`,`amount`,`onoff`,`dateoff`,`row_id` 
-                FROM `dgprofile` 
-                WHERE `an` = '$cAn' 
-                AND `drugcode` = '$drug_code' ";
-                $db->select($sql);
-                $d = $db->get_item();
-
-                $def_drug_h = $drug_height[$drug_code]['0'];
-                $tr_height = $def_drug_h + 1;
-
-                $slcode = $d['slcode'];
-
-                $sql = "SELECT * 
-                FROM `drugslip` 
-                WHERE `slcode` = '$slcode' ";
-                $db->select($sql);
-                $dSlip = $db->get_item();
-
-                $detail_txt = $dSlip['detail1'].'<br>';
-                $detail_txt .= $dSlip['detail2'].'<br>';
-                $detail_txt .= $dSlip['detail3'].'<br>';
-                $detail_txt .= $dSlip['detail4'];
-
-                ?>
-                <tr class="td_detail">
-                    <td rowspan="<?=$tr_height;?>" style="vertical-align: top; font-size: 16pt;">
-                        <?=$d['tradname'];?>&nbsp;&nbsp;( <?=$d['slcode'];?> )<br>
-                        <?=$detail_txt;?>
-                    </td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                    <td width="10%"></td>
-                </tr>
-                <?php
-                for ($i=0; $i < $def_drug_h; $i++) { 
-                    ?>
-                    <tr class="td_detail">
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <?php
-                }
-            }
-            ?>
-            <tr class="td_detail">
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr class="td_detail">
-                <td align="center"><b>Recheck order</b></td>
-                <td colspan="10" align="center"><b>ผู้ตรวจสอบ</b></td>
-            </tr>
-            <tr class="td_detail">
-                <td  align="center"><b>เวรเช้า</b></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr class="td_detail">
-                <td  align="center"><b>เวรบ่าย</b></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr class="td_detail">
-                <td  align="center"><b>เวรดึก</b></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </table>
-    </div>
-</div>
-<script>
-window.onload = function(){
-    // window.print();
-};
-</script>
