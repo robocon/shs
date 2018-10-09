213f19a0ab34c688dd02b0b19d4d53ce9bc50a30
Kritsanasak Kuntaros <rooconk@gmail.com>
2018-10-09 07:55:07 +0700

    ลองกับ Opday

--- surasak3/rdu_convert.php
+++ surasak3/rdu_convert.php
@@ -1,6 +1,6 @@
 <?php 
 // just for testing
-
+set_time_limit(0);
 // include 'bootstrap.php';
 
 function dump($txt){
@@ -9,90 +9,63 @@ function dump($txt){
     echo "</pre>";
 }
 
-$db = mysql_connect('192.168.1.13', 'dottwo', '') or die( mysql_error() );
+$db = mysql_connect('localhost', 'root', '1234') or die( mysql_error() );
 mysql_select_db('smdb', $db) or die( mysql_error() );
 
-// mysql_query('SET NAMES TIS620', $db);
+mysql_query('SET NAMES TIS620', $db);
+
+$date_start = '2560-10-01';
+$date_end = '2560-12-31';
+$quarter = 1;
+
+$dirPath = realpath(dirname(__FILE__))."/rdu";
+$filePath = $dirPath.'/'.$date_start.'_'.$date_end.'.sql';
 
 /**
  * @todo 
- * function �ѹ� local �ջѭ�� string tis �Ѻ utf
+ * function รันใน local มีปัญหา string tis กับ utf
  */
 $sql = "SELECT a.`row_id`,a.`thidate`,a.`hn`,a.`ptname`,a.`age`,a.`diag`,a.`icd10`,a.`doctor`, 
 CONCAT(SUBSTRING(a.`thidate`,1,10),a.`hn`) AS `date_hn` 
 FROM `opday` AS a 
-WHERE a.`thidate` >= '2560-10-01 00:00:00' AND a.`thidate` <= '2560-12-31 23:59:59' 
+WHERE a.`thidate` >= '$date_start 00:00:00' AND a.`thidate` <= '$date_end 23:59:59' 
 AND a.`an` IS NULL 
-AND ( a.`icd10` <> '' AND a.`icd10` IS NOT NULL ) ";
+AND ( a.`icd10` <> '' AND a.`icd10` IS NOT NULL ) 
+AND a.`doctor` <> '' ";
 $q = mysql_query($sql, $db) or die( mysql_error() );
 
-$opday_list = array();
+
+$sql_header = "INSERT INTO `opday` ( `id`,`row_id`,`date`,`hn`,`ptname`,`gender`,`age`,`diag`,`icd10`,`doctor`,`date_hn`,`date_generate`,`quarter`) VALUES ";
+$sql_data_list = '';
+
 while ( $item = mysql_fetch_assoc($q) ) {
-    // dump($item);
-    // $opday_list[] = $item;
 
     $row_id = $item['row_id'];
     $thidate = $item['thidate'];
     $hn = $item['hn'];
     $ptname = $item['ptname'];
-    $yot = $item['yot'];
     $age = $item['age'];
-    $diag = $item['diag'];
+    $diag = htmlspecialchars($item['diag'], ENT_QUOTES);
     $icd10 = $item['icd10'];
     $doctor = $item['doctor'];
     $date_hn = $item['date_hn'];
 
+    // dump($ptname);
+    $match = preg_match('/(หญิง|นาง|น.ส|ด.ญ|ms|mis)/', iconv('TIS620','UTF-8',$ptname), $matchs);
+    // dump($match);
+    $gender = 'm';
+    if( $match > 0 ){
+        $gender = 'f';
+    }
 
-    $match = preg_match('/[˭ԧ|�ҧ|�.�|�.�|ms|mis]/', $yot, $matchs);
-    dump($match);
-
-    $sql = "INSERT INTO `opday` ( 
-        `id`,`row_id`,`date`,`hn`,`ptname`,
-        `gender`,`age`,`diag`,`icd10`,`doctor`,
-        `date_hn`,`date_generate`
-    ) VALUES ( 
-        NULL,'$row_id','$thidate','$hn','$ptname',
-        '$sex','$age','$diag','$icd10','$doctor',
-        '$date_hn',NOW()
-    );";
-    // dump($sql);
+    $sql_data_list = $sql_header."( NULL,'$row_id','$thidate','$hn','$ptname','$gender','$age','$diag','$icd10','$doctor','$date_hn',NOW(),'$quarter');\n";
+    
+    // dump($sql_data_list);
+    
+    file_put_contents($filePath, $sql_data_list, FILE_APPEND);
 
 }
 
 mysql_close($db);
 
-
-exit;
-
-
-$db_rdu = mysql_connect('localhost', 'root', '1234') or die( mysql_error() );
-mysql_select_db('rdu', $db_rdu) or die( mysql_error() );
-
-foreach ($opday_list as $key => $item) { 
-
-    $row_id = $item['row_id'];
-    $thidate = $item['thidate'];
-    $hn = $item['hn'];
-    $ptname = $item['ptname'];
-    $sex = $item['sex'];
-    $age = $item['age'];
-    $diag = $item['diag'];
-    $icd10 = $item['icd10'];
-    $doctor = $item['doctor'];
-    $date_hn = $item['date_hn'];
-    
-
-    $sql = "INSERT INTO `opday` ( 
-        `id`,`row_id`,`date`,`hn`,`ptname`,
-        `gender`,`age`,`diag`,`icd10`,`doctor`,
-        `date_hn`,`date_generate`
-    ) VALUES ( 
-        NULL,'$row_id','$thidate','$hn','$ptname',
-        '$sex','$age','$diag','$icd10','$doctor',
-        '$date_hn',NOW()
-    )";
-    dump($sql);
-    // mysql_query($sql,$db_rdu) or die( mysql_error() );
-
-
-}
+echo "Success";
