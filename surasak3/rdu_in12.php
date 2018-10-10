8c11fcb6b6fb92026e5c0d535fa2b8db3c4b5177
Kritsanasak Kuntaros <rooconk@gmail.com>
2018-10-10 23:52:01 +0700

    ปรับ ตัวชี้วัดที่ 11 และ 12

--- surasak3/rdu_in12.php
+++ surasak3/rdu_in12.php
@@ -9,7 +9,7 @@ $en_date_min = bc_to_ad($date_min);
 $en_date_max = bc_to_ad($date_max);
 
 // lab ย้อนหลัง 6 เดือน
-$db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in12_lab`");
+// $db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in12_lab`");
 $sql = "CREATE TEMPORARY TABLE `pre_in12_lab` 
 SELECT b.`autonumber`,b.`orderdate`,b.`hn`,b.`patientname`,b.`sex`,c.`result`
 FROM (  
@@ -24,11 +24,11 @@ FROM (
 LEFT JOIN `resulthead` AS b ON b.`autonumber` = a.`latest_id` 
 LEFT JOIN `resultdetail` AS c ON c.`autonumber` = a.`latest_id` 
 WHERE c.`labname` = 'Creatinine' ";
-$db->select($sql);
+// $db->select($sql);
 
 // หา eGFR
-$db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in11_b`");
-$sql = "CREATE TEMPORARY TABLE `pre_in11_b` 
+// $db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in12_b`");
+$sql = "CREATE TEMPORARY TABLE `pre_in12_b` 
 SELECT a.`shortage`,a.`datehn`,b.*, eGFR(a.`shortage`,b.`sex`,b.`result`) AS `eGFR`
 FROM ( 
 	
@@ -43,10 +43,25 @@ FROM (
 LEFT JOIN `pre_in12_lab` AS b ON b.`hn` = a.`hn`
 WHERE b.`autonumber` IS NOT NULL 
 HAVING eGFR(a.`shortage`,b.`sex`,b.`result`) > 30 ";
-$db->select($sql);
+// $db->select($sql);
+
+$db->exec("DROP TEMPORARY TABLE IF EXISTS `test_in12`");
+$sql = "CREATE TEMPORARY TABLE `test_in12` 
+SELECT a.`row_id`,a.`hn`,a.`date_hn`,a.`icd10`,b.`egfr` 
+FROM ( 
+	SELECT * FROM `opday` WHERE `quarter` = '$quarter' AND `icd10` regexp 'E11' 
+) AS a 
+LEFT JOIN ( 
+	SELECT * FROM `lab` WHERE `quarter` = '$quarter' AND `egfr` > 30 
+) AS b ON b.`hn` = a.`hn` 
+WHERE b.`autonumber` IS NOT NULL  ";
+// dump($sql);
+
+// exit;
+$db->exec($sql);
 
 // Table B
-$sql = "SELECT COUNT(`autonumber`) AS `rows` FROM `pre_in11_b`";
+$sql = "SELECT COUNT(`row_id`) AS `rows` FROM `test_in12`";
 $db->select($sql);
 $pre_in12b = $db->get_item();
 // $in12b = count($pre_in12b );
@@ -55,12 +70,9 @@ $in12b = $pre_in12b['rows'];
 // Table A
 $sql = "SELECT COUNT(a.`row_id`) AS `rows` 
 FROM ( 
-    SELECT `row_id`,`date`,`hn`,`drugcode`,
-    CONCAT(SUBSTRING(`date`,1,10),`hn`) AS `datehn`
+    SELECT `row_id`,`date`,`hn`,`drugcode`,`date_hn`
     FROM `drugrx` 
-    WHERE ( `date` >= '$date_min' AND `date` <= '$date_max' ) 
-    AND `status` = 'Y' 
-    AND `an` IS NULL 
+    WHERE `quarter` = '$quarter' 
     AND `drugcode` IN ( 
         '1MET500-C', 
         '1METF', 
@@ -72,7 +84,7 @@ FROM (
     ) 
     GROUP BY `hn` 
 ) AS a 
-RIGHT JOIN `pre_in11_b` AS b ON b.`datehn` = a.`datehn` 
+RIGHT JOIN test_in12 AS b ON b.`date_hn` = a.`date_hn` 
 WHERE a.`row_id` IS NOT NULL ";
 $db->select($sql);
 $pre_in12a = $db->get_item();
