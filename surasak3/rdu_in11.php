7d0198767c9ef9abf8ac1b67513ddaa91679f4d8
kritsanasak kuntaros <roboconk@gmail.com>
2018-10-16 16:02:58 +0700

    ปรับ เงื่อนไขในตัวชี้วัดที่ 11 12 ลองปรับ เงื่อนไขในการเลือกข้อมูล eGFR

--- surasak3/rdu_in11.php
+++ surasak3/rdu_in11.php
@@ -9,7 +9,7 @@ if ( !defined('RDU_TEST') ) {
 // $en_date_max = bc_to_ad($date_max);
 
 // เตรียมข้อมูล opday
-$db->exec("DROP TEMPORARY TABLE IF EXISTS `pre_opday_in11`");
+$db->exec("DROP TEMPORARY TABLE IF EXISTS `pre_opday_in11`;");
 $sql = "CREATE TEMPORARY TABLE `pre_opday_in11` 
 SELECT `row_id`,`date`,`hn`,`age`,`icd10`,`date_hn`,TRIM(SUBSTRING(`age`, 1, 2)) AS `shortage`
 FROM `opday` 
@@ -20,7 +20,7 @@ GROUP BY `hn` ";
 $db->exec($sql); 
 
 // เตรียมข้อมูล drugrx
-$db->exec("DROP TEMPORARY TABLE IF EXISTS `pre_drugrx_in11`");
+$db->exec("DROP TEMPORARY TABLE IF EXISTS `pre_drugrx_in11`;");
 $sql = "CREATE TEMPORARY TABLE `pre_drugrx_in11` 
 SELECT `row_id`,`hn`,`drugcode`,`date_hn` 
 FROM `drugrx` 
@@ -31,9 +31,9 @@ GROUP BY `hn` ";
 $test = $db->exec($sql); 
 
 // เอาสองตัวบนมารวมกัน จะได้ ผู้ป่วยที่ได้รับยา gibenclamide แบบที่ยังไม่ได้แบ่งตามอายุ
-$db->exec("DROP TEMPORARY TABLE IF EXISTS `tmp_user_in11`");
+$db->exec("DROP TEMPORARY TABLE IF EXISTS `tmp_user_in11`;");
 $sql = "CREATE TEMPORARY TABLE `tmp_user_in11` 
-SELECT a.*,CONCAT( (SUBSTRING(a.`date`, 1, 4) - 543), SUBSTRING(a.`date`,5,6), a.`hn` ) AS `date_hn2`,b.`drugcode` 
+SELECT a.*,CONCAT( (SUBSTRING(a.`date`, 1, 4) - 543), SUBSTRING(a.`date`,5,6) ) AS `date_en`,b.`drugcode` 
 FROM `pre_opday_in11` AS a 
 LEFT JOIN `pre_drugrx_in11` AS b ON b.`date_hn` = a.`date_hn` 
 WHERE b.`hn` IS NOT NULL "; 
@@ -42,10 +42,9 @@ $db->exec($sql);
 
 // เตรียมหา A2 จากผลแลปครั้งล่าสุด
 // $db->exec("DROP TEMPORARY TABLE IF EXISTS `tmp_prelab_in11`");
-$sql = "CREATE TEMPORARY TABLE `tmp_prelab_in11` 
-SELECT *,COUNT(b.`autonumer`) AS `rows`, TIMESTAMPDIFF(MONTH,a.`date_en`,SUBSTRING(b.`orderdate`,1,10)) AS `month_diff`
+$sql = "SELECT COUNT(a.`row_id`) AS `rows` 
 FROM (  
-    SELECT `hn`,`date_en`,`shortage` 
+    SELECT `row_id`,`hn`,`date_en`,`shortage` 
     FROM `tmp_user_in11` 
     WHERE `shortage` <= 65 
 ) AS a 
@@ -53,11 +52,13 @@ LEFT JOIN (
     SELECT * 
     FROM `lab` 
     WHERE `quarter` = '1' 
-    AND `egfr` < 60 
+    AND ( `egfr` < 60 AND `egfr` > 0 ) 
 ) AS b ON b.`hn` = a.`hn` 
 WHERE b.`id` IS NOT NULL 
-AND TIMESTAMPDIFF(MONTH,a.`date_en`,SUBSTRING(b.`orderdate`,1,10)) <= 6 
-";
+AND ( 
+	TIMESTAMPDIFF(MONTH,SUBSTRING(b.`orderdate`,1,10),a.`date_en`) >= 0 
+	AND TIMESTAMPDIFF(MONTH,SUBSTRING(b.`orderdate`,1,10),a.`date_en`) <= 6 
+)";
 // dump($sql);
 // $db->exec($sql); 
 $db->select($sql); 
@@ -65,24 +66,14 @@ $pre_a2 = $db->get_item();
 
 
 
-// A2 คำนวณ eGFR ที่น้อยกว่า 60 และย้อนหลังไม่เกิน 6เดือน
-// $sql = "SELECT COUNT(d.`hn`) AS `rows` 
-// FROM tmp_prelab_in11 AS c 
-// LEFT JOIN `resulthead` AS d ON d.`autonumber` = c.`latest_id` 
-// LEFT JOIN `resultdetail` AS e ON e.`autonumber` = c.`latest_id`
-// WHERE e.`labname` = 'Creatinine' 
-// AND eGFR(c.`shortage`,d.`sex`,e.`result`) < 60 
-// AND TIMESTAMPDIFF(MONTH,d.`orderdate`,c.`opd_date`) <= 6 ";
-// $db->select($sql); 
-// $pre_a2 = $db->get_item();
-
-
 // A1
-$sql = "SELECT `hn`,`date_en`,`shortage`,COUNT(`row_id`)
+$sql = "SELECT COUNT(`row_id`) AS `rows`  
 FROM `tmp_user_in11` 
 WHERE `shortage` > 65 ;";
+// dump($sql);
 $db->select($sql);
 $pre_a1 = $db->get_item();
+
 
 $in11a = $pre_a1['rows'] + $pre_a2['rows'];
 
--- surasak3/rdu_in12.php
+++ surasak3/rdu_in12.php
@@ -10,46 +10,46 @@ $en_date_max = bc_to_ad($date_max);
 
 // lab ย้อนหลัง 6 เดือน
 // $db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in12_lab`");
-$sql = "CREATE TEMPORARY TABLE `pre_in12_lab` 
-SELECT b.`autonumber`,b.`orderdate`,b.`hn`,b.`patientname`,b.`sex`,c.`result`
-FROM (  
+// $sql = "CREATE TEMPORARY TABLE `pre_in12_lab` 
+// SELECT b.`autonumber`,b.`orderdate`,b.`hn`,b.`patientname`,b.`sex`,c.`result`
+// FROM (  
 
-	SELECT MAX(`autonumber`) as `latest_id`
-	FROM `resulthead` 
-	WHERE (`orderdate` >= '$en_date_min' AND `orderdate` <= '$en_date_max' ) 
-	AND ( `profilecode` = 'CREA' OR `profilecode` = 'CREAG' ) 
-	GROUP BY `hn` 
+// 	SELECT MAX(`autonumber`) as `latest_id`
+// 	FROM `resulthead` 
+// 	WHERE (`orderdate` >= '$en_date_min' AND `orderdate` <= '$en_date_max' ) 
+// 	AND ( `profilecode` = 'CREA' OR `profilecode` = 'CREAG' ) 
+// 	GROUP BY `hn` 
 
-) AS a 
-LEFT JOIN `resulthead` AS b ON b.`autonumber` = a.`latest_id` 
-LEFT JOIN `resultdetail` AS c ON c.`autonumber` = a.`latest_id` 
-WHERE c.`labname` = 'Creatinine' ";
+// ) AS a 
+// LEFT JOIN `resulthead` AS b ON b.`autonumber` = a.`latest_id` 
+// LEFT JOIN `resultdetail` AS c ON c.`autonumber` = a.`latest_id` 
+// WHERE c.`labname` = 'Creatinine' ";
 // $db->select($sql);
 
 // หา eGFR
 // $db->select("DROP TEMPORARY TABLE IF EXISTS `pre_in12_b`");
-$sql = "CREATE TEMPORARY TABLE `pre_in12_b` 
-SELECT a.`shortage`,a.`datehn`,b.*, eGFR(a.`shortage`,b.`sex`,b.`result`) AS `eGFR`
-FROM ( 
+// $sql = "CREATE TEMPORARY TABLE `pre_in12_b` 
+// SELECT a.`shortage`,a.`datehn`,b.*, eGFR(a.`shortage`,b.`sex`,b.`result`) AS `eGFR`
+// FROM ( 
 	
-	SELECT `hn`,SUBSTRING(`age`,1,2) AS `shortage`, 
-	SUBSTRING(`thidate`,1,10) AS `date`, 
-	CONCAT(SUBSTRING(`thidate`,1,10),`hn`) AS `datehn` 
-	FROM `opday` 
-	WHERE ( `thidate` >= '$date_min' AND `thidate` <= '$date_max' ) 
-	AND `icd10` regexp 'E11' 	
+// 	SELECT `hn`,SUBSTRING(`age`,1,2) AS `shortage`, 
+// 	SUBSTRING(`thidate`,1,10) AS `date`, 
+// 	CONCAT(SUBSTRING(`thidate`,1,10),`hn`) AS `datehn` 
+// 	FROM `opday` 
+// 	WHERE ( `thidate` >= '$date_min' AND `thidate` <= '$date_max' ) 
+// 	AND `icd10` regexp 'E11' 	
 
-) AS a 
-LEFT JOIN `pre_in12_lab` AS b ON b.`hn` = a.`hn`
-WHERE b.`autonumber` IS NOT NULL 
-HAVING eGFR(a.`shortage`,b.`sex`,b.`result`) > 30 ";
+// ) AS a 
+// LEFT JOIN `pre_in12_lab` AS b ON b.`hn` = a.`hn`
+// WHERE b.`autonumber` IS NOT NULL 
+// HAVING eGFR(a.`shortage`,b.`sex`,b.`result`) > 30 ";
 // $db->select($sql);
 
 $db->exec("DROP TEMPORARY TABLE IF EXISTS `test_in12`");
 $sql = "CREATE TEMPORARY TABLE `test_in12` 
 SELECT a.`row_id`,a.`hn`,a.`date_hn`,a.`icd10`,b.`egfr` 
 FROM ( 
-	SELECT * FROM `opday` WHERE `quarter` = '$quarter' AND `icd10` regexp 'E11' 
+	SELECT * FROM `opday` WHERE `quarter` = '$quarter' AND `icd10` regexp 'E11' GROUP BY `hn`
 ) AS a 
 LEFT JOIN ( 
 	SELECT * FROM `lab` WHERE `quarter` = '$quarter' AND `egfr` > 30 
