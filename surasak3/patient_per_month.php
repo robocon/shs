7143a46a622332c58d5a152deedaf15d670172af
kritsanasak kuntaros <roboconk@gmail.com>
2018-11-23 18:12:06 +0700

    เก็บ query พี่โย

--- surasak3/patient_per_month.php
+++ surasak3/patient_per_month.php
@@ -1,5 +1,6 @@
 <?php 
-
+// งานกายภาพ + แพทย์แผนไทย
+// @readme งานกายภาพจะเป็น `staf_massage` = '' แต่ถ้าเป็นแพทย์แผนไทยจะเป็น `staf_massage` <> '' 
 // WEEKDAY 0 => Monday 
 DROP TEMPORARY TABLE IF EXISTS `tmp_depart`; 
 CREATE TEMPORARY TABLE `tmp_depart` 
@@ -11,7 +12,7 @@ AND `cashok` <> ''
 AND `staf_massage` = '' 
 GROUP BY `hn`; 
 
-// วันธรรมดา
+// งานกายภาพ วันธรรมดา
 SELECT `only_date`, COUNT(`row_id`) AS `rows` 
 FROM `tmp_depart` 
 WHERE ( `day_in` >= 0 AND `day_in` <= 4 ) 
@@ -23,10 +24,49 @@ AND (
 GROUP BY `only_date` 
 ORDER BY row_id ASC 
 
-// เสาร์-อาทิตย์
+// งานกายภาพ เสาร์-อาทิตย์
 SELECT `only_date`, COUNT(`row_id`) AS `rows` 
 FROM `tmp_depart` 
 WHERE ( `day_in` >= 5 AND `day_in` <= 6 ) 
 AND ( `time` >= '08:00:00' AND `time` <= '16:00:00' ) 
+GROUP BY `only_date` 
+ORDER BY row_id ASC 
+
+
+// งานนวด ตอนเย็นวันจันทร์
+SELECT `only_date`, COUNT(`row_id`) AS `rows` 
+FROM `tmp_depart` 
+WHERE `day_in` = 0 
+AND ( `time` >= '16:00:00' AND `time` <= '20:00:00' )
+GROUP BY `only_date` 
+ORDER BY row_id ASC 
+
+// งานนวด ตอนเช้าวันเสาร์-อาทิตย์
+SELECT `only_date`, COUNT(`row_id`) AS `rows` 
+FROM `tmp_depart` 
+WHERE ( `day_in` >= 5 AND `day_in` <= 6 )
+AND ( `time` >= '08:00:00' AND `time` <= '16:00:00' )
+GROUP BY `only_date` 
+ORDER BY row_id ASC 
+
+
+// หมอขชล เย็น
+DROP TEMPORARY TABLE IF EXISTS `tmp_depart`; 
+CREATE TEMPORARY TABLE `tmp_depart` 
+SELECT *, SUBSTRING(`date`, 1, 10) AS `only_date`, 
+CONCAT(SUBSTRING(`date`, 1, 10), `hn`) AS `date_hn`, 
+SUBSTRING(`date`, 12, 8) AS `time`, 
+WEEKDAY(toEn(`date`)) AS `day_in`  
+FROM `depart` 
+WHERE `date` LIKE '2561-11%' 
+AND `doctor` LIKE 'MD101%'  
+AND `cashok` <> '' 
+AND `status` = 'Y' 
+GROUP BY CONCAT(SUBSTRING(`date`, 1, 10), `hn`); 
+
+
+SELECT `only_date`, COUNT(`row_id`) AS `rows` 
+FROM `tmp_depart` 
+WHERE ( `time` >= '16:00:00' AND `time` <= '24:00:00' ) 
 GROUP BY `only_date` 
 ORDER BY row_id ASC 
