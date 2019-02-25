a6eeb025809a44da66f7658b112eaff48a6a9c71
kritsanasak kuntaros <roboconk@gmail.com>
2019-02-25 10:21:13 +0700

    ส่งออก policy

--- surasak3/new43file/menu.php
+++ surasak3/new43file/menu.php
@@ -1,8 +1,8 @@
 <div>
     <a href="export_new43.php">หน้าหลัก 43แฟ้ม</a> | 
-    <a href="specialpp.php">SPECIALPP</a> | 
-    <a href="onlycharge_ipd.php">CHARGE_IPD</a> | 
-    <a href="onlycard.php">CARD</a> | 
+    <!-- <a href="specialpp.php">SPECIALPP</a> |  -->
+    <!-- <a href="onlycharge_ipd.php">CHARGE_IPD</a> | 
+    <a href="onlycard.php">CARD</a> |  -->
     <a href="popuckai.php">อัพเดทข้อมูลผู้ป่วยสิทธิ์ 30บาท</a> | 
     <!--
     <a href="diff_person.php">เปรียบเทียบข้อมูล person</a>
@@ -11,5 +11,5 @@
 
     <a href="pre_home.php" target="_blank">เตรียมข้อมูลแฟ้ม HOME</a> | 
     <br>
-    <a href="disability_andicf_form.php" target="_blank">จัดการข้อมูล ICF และ DISABILITY</a>
+    <!-- <a href="disability_andicf_form.php" target="_blank">จัดการข้อมูล ICF และ DISABILITY</a> -->
 </div>
