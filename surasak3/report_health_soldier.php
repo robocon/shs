387bda9da6b7c899052154da1fd0169c4b96e7de
kritsanasak kuntaros <roboconk@gmail.com>
2019-04-24 15:18:02 +0700

    ปรับการดึงข้อมูลในหน้ารายงาน

--- surasak3/report_health_soldier.php
+++ surasak3/report_health_soldier.php
@@ -12,6 +12,7 @@ $shs_configs = array(
 );
 $db = Mysql::load($shs_configs);
 
+// $db->select("SET NAMES UTF-8");
 /*
 $camp_lists = array(
 	'312600' => 'Ã¾.¤èÒÂÊØÃÈÑ¡´ÔìÁ¹µÃÕ',
@@ -28,59 +29,68 @@ $camp_lists = array(
 #a.`bs`, --> glu_result
 #a.`tg`, --> trig_result
 #
-$sql = "SELECT a.`row_id`,a.`hn`,a.`registerdate`,a.`camp`,a.`yot`,a.`ptname`,a.`idcard`,a.`birthday`,a.`age`,a.`gender`,
-a.`cigarette`,a.`alcohol`,a.`exercise`,a.`weight`,a.`height`,a.`waist`,a.`bp1`,
-a.`bp2`,a.`chol_result`,a.`chunyot`,a.`hdl_result`,a.`ldl_result`,a.`glu_result`,a.`trig_result`,a.`bmi`,
+
+/*
+¢Ò´
+yot
+idcard
+birthday
+gender
+waist
+ */
+
+
+$sql = "SELECT a.`row_id`,a.`hn`,a.`thidate`,a.`camp`,a.`ptname`,a.`age`,
+a.`cigarette`,a.`alcohol`,a.`exercise`,a.`weight`,a.`height`,a.`bp1`,
+a.`bp2`,a.`chol`,a.`chunyot1`,a.`hdl`,a.`ldl`,a.`bs`,a.`cr`,a.`bmi`,
+b.`idcard`,b.`dbirth`,a.`gender`,a.`round_`,
 (
     CASE 
-        WHEN a.`camp` LIKE '%Ã¾.¤èÒÂÊØÃÈÑ¡´ÔìÁ¹µÃÕ' THEN '312600' 
-        WHEN a.`camp` LIKE '%Á·º.32' THEN '312601' 
-        WHEN a.`camp` LIKE '%ÃéÍÂ.½Ã¾.3' THEN '312602' 
-        WHEN a.`camp` LIKE '%Ã.17 ¾Ñ¹.2' THEN '312603' 
-        WHEN a.`camp` LIKE '%ª.¾Ñ¹.4 ÃéÍÂ4' THEN '312604' 
-        WHEN a.`camp` LIKE '%Ê§.Ê´.¨Ç.Å.».' THEN '312605' 
-        WHEN a.`camp` LIKE '%¡·¾.33' THEN '312606' 
+        WHEN a.`camp` LIKE 'M04%' THEN '312600' 
+        WHEN a.`camp` LIKE 'M03%' THEN '312601' 
+        WHEN a.`camp` LIKE 'M06%' THEN '312602' 
+        WHEN a.`camp` LIKE 'M02%' THEN '312603' 
+        WHEN a.`camp` LIKE 'M05%' THEN '312604' 
+        WHEN a.`camp` LIKE 'M08%' THEN '312605' 
+        WHEN a.`camp` LIKE 'M010%' THEN '312606' 
     END
 ) AS `camp_code`
-FROM `armychkup` AS a 
-WHERE a.`yearchkup` = '61' 
+FROM `condxofyear_so` AS a 
+LEFT JOIN `opcard` AS b ON b.`hn` = a.`hn`
+WHERE a.`yearcheck` = '2562' 
 AND a.`camp` != '' 
-GROUP BY a.`hn`
+GROUP BY a.`hn` 
 ORDER BY `camp_code` ASC, a.`row_id` DESC";
 $db->select($sql);
 $items = $db->get_items();
 $new_itmes = array();
 foreach($items as $key => $item){
-	
-	// $sql = "SELECT * FROM condxofyear_so_temp WHERE `camp` LIKE '%$camp' ORDER BY `row_id` ASC ";
-	// $db->select($sql);
-	// $items = $db->get_items();
 
-	// dump(count($items));
+	list($preYot,$preName,$preSurname) = explode(' ', $item['ptname']);
+	$item['yot'] = $preYot;
+	$item['ptname'] = "$preName $preSurname";
+
+	$item['birthday'] = $item['dbirth'];
 
 	$yot1 = array('¨ÍÁ¾Å','¾Å.Í.','¾Å.·.','¾Å.µ.','¾Å.¨.','¾.Í.','¾.·.','¾.µ.','Ã.Í.','Ã.·.','Ã.µ.','¾ÅµÃÕ');
 	$yot2 = array('¨.Ê.Í.','¨.Ê.Í .','¨.Ê.·.','¨.Ê.µ.','Ê.Í.','Ê.Í','Ê.·.','Ê.µ.');
 	$yot3 = array('¹ÒÂ','¹Ò§ÊÒÇ','¹.Ê.','¹Ò§');
 	$yot4 = array('¾ÅÍÒÊÒ','¾ÅÍÒÊÒÊÁÑ¤Ã');
 	
-	// foreach ($items as $camp_key => $value) {
-		// $item['camp_code'] = $key;
-		
-		$test_yot = trim(str_replace(array('Ë­Ô§','(¾)'), '', $item['yot']));
-		
-		if( in_array($test_yot, $yot1) ){
-			$item['rankgroup'] = 1;
-		}else if(in_array($test_yot, $yot2)){
-			$item['rankgroup'] = 2;
-		}else if(in_array($test_yot, $yot3)){
-			$item['rankgroup'] = 3;
-		}else{
-			$item['rankgroup'] = 4;
-		}
-		
-		$new_itmes[] = $item;
-		
-	// }
+	$test_yot = trim(str_replace(array('Ë­Ô§','(¾)'), '', $item['yot']));
+	
+	if( in_array($test_yot, $yot1) ){
+		$item['rankgroup'] = 1;
+	}else if(in_array($test_yot, $yot2)){
+		$item['rankgroup'] = 2;
+	}else if(in_array($test_yot, $yot3)){
+		$item['rankgroup'] = 3;
+	}else{
+		$item['rankgroup'] = 4;
+	}
+	
+	$new_itmes[] = $item;
+	
 }
 
 // dump($new_itmes);
@@ -89,8 +99,8 @@ foreach($items as $key => $item){
 ?>
 <style>
 *{
-    font-family: "TH Sarabun New","TH SarabunPSK";
-    font-size: 14pt;
+    font-family: TH SarabunPSK;
+    font-size: 16pt;
 }
 /* µÒÃÒ§ */
 .chk_table{
@@ -148,35 +158,6 @@ foreach($items as $key => $item){
 		$ptname = preg_replace('/\s+/', ' ', $item['ptname']);
 		list($firstname, $lastname) = explode(' ', $ptname);
 
-
-		if( $item['birthday'] == "0000-00-00" ){
-
-			$test_hn = preg_match('/\d{2}\-\d.+/',$item['hn'],$matchs);
-			if( $test_hn > 0 ){
-				$sql_opcard = "SELECT `idcard`,`dbirth` FROM `opcard` WHERE `hn` = '".$item['hn']."' ";
-				$db->select($sql_opcard);
-				$opc = $db->get_item();
-
-				if( $opc != NULL ){
-
-					list($oy, $om, $od) = explode('-', $opc['dbirth']); 
-					$date_of_birth = intval($od)."/".intval($om)."/".$oy;
-
-
-					$item['idcard'] = $opc['idcard'];
-				}else{
-					$date_of_birth = '00/00/0000';
-				}
-
-			}else{
-				$date_of_birth = '00/00/0000';
-			}
-
-		}else{
-			list($y, $m, $d) = explode('-', $item['birthday']); 
-			$date_of_birth = intval($d)."/".intval($m)."/".($y+543);
-		}
-
 		?>
 		<tr>
 			<td><?php echo $i; ?></td>
@@ -192,10 +173,8 @@ foreach($items as $key => $item){
 			<td><?php echo $item['rankgroup'];?></td>
 			<td>
 				<?php 
-				
-
-				echo $date_of_birth
-				
+				list($y, $m, $d) = explode('-', $item['birthday']); 
+				echo intval($d)."/".intval($m)."/".($y);
 				?>
 			</td>
 			<td>
@@ -210,14 +189,14 @@ foreach($items as $key => $item){
 			<td>
 				<?php echo $item['bmi'];?>
 			</td>
-			<td><?php echo number_format(($item['waist'] / 0.39370), 1);?></td>
+			<td><?php echo number_format(($item['round_'] / 0.39370), 1);?></td>
 			<td><?php echo $item['bp1']; ?></td>
 			<td><?php echo $item['bp2']; ?></td>
-			<td><?php echo !empty($item['glu_result']) ? $item['glu_result'] : '' ; ?></td>
-			<td><?php echo !empty($item['chol_result']) ? $item['chol_result'] : '' ; ?></td>
-			<td><?php echo !empty($item['trig_result']) ? $item['trig_result'] : '' ; ?></td>
-			<td><?php echo !empty($item['hdl_result']) ? $item['hdl_result'] : '' ; ?></td>
-			<td><?php echo !empty($item['ldl_result']) ? $item['ldl_result'] : '' ; ?></td>
+			<td><?php echo !empty($item['bs']) ? $item['bs'] : '' ; ?></td>
+			<td><?php echo !empty($item['chol']) ? $item['chol'] : '' ; ?></td>
+			<td><?php echo !empty($item['cr']) ? $item['cr'] : '' ; ?></td>
+			<td><?php echo !empty($item['hdl']) ? $item['hdl'] : '' ; ?></td>
+			<td><?php echo !empty($item['ldl']) ? $item['ldl'] : '' ; ?></td>
 		</tr>
 		<?php
 		$i++;
