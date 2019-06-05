d9adb823fa0303a50fcfce38db8757dca47cdf39
kritsanasak kuntaros <roboconk@gmail.com>
2019-06-05 15:41:09 +0700

    ปรับ ให้ลงผลcxrได้ และแสดงการสรุปผลของรังษีแพทย์ในห้องหมอได้ เพิ่ม การค้นหาข้อมูลตามเลขบัตรประชาชน

--- surasak3/chk_cxr_doctor.php
+++ surasak3/chk_cxr_doctor.php
@@ -3,6 +3,7 @@ include 'bootstrap.php';
 
 $db = Mysql::load();
 $action = input_post('action');
+$officer = $_SESSION['sOfficer'];
 
 if ( $action == 'save' ) {
     
@@ -10,18 +11,18 @@ if ( $action == 'save' ) {
     $detail = $_POST['cxr_detail'];
     $part = input_post('part');
 
-    $officer = $_SESSION['sOfficer'];
+    $db->select("SELECT `yearchk` FROM `chk_company_list` WHERE `code` = '$part' ");
+    $company = $db->get_item();
+    $yearchk = substr($company['yearchk'], 2, 2);
 
     $msg = "บันทึกข้อมูลเรียบร้อย";
 
     foreach ($cxr_items as $hn => $item) {
-
-        $yearchk = get_year_checkup();
         
         $sql = "SELECT `id` FROM `chk_cxr` WHERE `hn` = '$hn' AND `part` = '$part' ";
 
         $cxr = $item;
-        $cxr_detail = $detail[$hn];
+        $cxr_detail = htmlspecialchars($detail[$hn], ENT_QUOTES);
 
         $db->select($sql);
         $row = $db->get_rows();
@@ -48,7 +49,6 @@ if ( $action == 'save' ) {
             $sql = "SELECT *,CONCAT(`name`,' ',`surname`) AS `ptname` FROM `opcardchk` WHERE `HN` = '$hn' AND `part` = '$part' ";
             $db->select($sql);
             $user = $db->get_item();
-
             $ptname = $user['ptname'];
 
             // insert 
@@ -68,6 +68,67 @@ if ( $action == 'save' ) {
     } // end for 
 
     redirect('chk_cxr_doctor.php', $msg);
+    exit;
+
+}elseif ( $action == 'save_per_user' ) {
+    
+    $hn = input_post('hn');
+    $part = input_post('part');
+    $cxr = $_POST['res'];
+    $cxr_detail = $_POST['res_detail'];
+
+    $part = iconv('UTF-8', 'TIS-620', $part);
+    $cxr = iconv('UTF-8', 'TIS-620', $cxr);
+    $cxr_detail = iconv('UTF-8', 'TIS-620', $cxr_detail);
+    $cxr_detail = htmlspecialchars($cxr_detail, ENT_QUOTES);
+
+    $db->select("SELECT `yearchk` FROM `chk_company_list` WHERE `code` = '$part' ");
+    $company = $db->get_item();
+    $yearchk = substr($company['yearchk'], 2, 2);
+
+    $db->select("SELECT `id` FROM `chk_cxr` WHERE `hn` = '$hn' AND `part` = '$part' ");
+    $test_rows = $db->get_rows();
+
+    $response = 0;
+
+    if( $test_rows > 0 ){
+        // update
+        $out_items = $db->get_item();
+        $row_id = $out_items['id'];
+
+        $sql = "UPDATE `chk_cxr` SET 
+        `cxr`='$cxr', 
+        `detail`='$cxr_detail', 
+        `editor`='$officer', 
+        `edit_date`=NOW(),
+        `year_chk`='$yearchk'
+        WHERE (`id`='$row_id');";
+        
+        $save = $db->update($sql);
+        if( $save !== true ){
+            $response = 1;
+        }
+
+    }else{
+        // insert
+
+        $sql = "SELECT *,CONCAT(`name`,' ',`surname`) AS `ptname` FROM `opcardchk` WHERE `HN` = '$hn' AND `part` = '$part' ";
+        $db->select($sql);
+        $user = $db->get_item();
+        $ptname = $user['ptname'];
+        
+        $sql = "INSERT INTO `chk_cxr` (
+            `id`, `hn`, `ptname`, `cxr`, `detail`, `officer`, `date`, `editor`, `edit_date`, `part`, `year_chk` 
+        ) VALUES (
+            NULL, '$hn', '$ptname', '$cxr', '$cxr_detail', '$officer', NOW(), '$officer', NOW(), '$part', '$yearchk'
+        );";
+         $save = $db->insert($sql);
+         if( $save !== true ){
+            $response = 1;
+         }
+    }
+    
+    echo $response;
     exit;
 }
 
@@ -198,21 +259,38 @@ if ( $page == 'search' ) {
     $sql = "SELECT *,CONCAT(`name`,' ',`surname`) AS `ptname` FROM `opcardchk` WHERE `part` = '$part' ORDER BY `row` ";
     $db->select($sql);
     $items = $db->get_items();
+
+    $pre_items = array();
+    foreach ($items as $key => $item) {
+        
+        list($year, $number) = explode('-', $item['HN']);
+        $number = sprintf('%05d', $number);
+        $key = $year.$number;
+
+        $pre_items[$key] = $item;
+
+    }
+    ksort($pre_items);
     
     ?>
     <fieldset>
-        <legend>บันทึกข้อมูล <?=$company['name'];?></legend>
+        <legend>บันทึกข้อมูล <b><?=$company['name'];?></b></legend>
         <form action="chk_cxr_doctor.php" method="post">
             <div>
                 <table class="chk_table">
                     <tr>
+                        <th>#</th>
                         <th>HN</th>
                         <th>ชื่อ-สกุล</th>
                         <th>บันทึกผล</th>
+                        <th></th>
                     </tr>
 
                     <?php 
-                    foreach ($items as $key => $item) {
+                    $i = 0;
+                    foreach ($pre_items as $key => $item) {
+
+                        ++$i;
                         
                         $row = $item['row'];
                         $hn = $item['HN'];
@@ -235,25 +313,39 @@ if ( $page == 'search' ) {
                                 $normal = 'checked="checked"';
                             } 
 
-                            if( $res == 'ผิดปกติ' ){
+                            if( $res == 'ผิดปกติเล็กน้อย' ){
+                                $unnormal_abit = 'checked="checked"';
+                            } 
+
+                            if( $res == 'ผิดปกติควรพบแพทย์' ){
                                 $unnormal = 'checked="checked"';
                             } 
 
                         }
 
                         ?>
-                        <tr>
+                        <tr style="vertical-align: top; vertical-align: text-top;">
+                            <td><?=$i;?></td>
                             <td><?=$hn;?></td>
                             <td><?=$item['ptname'];?></td>
                             <td>
                                 <label for="cxr<?=$row;?>a">
-                                    <input type="radio" name="cxr[<?=$hn;?>]" id="cxr<?=$row;?>a" value="ปกติ" <?=$normal;?> > ปกติ
+                                    <input type="radio" name="cxr[<?=$hn;?>]" class="cxr<?=$row;?>" id="cxr<?=$row;?>a" value="ปกติ" <?=$normal;?> > ปกติ
                                 </label>
+
                                 <label for="cxr<?=$row;?>b">
-                                    <input type="radio" name="cxr[<?=$hn;?>]" id="cxr<?=$row;?>b" value="ผิดปกติ" <?=$unnormal;?> > ผิดปกติ
+                                    <input type="radio" name="cxr[<?=$hn;?>]" class="cxr<?=$row;?>" id="cxr<?=$row;?>b" value="ผิดปกติเล็กน้อย" <?=$unnormal_abit;?> > ผิดปกติเล็กน้อย
                                 </label>
-                                <input type="text" name="cxr_detail[<?=$hn;?>]" id="" size="40" value="<?=$res_detail;?>">
+
+                                <label for="cxr<?=$row;?>c">
+                                    <input type="radio" name="cxr[<?=$hn;?>]" class="cxr<?=$row;?>" id="cxr<?=$row;?>c" value="ผิดปกติควรพบแพทย์" <?=$unnormal;?> > ผิดปกติควรพบแพทย์
+                                </label>
+                                <br>
+                                รายละเอียด: <textarea name="cxr_detail[<?=$hn;?>]" class="cxr_detail<?=$row;?>" cols="50" rows="3"><?=$res_detail;?></textarea>
                                 <input type="hidden" name="hn[]" value="<?=$item['HN'];?>">
+                            </td>
+                            <td style="vertical-align: middle;">
+                                <button type="botton" onclick="return false;" class="per_user" data-hn="<?=$hn;?>" data-part="<?=$part;?>" data-row="<?=$row;?>">บันทึก</button>
                             </td>
                         </tr>
                         <?php
@@ -262,12 +354,55 @@ if ( $page == 'search' ) {
                 </table>
             </div>
             <div>
-                <button type="submit">บันทึกข้อมูล</button>
+                <button type="submit">บันทึกข้อมูลทั้งหมด</button>
                 <input type="hidden" name="action" value="save">
                 <input type="hidden" name="part" value="<?=$part;?>">
             </div>
         </form>
     </fieldset>
+    <script src="js/vendor/jquery-1.11.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        jQuery.noConflict();
+        (function( $ ) {
+        $(function() {
+            
+            $(document).on('click', '.per_user', function(){
+
+                var hn = $(this).attr('data-hn');
+                var part = $(this).attr('data-part');
+                var row = $(this).attr('data-row');
+                
+                var key_res = '.cxr'+row+':checked';
+                var key_res_detail = '.cxr_detail'+row;
+                var res = $(key_res).val();
+                var res_detail = $(key_res_detail).val();
+
+                $.ajax({
+                    method: "POST",
+                    url: "chk_cxr_doctor.php",
+                    data: { 'hn': hn,'part': part,'res': res,'res_detail': res_detail,'action': 'save_per_user'},
+                    success: function(response){
+
+                        if( response == 0 ){
+                            alert('บันทึกข้อมูล HN: '+hn+' เรียบร้อย');
+                        }else if( response == 1 ){
+                            alert('การบันทึกข้อมูลมีปัญหา กรุณาติดต่อศูนย์คอมพิวเตอร์');
+                        }
+
+                        // res = $.parseJSON(res);
+                        // if( res.find_rows > 0 ){
+                        //     $('#alert_code').show();
+                        // }else{
+                        //     $('#alert_code').hide();
+                        // }
+                    }
+                });
+
+            });
+            
+        });
+    })(jQuery);
+    </script>
     <?php
 
 }
---
+++ surasak3/chk_test_ipcard.php
@@ -0,0 +1,116 @@
+<?php 
+
+include 'bootstrap.php';
+
+$action = input('action');
+$db = Mysql::load($shs_configs);
+// $db = Mysql::load();
+
+include 'chk_menu.php';
+    ?>
+    <h3>ค้นหา ชื่อ-สกุล จากเลขบัตรประชาชน</h3>
+    <form action="chk_test_ipcard.php" method="post" enctype="multipart/form-data">
+        <div>
+            ไฟล์นำเข้า : <input type="file" name="file">
+        </div>
+        <div>
+            <p><b>ตัวอย่าง รูปแบบการจัดไฟล์ Excel</b></p>
+            <table class="chk_table">
+                <tr>
+                    <td>เลขบัตรประชาชน</td>
+                    <td>ชื่อ</td>
+                    <td>สกุล</td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <button type="submit">ตรวจสอบตามเลขบัตร</button>
+            <input type="hidden" name="action" value="test">
+        </div>
+    </form>
+    <?php 
+
+
+if( $action == false ){ 
+    
+}elseif ( $action == 'test' ) {
+    
+    $file = $_FILES['file'];
+    $content = file_get_contents($file['tmp_name']);
+
+    if( $content !== false ){
+        $items = explode("\r\n", $content);
+        
+        ?>
+        <style>
+        .chk_table{
+            border-collapse: collapse;
+        }
+        .chk_table th, 
+        .chk_table td{
+            border: 1px solid black;
+            padding: 3px;
+        }
+        </style>
+        <table class="chk_table">
+            <tr>
+                <th rowspan="2">#</th>
+                <th colspan="3">ข้อมูลที่ตรวจสอบ</th>
+                <th colspan="4">ข้อมูลจากฐานข้อมูล</th>
+            </tr>
+            <tr>
+                <th>เลขบัตรประชาชน</th>
+                <th>ชื่อ</th>
+                <th>สกุล</th>
+                <th>เลขบัตรประชาชน</th>
+                <th>ชื่อ</th>
+                <th>สกุล</th>
+                <th>HN</th>
+            </tr>
+        <?php
+
+        $i = 0;
+        foreach ( $items as $key => $item ) {
+
+            list($idcard, $name, $surname) = explode(',', $item,3);
+
+            if( !empty($idcard) ){
+
+                ++$i;
+            
+                $sql = "SELECT `hn`,`name`,`surname`,CONCAT(`yot`,`name`,' ',`surname`) AS `ptname`, `idcard`,`sex` 
+                FROM `opcard` 
+                WHERE `idcard` = '$idcard' ";
+                $db->select($sql);
+                $user = $db->get_item();
+                
+                $color = '';
+
+                if( $name != $user['name'] ){
+                    $color = 'style="background-color: yellow;"';
+                }
+
+                if( $surname != $user['surname'] ){
+                    $color = 'style="background-color: yellow;"';
+                }
+
+                ?>
+                <tr <?=$color;?>>
+                    <td><?=$i;?></td>
+                    <td><?=$idcard;?></td>
+                    <td><?=$name;?></td>
+                    <td><?=$surname;?></td>
+                    <td><?=$user['idcard'];?></td>
+                    <td><?=$user['name'];?></td>
+                    <td><?=$user['surname'];?></td>
+                    <td><?=$user['hn'];?></td>
+                </tr>
+                <?php
+            }
+
+        }
+        ?>
+        </table>
+        <?php
+    }
+}
