9bf476ef414c0694e9bd989b11ef8969254d3875
kritsanasak kuntaros <roboconk@gmail.com>
2019-07-15 15:52:28 +0700

    no message

--- surasak3/opdcard.php
+++ surasak3/opdcard.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+	include("connect.inc");
     if (isset($sIdname)){} else {die;} //for security
 ?>
 <div align='left'>
@@ -9,11 +10,18 @@
       <td width='87%'>&nbsp;
         <p><b><font size='4'>โปรดทราบ !</font></b><br>
 	ต้องการทำบัตรตรวจโรคให้เลือก  ทำบัตรตรวจโรค<br>
-	ให้ทำการตรวจหาข้อมูลผู้ป่วยก่อนทำการทำประวัติใหม่<br>
-	<br>
-        <br>
-               <a target=_parent href="opcard.php">ทำบัตรตรวจโรค</a>
-    <br>           &nbsp;&nbsp;&nbsp;&nbsp <a target=_self  href="../nindex.htm"><< ยกเลิก</a></td>
+	ให้ทำการตรวจหาข้อมูลผู้ป่วยก่อนทำการทำประวัติใหม่
+    <p>&nbsp;</p>
+	<?php
+	$q = mysql_query("SELECT * FROM `runno` WHERE `title` = 'kewcard' ");
+	$item = mysql_fetch_assoc($q);
+	$dVndate=$item['startday'];
+	$nVkew=$item['runno'];
+	?>        
+        <a href="kewcard.php" target="_blank" onclick="refresh">คิวทำบัตรใหม่</a><br>
+        <a target=_parent href="opcard.php">ทำบัตรตรวจโรค</a><br>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp <a target=_self  href="../nindex.htm"><< ยกเลิก</a></td>
     </tr>
   </table>
 </div>
