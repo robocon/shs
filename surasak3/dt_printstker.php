3b7c4c83ccfeb5718c64809c684c4e65fe5b1cab
kritsanasak kuntaros <roboconk@gmail.com>
2019-08-27 10:46:57 +0700

    fix

--- surasak3/dt_printstker.php
+++ surasak3/dt_printstker.php
@@ -61,7 +61,7 @@ echo $_SESSION["dt_drugstk"];
 	window.onload = function(){
 		window.print();
 		setTimeout(function(){
-			// window.location.href = 'dt_index.php';
+			window.location.href = 'dt_index.php';
 		},<?php echo $time;?>);
 	}
 </script>
