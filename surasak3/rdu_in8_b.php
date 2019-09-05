834d9f3dc29ae571f7b8ff26b82e5b2df3c2c52b
kritsanasak kuntaros <roboconk@gmail.com>
2019-09-05 14:57:07 +0700

    แก้ดึงแผลจาก ER

--- surasak3/rdu_in8_b.php
+++ surasak3/rdu_in8_b.php
@@ -8,24 +8,37 @@ $db = Mysql::load($rdu_configs);
 $year = input_get('year');
 $quarter = input_get('quarter');
 
-$db->exec("DROP TEMPORARY TABLE IF EXISTS `tmp_opday_in8`");
-$sql = "CREATE TEMPORARY TABLE `tmp_opday_in8` 
-SELECT `row_id`,`date`,`hn`,`ptname`,`age`,`diag`,`icd10`,`doctor`,`date_hn` 
-FROM `opday` 
-WHERE `year` = '$year' AND `quarter` = '$quarter' 
-AND ( 
-    `icd10` IN ( 'S00', 'S01', 'S05', 'S07', 'S08', 'S09', 'S10', 'S11' ) 
-    OR `icd10` IN ( 'S16', 'S17', 'S18', 'S19', 'S20', 'S21' ) 
-    OR `icd10` regexp 'S(2[8-9]|3[0-1])' 
-    OR `icd10` regexp 'S(3[8-9]|4[0-1])' 
-    OR `icd10` regexp 'S{1}([4-8]([6-9]|[0-1]))' 
-    OR `icd10` regexp 'S(8[6-9]|9[0-1]|9[6-9])' 
-    OR `icd10` regexp 'X([0-1][0-9])' 
-    OR `icd10` regexp 'X([2-3][0-9])' 
-)";
+$db->exec("DROP TEMPORARY TABLE IF EXISTS `tmp_opday_in8b`");
+$sql = "CREATE TEMPORARY TABLE `tmp_opday_in8b` 
+SELECT a.`hn`,a.`organ`,a.`maintenance`,
+b.`row_id`,b.`svdate` AS `date`,b.`icd10`,b.`date_hn`,b.`diag`,b.`doctor` 
+FROM ( 
+    SELECT `trauma_id` AS `row_id`,`hn`,`organ`,`maintenance`,`date_hn`
+    FROM trauma 
+    WHERE `year` = '$year' AND `quarter` = '$quarter' 
+    AND ( `organ` REGEXP 'มีด|mc|แผล' AND `maintenance` REGEXP 'AP.+lat' )
+    AND ( `organ` NOT REGEXP 'ไม่มีบาดแผล|ไม่มีแผล' )
+) AS a 
+LEFT JOIN ( 
+    SELECT `diag_id` AS `row_id`,`svdate`,`icd10`,`date_hn`,`diag`,`doctor` 
+    FROM `diag` 
+    WHERE `year` = '$year' AND `quarter` = '$quarter' 
+    AND ( 
+        `icd10` IN ( 'S00', 'S01', 'S05', 'S07', 'S08', 'S09', 'S10', 'S11' ) 
+        OR `icd10` IN ( 'S16', 'S17', 'S18', 'S19', 'S20', 'S21' ) 
+        OR `icd10` REGEXP 'S(2[8-9]|3[0-1])' 
+        OR `icd10` REGEXP 'S(3[8-9]|4[0-1])' 
+        OR `icd10` REGEXP 'S{1}([4-8]([6-9]|[0-1]))' 
+        OR `icd10` REGEXP 'S(8[6-9]|9[0-1]|9[6-9])' 
+        OR `icd10` REGEXP 'X([0-1][0-9])' 
+        OR `icd10` REGEXP 'X([2-3][0-9])' 
+    ) 
+    GROUP BY `hn`
+) AS b ON b.`date_hn` = a.`date_hn` 
+WHERE b.`row_id` IS NOT NULL ";
 $db->exec($sql);
 
-$sql = "SELECT * FROM `tmp_opday_in8`";
+$sql = "SELECT * FROM `tmp_opday_in8b`";
 $db->select($sql);
 $items = $db->get_items();
 
@@ -63,8 +76,6 @@ body, button{
         <th>อายุ</th>
         <th>Diag</th>
         <th>ICD-10</th>
-        <th>Drug code</th>
-        <th>จำนวน</th>
         <th>แพทย์</th>
     </tr>
 <?php 
@@ -79,8 +90,6 @@ foreach ($items as $key => $item) {
             <td><?=$item['age'];?></td>
             <td><?=$item['diag'];?></td>
             <td><?=$item['icd10'];?></td>
-            <td><?=$item['drugcode'];?></td>
-            <td><?=$item['amount'];?></td>
             <td><?=$item['doctor'];?></td>
     </tr>
     <?php
