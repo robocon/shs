c0b99cc3a94f9f39c524f0739b7d3e2eb877075b
kritsanasak kuntaros <roboconk@gmail.com>
2019-11-29 12:27:07 +0700

    fix

--- surasak3/chk_doctor.php
+++ surasak3/chk_doctor.php
@@ -628,7 +628,7 @@ h1,h3,p{
             <td valign="top">
                 <?php
 
-                $year_checkup = '62';
+                // $year_checkup = '62';
 
                 $sql = "SELECT b.`labcode`,b.`labname`,b.`result`,b.`normalrange`,b.`unit`,b.`flag`,SUBSTRING(b.`authorisedate`,1,10) AS `resultdate`
                 FROM ( 
