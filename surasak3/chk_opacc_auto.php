7319a642bed48b5af3274f4e5d72404620b38649
roboconk <roboconk@gmail.com>
2019-12-17 21:47:36 +0700

    ร่าง ดีดสอบตำรวจให้ส่วนเก็บเงิน

---
+++ surasak3/chk_opacc_auto.php
@@ -0,0 +1,28 @@
+<?php 
+
+include 'bootstrap.php';
+
+$db = Mysql::load();
+
+SELECT a.* 
+FROM `log_opcardchk` AS a 
+WHERE a.`part` = 'ÊÍºµÓÃÇ¨63' 
+
+$Thidate2 =(date("Y")+543).date("-m-d H:i:s");
+$depart = "OTHER";
+$detail = "¤èÒºÃÔ¡ÒÃµÃÇ¨ÊØ¢ÀÒ¾µÓÃÇ¨";
+$price = 880;
+$paid  = 880;
+$idname='¹Ò§¾Ç§à¾çªÃ â¹ã¨»Ô§';
+$credit="à§Ô¹Ê´";
+
+$sql = "INSERT INTO `opacc` ( 
+    `date` , `txdate` , `hn` , `depart` , `detail` , 
+    `price` , `paid` , `idname` , `credit` , `ptright` , 
+    `credit_detail` , `billno`
+) VALUES ( 
+    '$Thidate2', '$Thidate2', '$hn', '$depart', '$detail', 
+    '$price', '$paid', '$idname',  '$credit', 'R01 à§Ô¹Ê´', 
+    '', '$billno'
+);";
+	
