7c3aa845fbd51ebad08fe8f0bb35d008d78d2f9c
kritsanasak kuntaros <roboconk@gmail.com>
2020-01-06 11:18:57 +0700

    เก็บตก ตรวจตำรวจซ้ำเพิ่มเติม 11คน

--- surasak3/chkhn.php
+++ surasak3/chkhn.php
@@ -1,45 +1,70 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-874" />
-<title>กรอก HN เพื่อออกใบเสร็จ</title>
-<style type="text/css">
-<!--
-body,td,th {
-	font-family: TH SarabunPSK;
-	font-size: 24px;
-}
-.forminput {
-	font-family: TH SarabunPSK;
-	font-size: 18px;
-}
--->
-</style>
+	<meta http-equiv="Content-Type" content="text/html; charset=TIS-620" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>กรอก HN เพื่อออกใบเสร็จ</title>
+	<script type="text/javascript">
+		function IsNumeric(sText,obj)<!--เช็คตัวเลข-->
+		{
+		var ValidChars = "0123456789.";
+		var IsNumber=true;
+		var Char;
+		for (i = 0; i < sText.length && IsNumber == true; i++) 
+			{ 
+			Char = sText.charAt(i); 
+			if (ValidChars.indexOf(Char) == -1) 
+				{
+				IsNumber = false;
+				}
+			}
+			if(IsNumber==false){
+				alert("กรอกข้อมูลได้เฉพาะตัวเลขเท่านั้น");
+				obj.value=sText.substr(0,sText.length-1);
+			}
+		}
+	</script>
+	<style type="text/css">
+		body,td,th {
+			font-family: TH SarabunPSK;
+			font-size: 24px;
+		}
+		.forminput {
+			font-family: TH SarabunPSK;
+			font-size: 18px;
+		}
+		body {
+			background-image: url();
+			background-color: #339999;
+		}
+	</style>
 </head>
-
 <body>
-<div align="center">
-  <p><strong>โปรแกรมออกใบเสร็จรับเงิน<br />
-  ตรวจสุขภาพตำรวจ 2558</strong></p>
-  <form id="form1" name="form1" method="post" action="slip.php" target="_blank">
-     
-    <label><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;กรอก HN :</strong></label>
-    
-    <label>
-    <input name="hn" type="text" class="forminput" id="hn" maxlength="6" />
-    </label>
-    <br />
-    <label><strong>จำนวนเงินที่รับ :</strong></label>
-    <label>
-    <input name="cash" type="text" class="forminput" id="cash" maxlength="6" />
-    </label>
-    <br />    
-    <label> <br />
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input name="button" type="submit" class="forminput" id="button" value="ออกใบเสร็จ" />
-    </label>
-  </form>
-  <p>&nbsp;</p>
-</div>
+	<div align="center">
+		<div align="center"><img src="images/shs.png" width="129" height="101" border="0" /></div>
+	<div><strong>โปรแกรมออกใบเสร็จรับเงิน<br />
+		ตรวจสุขภาพตำรวจ 2563</strong></div>
+		<br /> 
+		<form id="form1" name="form1" method="post" action="slip.php" target="_blank">
+			<table width="50%" border="0">
+				<tr>
+					<td align="right"><label><strong>กรอก HN :</strong></label></td>
+					<td><input name="hn" type="text" class="forminput" id="hn" maxlength="7" autocomplete="off" autofocus="autofocus" value="630" /></td>
+				</tr>
+				<tr>
+					<td align="right"><label><strong>จำนวนเงินที่รับ :</strong></label></td>
+					<td>
+						<input name="cash" type="text" class="forminput" id="cash" maxlength="4" onkeyup="IsNumeric(this.value,this)" value="60" readonly/>
+					</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td><input name="button" type="submit" class="forminput" id="button" value="ออกใบเสร็จรับเงิน" /></td>
+				</tr>
+			</table>
+		</form>
+		<p>&nbsp;</p>
+	</div>
 </body>
 </html>
--- surasak3/slip.php
+++ surasak3/slip.php
@@ -1,42 +1,66 @@
+<?php 
+include("connect.inc");
+$sql="select * from opcardchk where hn='".$_POST['hn']."' and part LIKE 'สอบตำรวจ63%'";
+
+$query=mysql_query($sql);
+
+if( mysql_num_rows($query) == 0 ){
+	echo "ไม่พบ HN";
+	exit;
+}
+
+$rows=mysql_fetch_array($query);
+$ptname=$rows["yot"]." ".$rows["name"]."   ".$rows["surname"];
+
+// $showdate="27-03-2558";
+$showdate = date('d-m-').(date('Y')+543);
+$showtime=date("H:i");
+
+$change=$_POST["cash"]-60;
+
+$hn = $HN = $rows["HN"];
+$exam_no=$rows["pid"];
+$part=$rows['part'];
+$datechk=date("Y-m-d H:i:s");
+$ptname = trim($ptname);
+
+$add="INSERT INTO `log_opcardchk` SET 
+`log_examno`='$exam_no', 
+`log_hn`='$HN', 
+`log_ptname`='$ptname', 
+`log_part`='$part', 
+`log_datechk`='$datechk',
+`price`='60'";
+$query=mysql_query($add);
+?>
+
+<html>
+<head>
 <style type="text/css">
-<!--
 body,td,th {
 	font-family: Angsana New;
 	font-size: 20px;
 }
--->
-</style><title>ออกใบเสร็จ</title></head>
-
-<body onLoad="window.print(); window.close();">
-<?
- include("connect.inc");
-$sql="select * from opcardchk where hn='$_POST[hn]'";
-//echo $sql;
-$query=mysql_query($sql);
-$rows=mysql_fetch_array($query);
-$ptname=$rows["yot"]." ".$rows["name"]."&nbsp;&nbsp;&nbsp;".$rows["surname"];
-
-$showdate="27-03-2558";
-$showtime=date("H:i");
-
-
-$change=$_POST["cash"]-800;
-?>
+</style>
+<title>ออกใบเสร็จ</title>
+</head>
+<body>
 <div style="margin-top:85px;"><strong>ชื่อ : <?=$ptname;?></strong>&nbsp;&nbsp;&nbsp;HN : <?=$hn;?>&nbsp;&nbsp;&nbsp;วันที่ <?=$showdate;?>&nbsp;&nbsp;เวลา <?=$showtime;?></div>
 
+<div style="margin-top:70px;">ค่าบริการตรวจสุขภาพตำรวจ <span style="margin-left:295px;">60.00</span></div>
 
-<div style="margin-top:70px;">ค่าบริการตรวจสุขภาพตำรวจ <span style="margin-left:295px;">800.00</span></div>
+<div style="margin-top:400px; margin-left:370px;">0.00 <span style="margin-left:55px;">60.00</span></div>
 
-<div style="margin-top:400px; margin-left:370px;">0.00 <span style="margin-left:55px;">800.00</span></div>
-
-<div style="margin-left: 40px;"><strong>** แปดร้อยบาทถ้วน **</strong><span style="margin-left:245px;">800.00</span></div>
+<div style="margin-left: 40px;"><strong>** หกสิบบาทถ้วน **</strong><span style="margin-left:245px;">60.00</span></div>
 <div style="font-size:18px;">
 <div style="margin-top:10px; margin-left: 250px;">ลงชื่อ.........................................ผู้รับเงิน</div>
-<? if($_POST["cash"]=="1000"){ ?>
-<div style="margin-left: 5px;">ได้รับ เงินสด (<?=$_POST["cash"];?>,<?=$change;?>)<span style="margin-left:150px;">(นาง พวงเพ็ชร&nbsp;&nbsp;&nbsp;โนใจปิง)</span></div>
-<? }else{?>
 <div style="margin-left: 5px;">ได้รับ เงินสด (<?=$_POST["cash"];?>,<?=$change;?>)<span style="margin-left:170px;">(นาง พวงเพ็ชร&nbsp;&nbsp;&nbsp;โนใจปิง)</span></div>
-<? }?>
 <div style="margin-left: 295px;">เจ้าหน้าที่เก็บเงิน</div>
 </div>
+<style type="text/javascript">
+window.onload = function(){
+	window.print();
+}
+</style>
 </body>
+</html>
