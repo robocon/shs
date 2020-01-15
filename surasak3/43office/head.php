fbba6106732392f259c88dd5d0029183876b835b
kritsanasak kuntaros <roboconk@gmail.com>
2020-01-15 15:52:50 +0700

    ฟอร์มบันทึกเด็กแรกเกิด

---
+++ surasak3/43office/head.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ทดสอบระบบ43แฟ้ม</title>
+
+    <link type="text/css" href="assets/menu.css" rel="stylesheet" />
+    <link type="text/css" href="assets/epoch_styles.css" rel="stylesheet" />
+    <script type="text/javascript" src="assets/epoch_classes.js"></script>
+    
+    <script type="text/javascript" src="assets/jquery.js"></script>
+    <script type="text/javascript" src="assets/menu.js"></script>
+
+</head>
+<body>
+
+<div id="no_print">
+	<div id="menu">
+		<ul class="menu">
+      <li><a href="../../nindex.htm" class="parent" title="หน้าหลัก ร.พ."><span>&#127968;</span></a></li>
+      <li>
+        <a href="javascript: void(0);"><span>เด็ก</span></a>
+        <ul>
+          <li><a href="wellbaby_lpho.php"><span>ฟอร์มเด็กแรกเกิด</span></a></li>
+          <li><a href="javascript: void(0);"><span>ฟอร์มเด็กหลังคลอด</span></a></li>
+          <li><a href="javascript: void(0);"><span>ฟอร์มพัฒนาการเด็ก0-12ปี</span></a></li>
+          <li class="last"><a href="javascript: void(0);"><span>ดูข้อมูล</span></a></li>
+        </ul>
+      </li>
+		</ul>
+	</div>
+	<div style="visibility: hidden"><a href="http://apycom.com/">a</a></div>
+</div>
---
+++ surasak3/43office/wellbaby_lpho.php
@@ -0,0 +1,238 @@
+<?php 
+
+include '../bootstrap.php';
+
+$action = input_post('action');
+if($action === 'save'){
+    
+    dump($_POST);
+    exit;
+}
+
+include 'head.php';
+?>
+
+<style>
+/* ตาราง */
+body, button{
+    font-family: "TH Sarabun New","TH SarabunPSK";
+    font-size: 13pt;
+}
+.chk_table{
+    border-collapse: collapse;
+}
+
+.chk_table th,
+.chk_table td{
+    padding: 3px;
+    border: 1px solid black;
+}
+label{
+    cursor: pointer;
+}
+
+@media print{
+    .div-hide{
+        display: none;
+    }
+}
+</style>
+
+<fieldset>
+    <legend>ค้นหาข้อมูลตาม HN</legend>
+    <form action="wellbaby_lpho.php" method="post">
+        <div>
+            HN : <input type="text" name="hn" id="hn">
+        </div>
+        <div>
+            <button type="submit">ค้นหา</button>
+            <input type="hidden" name="page" value="searchHn">
+        </div>
+    </form>
+</fieldset>
+<script>
+window.onload = function(){
+    document.getElementById("hn").focus();
+}
+</script>
+
+<?php 
+
+$page = input_post('page');
+if( $page === 'searchHn' ){ 
+
+    $db = Mysql::load();
+    $hn = input_post('hn');
+    $sql = "SELECT * FROM `ipcard` WHERE `hn` = '$hn'";
+    $db->select($sql);
+
+    if( $db->get_rows() > 0 ){
+        $item = $db->get_item();
+
+        // dump($item);
+        // echo "<hr>";
+
+        $db->select("SELECT * FROM `opcard` WHERE `hn`= '$hn'");
+        $opcard = $db->get_item();
+
+        // dump($opcard);
+        // echo "<hr>";
+
+        if( $opcard['yot'] == 'ด.ช.' ){
+            $sex = '1';
+        }elseif ( $opcard['yot'] == 'ด.ญ.' ) {
+            $sex = '2';
+        }
+        
+
+        $address = $opcard['address'].' ต.'.$opcard['tambol'].' อ.'.$opcard['ampur'].' จ.'.$opcard['changwat'];
+
+        ?>
+        <fieldset>
+            <legend>ข้อมูลเบื้องต้น</legend>
+            <table>
+                <tr>
+                    <td><b>AN : </b><?=$item['an'];?> <b>HN : </b><?=$item['hn'];?> <b>ชื่อ-สกุล : </b><?=$item['ptname'];?></td>
+                </tr>
+                <tr>
+                    <td>วันที่รับบริการ : <?=$item['date'];?></td>
+                </tr>
+            </table>
+        </fieldset>
+        
+        <form action="wellbaby_lpho.php" method="post">
+
+            <table>
+                <tr>
+                    <td>
+                    <h3>แบบเก็บข้อมูลโภชนาการและวัคซีนเด็ก 0-5 ปี หน่วยบริการ <span>โรงพยาบาลค่ายสุรศักดิ์มนตรี</span></h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        ชื่อสกุลบิดา <input type="text" name="father" id="" value="<?=trim($opcard['father']);?>"> ID <input type="text" name="fatherId" id="" size="12">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        ชื่อสกุลมารดา <input type="text" name="mother" id="" value="<?=trim($opcard['mother']);?>"> ID <input type="text" name="motherId" id="" size="12">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        บันทึกทารกแรกเกิด <input type="radio" name="prefix" id="prefix1" value="ด.ช." <?=($sex==1?'checked="checked"':'');?> > <label for="prefix1">ด.ช.</label> 
+                        <input type="radio" name="prefix" id="prefix2" value="ด.ญ." <?=($sex==2?'checked="checked"':'');?>> <label for="prefix2">ด.ญ.</label> 
+
+                        ชื่อ-สกุล <input type="text" name="name" id="" value="<?=$opcard['name'].' '.$opcard['surname'];?>"> ID <input type="text" name="idcard" id="" size="12" value="<?=$opcard['idcard'];?>">
+                        <input type="hidden" name="sex" value="<?=$sex;?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        ที่อยู่ <input type="text" name="address" id="" value="<?=$address;?>" size="40"> 
+                        เบอร์โทรที่ติดต่อได้ <input type="text" name="phone" id="" value="<?=$opcard['phone'];?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        วดป.เกิด <input type="text" name="dateBorn" id="dateBorn" value="<?=$opcard['dbirth'];?>"> 
+                        เวลา <input type="text" name="timeBorn" id="" size="10"> 
+                        น. น้ำหนักแรกเกิด <input type="text" name="weight" id="" size="5">กรัม
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        ความยาว <input type="text" name="height" id="" size="5">ซม. 
+                        เส้นรอบศรีษะ <input type="text" name="head" id="" size="5">ซม. 
+                        เส้นรอบอก <input type="text" name="breast" id="" size="5">ซม.
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        APGAR SCORE(1นาที) <input type="text" name="apgar1" id="" size="5"> 
+                        <!-- (5นาที) <input type="text" name="apgar5" id="" size="5"> -->
+                        ความผิดปกติแต่กำเนิด <input type="radio" name="disorder" id="disorder1" value="ไม่มี"><label for="disorder1">ไม่มี</label> 
+                        <input type="radio" name="disorder" id="disorder2" value="มี"><label for="disorder2">มี</label> 
+                        ระบุ <input type="text" name="disorderDetail" id="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        สภาวะสุขภาพแรกเกิด <input type="radio" name="health" id="health1" value="แข็งแรงดี"><label for="health1">แข็งแรงดี</label> 
+                        <input type="radio" name="health" id="health2" value="ผิดปกติ"><label for="health2">ผิดปกติ</label> 
+                        ระบุ <input type="text" name="healthDetail" id="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>วันที่จำหน่าย <input type="text" name="discharge" id="dischargeDate" size="10"> 
+                        น้ำหนักวันที่จำหน่าย <input type="text" name="weightDischarge" id="" size="5"> 
+                        วิตามินเค <input type="radio" name="vitamink" id="vitamink1" value="ฉีด"><label for="vitamink1">ฉีด</label> 
+                        <input type="radio" name="vitamink" id="vitamink2" value="ไม่ฉีด"><label for="vitamink2">ไม่ฉีด</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>การตรวจสภาวะพร่องไทรอยด์ฮอร์โมน <input type="radio" name="thyroid" id="thyroid1" value="ปกติ"><label for="thyroid1">ปกติ</label> 
+                        <input type="radio" name="thyroid" id="thyroid2" value="ผิดปกติ"><label for="thyroid2">ผิดปกติ</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>การตรวจPKU <input type="radio" name="pku" id="pku1" value="ปกติ"><label for="pku1">ปกติ</label> 
+                        <input type="radio" name="pku" id="pku2" value="ผิดปกติ"><label for="pku2">ผิดปกติ</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="bcg" id="bcg" value="1"> <label for="bcg">ได้ฉีดวัคซีนป้องกันโรค(BCG)</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="hb" id="hb" value="1"> <label for="hb">ได้ฉีดวัคซีนป้องกันโรคตับอักเสบบี(HB)</label>
+                    </td>
+                </tr>
+            </table>
+            <div>
+                <button type="submit">บันทึกข้อมูล</button>
+                <input type="hidden" name="action" value="save">
+            </div>
+        </form>
+        <script type="text/javascript">
+            var popup1, popup2;
+            window.onload = function() {
+                popup1 = new Epoch('popup1','popup',document.getElementById('dateBorn'),false);
+                popup2 = new Epoch('popup2','popup',document.getElementById('dischargeDate'),false);
+            };
+        </script>
+        <?php
+
+    }else{
+        ?>
+        <h1>ไม่พบข้อมูล</h1>
+        <?php
+    }
+    ?>
+    <?php
+}
+?>
+
+
+
+<?php 
+
+include 'footer.php';
+?>
+
+<!-- 
+<table class="chk_table">
+    <tr>
+        <td rowspan="2">วัคซีนที่ให้</td>
+        <td rowspan="2">อายุที่ควรรับ</td>
+        <td colspan="3">วันเดือนปีที่ได้รับวัคซีน</td>
+    </tr>
+    <tr>
+        <td>ครั้งที่1</td>
+        <td>ครั้งที่2</td>
+        <td>ครั้งที่3</td>
+    </tr>
+</table>
+-->
