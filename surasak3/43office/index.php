430f39982fe7d5cec31a20b117324846698eb055
kritsanasak kuntaros <roboconk@gmail.com>
2020-02-14 10:00:57 +0700

    แจ้งเตือนเบราเซอร์ต่ำกว่า IE9

--- surasak3/43office/index.php
+++ surasak3/43office/index.php
@@ -5,9 +5,13 @@ if( empty($_SESSION['sIdname']) ){
     redirect('../login_page.php','ชื่อผู้ใช้งานไม่ถูกต้อง');
     exit;
 }
-
 ?>
 <h1>ทดสอบระบบบันทึกข้อมูล43แฟ้ม</h1>
+<div>
+<!--[if lt IE 9]>
+เบราเซอร์ของท่านไม่รองรับการทำงาน กรุณาเปิดใช้งานด้วย <a href="https://www.google.com/chrome/" target="_blank">Google Chrome</a>, <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a> หรือ <a href="https://www.microsoft.com/en-us/edge" target="_blank">Microsoft Edge</a>
+<![endif]-->
+</div>
 <?php
 include 'footer.php';
 ?>
