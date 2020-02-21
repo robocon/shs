17123c68e0679f1452f775f9a8a89d43e541fe06
kritsanasak kuntaros <roboconk@gmail.com>
2020-02-21 11:08:51 +0700

    ปรับเมนูเป็น pure css

--- surasak3/43office/head.php
+++ surasak3/43office/head.php
@@ -10,127 +10,232 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <![endif]-->
 
-    <link type="text/css" href="assets/menu.css" rel="stylesheet" />
     <link type="text/css" href="assets/epoch_styles.css" rel="stylesheet" />
     <script type="text/javascript" src="assets/epoch_classes.js"></script>
     
     <script type="text/javascript" src="assets/jquery.js"></script>
-    <script type="text/javascript" src="assets/menu.js"></script>
 
+    <style>
+    /* Menu Pure CSS : */
+    nav {
+        /* margin: 100px auto; 
+        text-align: center; */
+    }
+    nav ul ul {
+        display: none;
+    }
+    nav ul li:hover > ul {
+        display: block;
+    }
+    nav ul {
+        background: #006781; 
+        background: linear-gradient(top, #00a4ce 0%, #006781 100%);  
+        background: -moz-linear-gradient(top, #00a4ce 0%, #006781 100%); 
+        background: -webkit-linear-gradient(top, #00a4ce 0%,#006781 100%); 
+        box-shadow: 0px 0px 9px rgba(0,0,0,0.15);
+        padding: 0 20px;
+        /* border-radius: 10px;   */
+        list-style: none;
+        position: relative;
+        display: inline-table;
+    }
+    nav ul:after {
+        content: ""; clear: both; display: block;
+    }
+    nav ul li {
+        float: left;
+    }
+    nav ul li:hover {
+        background: #4b545f;
+        background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+        background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+        background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+    }
+    nav ul li:hover a {
+        color: #fff;
+    }
+    nav ul li a {
+        display: block; 
+        padding: 10px 15px;
+        color: #ffffff; text-decoration: none;
+    }
+    nav ul ul {
+        background: #5f6975; border-radius: 0px; padding: 0;
+        position: absolute; top: 100%;
+    }
+    nav ul ul li {
+        float: none; 
+        border-top: 1px solid #6b727c;
+        border-bottom: 1px solid #575f6a; position: relative;
+    }
+    nav ul ul li a {
+        padding: 10px 15px;
+        color: #fff;
+        width: 200px;
+    }	
+    nav ul ul li a:hover {
+        background: #4b545f;
+    }
+    nav ul ul ul {
+        position: absolute; left: 100%; top:0;
+    }
+
+
+
+    /* ตาราง */
+    body, button{
+        font-family: "TH Sarabun New","TH SarabunPSK";
+        font-size: 14pt;
+    }
+    input:-moz-read-only{
+    background-color: #bbbbbb;
+    }
+    input:read-only{
+    background-color: #bbbbbb;
+    }
+    .clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+    }
+
+    .chk_table{
+        border-collapse: collapse;
+    }
+    .chk_table th,
+    .chk_table td{
+        padding: 3px;
+        border: 1px solid black;
+    }
+
+    fieldset{
+        border: 2px solid #656565;
+        padding: 4px;
+    }
+    legend{
+        margin-left: 10px;
+    }
+
+    label{
+        cursor: pointer;
+    }
+    .tdRow{
+        padding-bottom: 6px;
+        height: 32px;
+    }
+    .sRow{
+        padding-right: 15px;
+    }
+    .important{
+        border: 1px solid red;
+    }
+    .warning{
+        background-color: yellow;
+    }
+
+    .txtRight{
+        text-align: right;
+        font-weight: bold;
+    }
+
+    @media print{
+        .div-hide{
+            display: none;
+        }
+    }
+    </style>
 </head>
 <body>
-
 <div id="no_print">
-	<div id="menu">
-        <ul class="menu">
-            <li><a href="index.php" class="parent" title="หน้าหลัก ร.พ."><span>Home</span></a></li>
+    <nav>
+        <ul>
             <li>
-                <a href="javascript: void(0);"><span>เด็กแรกเกิด</span></a>
+                <a href="index.php" title="หน้าหลัก ร.พ.">Home</a>
+            </li>
+            <li>
+                <a href="javascript: void(0);">เด็กแรกเกิด</a>
                 <ul>
-                    <li><a href="formNewborn.php"><span>ฟอร์มทารกแรกเกิด</span></a></li>
-                    <li><a href="reportNewborn.php"><span>รายงาน NEWBORN</span></a></li>
-                    <li><a href="formNewborncare.php"><span>ฟอร์มทารกหลังคลอด</span></a></li>
-                    <li><a href="reportNewborncare.php"><span>รายงาน NEWBORNCARE</span></a></li>
-                    <li><a href="reportPolicy.php"><span>รายงาน POLICY</span></a></li>
+                    <li>
+                        <a href="formNewborn.php">ฟอร์มทารกแรกเกิด</a>
+                        <ul>
+                            <li><a href="reportNewborn.php">รายงาน NEWBORN</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="formNewborncare.php">ฟอร์มทารกหลังคลอด</a>
+                        <ul>
+                            <li><a href="reportNewborncare.php">รายงาน NEWBORNCARE</a></li>
+                        </ul>
+                    <li><a href="reportPolicy.php">รายงาน POLICY</a></li>
                 </ul>
             </li>
             <li>
-                <a href="javascript: void(0);"><span>หญิงตั้งครรภ์</span></a>
+                <a href="javascript: void(0);">หญิงตั้งครรภ์</a>
                 <ul>
-                    <li><a href="prenatal.php"><span>ประวัติตั้งครรภ์ PRENATAL</span></a></li>
-                    <li><a href="reportPrenatal.php"><span>รายงาน PRENATAL</span></a></li>
-                    <li><a href="anc.php"><span>บริการฝากครรภ์ ANC</span></a></li>
-                    <li><a href="anc_view.php"><span>รายงาน ANC</span></a></li>
-                    <li><a href="labor.php"><span>ประวัติการคลอด LABOR</span></a></li>
-                    <li><a href="reportLabor.php"><span>รายงาน LABOR</span></a></li>
-                    <li><a href="postnatal.php"><span>ดูแลมารดาหลังคลอด POSTNATAL</span></a></li>
-                    <li><a href="reportPostnatal.php"><span>รายงาน POSTNATAL</span></a></li>
-                    <li><a href="women.php"><span>หญิงเจริญพันธ์ WOMEN</span></a></li>
-                    <li><a href="reportWomen.php"><span>รายงาน WOMEN</span></a></li>
-                    <li><a href="fp.php"><span>วางแผนครอบครัว FP</span></a></li>
-                    <li><a href="reportFp.php"><span>รายงาน FP</span></a></li>
-                    <li><a href="dental.php"><span>สภาวะทันต DENTAL</span></a></li>
-                    <li><a href="reportDental.php"><span>รายงาน DENTAL</span></a></li>
+                    <li>
+                        <a href="prenatal.php">ประวัติตั้งครรภ์ PRENATAL</a>
+                        <ul>
+                            <li><a href="reportPrenatal.php">รายงาน PRENATAL</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="anc.php">บริการฝากครรภ์ ANC</a>
+                        <ul>
+                            <li><a href="anc_view.php">รายงาน ANC</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="labor.php">ประวัติการคลอด LABOR</a>
+                        <ul>
+                            <li><a href="reportLabor.php">รายงาน LABOR</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="postnatal.php">ดูแลมารดาหลังคลอด POSTNATAL</a>
+                        <ul>
+                            <li><a href="reportPostnatal.php">รายงาน POSTNATAL</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="women.php">หญิงเจริญพันธ์ WOMEN</a>
+                        <ul>
+                            <li><a href="reportWomen.php">รายงาน WOMEN</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="fp.php">วางแผนครอบครัว FP</a>
+                        <ul>
+                            <li><a href="reportFp.php">รายงาน FP</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="dental.php">สภาวะทันต DENTAL</a>
+                        <ul>
+                            <li><a href="reportDental.php">รายงาน DENTAL</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </li>
             <li>
-                <a href="javascript: void(0);"><span>วัคซีนและโภชนาการ</span></a>
+                <a href="javascript: void(0);">วัคซีนและโภชนาการ</a>
                 <ul>
-                    <li><a href="epi.php"><span>วัคซีนเด็ก EPI</span></a></li>
-                    <li><a href="reportEpi.php"><span>รายงาน EPI</span></a></li>
-                    <li><a href="nutrition.php"><span>ระดับโภชนาการ NUTRITION</span></a></li>
-                    <li><a href="reportNutrition.php"><span>รายงาน NUTRITION</span></a></li>
+                    <li>
+                        <a href="epi.php">วัคซีนเด็ก EPI</a>
+                        <ul>
+                            <li><a href="reportEpi.php">รายงาน EPI</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="nutrition.php">ระดับโภชนาการ NUTRITION</a>
+                        <ul>
+                            <li><a href="reportNutrition.php">รายงาน NUTRITION</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </li>
         </ul>
-	</div>
-	<div style="visibility: hidden"><a href="http://apycom.com/">a</a></div>
+    </nav>
 </div>
-
-<style>
-/* ตาราง */
-body, button{
-    font-family: "TH Sarabun New","TH SarabunPSK";
-    font-size: 14pt;
-}
-input:-moz-read-only{
-  background-color: #bbbbbb;
-}
-input:read-only{
-  background-color: #bbbbbb;
-}
-.clearfix::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-.chk_table{
-    border-collapse: collapse;
-}
-.chk_table th,
-.chk_table td{
-    padding: 3px;
-    border: 1px solid black;
-}
-
-fieldset{
-    border: 2px solid #656565;
-    padding: 4px;
-}
-legend{
-    margin-left: 10px;
-}
-
-label{
-    cursor: pointer;
-}
-.tdRow{
-    padding-bottom: 6px;
-    height: 32px;
-}
-.sRow{
-    padding-right: 15px;
-}
-.important{
-    border: 1px solid red;
-}
-.warning{
-    background-color: yellow;
-}
-
-.txtRight{
-    text-align: right;
-    font-weight: bold;
-}
-
-@media print{
-    .div-hide{
-        display: none;
-    }
-}
-</style>
-
 <?php 
 if( isset($_SESSION['x-msg']) ){
     ?><p style="background-color: #ffffc1; border: 1px solid #f0f000; padding: 5px;"><?=$_SESSION['x-msg'];?></p><?php
