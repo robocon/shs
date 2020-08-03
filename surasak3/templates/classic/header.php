93b1edca83311cc5f174e0d86d02bb9a4c91b816
kritsanasak kuntaros <roboconk@gmail.com>
2020-08-03 10:53:02 +0700

    fix

--- surasak3/templates/classic/header.php
+++ surasak3/templates/classic/header.php
@@ -1,20 +1,14 @@
 <?php 
-$charset = (defined('CHARSET') !== false) ? CHARSET : 'utf-8' ; 
+$charset = (defined('CHARSET') !== false) ? CHARSET : 'UTF-8' ; 
 $title = isset($title) ? $title : '' ;
 $content = isset($content) ? $content : '' ;
-/*
-<!DOCTYPE html 
-     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
         <meta charset="<?php echo $charset;?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $title;?></title>
         <meta name="description" content="<?php echo $content;?>">
         <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
