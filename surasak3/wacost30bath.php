4cb6d08fdde1a306ae9673d547e5271b6ad3b4fd
kritsanasak kuntaros <roboconk@gmail.com>
2020-08-26 15:59:45 +0700

    เพิ่ม ในหน้าสถิติ 30บาทให้แสดง icd10 ด้วย

--- surasak3/wacost30bath.php
+++ surasak3/wacost30bath.php
@@ -12,31 +12,40 @@
 	<th align="center" colspan="12"><font face='Angsana New'>บันทึกผู้ป่วยประกันสุขภาพถ้วนหน้า ประจำเดือน <?php echo $m." - ".$yr;?></th>
 </tr>
  <tr>
-<th bgcolor=6495ED><font face='Angsana New'>ลำดับ</th>
-<th bgcolor=6495ED><font face='Angsana New'>วันรักษา</th>
-<th bgcolor=6495ED><font face='Angsana New'>เลขบัตรทอง</th>
-<th bgcolor=6495ED><font face='Angsana New'>ยศ-ชื่อ-สกุล</th>
-<th bgcolor=6495ED><font face='Angsana New'>เลขบัตรประชาชน</th>
-<th bgcolor=6495ED><font face='Angsana New'>ยาเวชภัณฑ์</th>
-<th bgcolor=6495ED><font face='Angsana New'>พยาธิ</th>
-<th bgcolor=6495ED><font face='Angsana New'>เอกซเรย์</th>
-<th bgcolor=6495ED><font face='Angsana New'>ฉุกเฉิน/อื่น</th>
-<th bgcolor=6495ED><font face='Angsana New'>ทันตกรรม</th>
-<th bgcolor=6495ED><font face='Angsana New'>รวมเงิน</th>
-<th bgcolor=6495ED><font face='Angsana New'>วินิจฉัยโรค</th>
-<th bgcolor=6495ED><font face='Angsana New'>หมายเหตุ</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ลำดับ</th>
+<th bgcolor="6495ED"><font face='Angsana New'>วันรักษา</th>
+<th bgcolor="6495ED"><font face='Angsana New'>เลขบัตรทอง</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ยศ-ชื่อ-สกุล</th>
+<th bgcolor="6495ED"><font face='Angsana New'>เลขบัตรประชาชน</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ยาเวชภัณฑ์</th>
+<th bgcolor="6495ED"><font face='Angsana New'>พยาธิ</th>
+<th bgcolor="6495ED"><font face='Angsana New'>เอกซเรย์</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ฉุกเฉิน/อื่น</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ทันตกรรม</th>
+<th bgcolor="6495ED"><font face='Angsana New'>รวมเงิน</th>
+<th bgcolor="6495ED"><font face='Angsana New'>วินิจฉัยโรค</th>
+<th bgcolor="6495ED"><font face='Angsana New'>ICD10</th>
+<th bgcolor="6495ED"><font face='Angsana New'>OTHER</th>
+<th bgcolor="6495ED"><font face='Angsana New'>หมายเหตุ</th>
   </tr>
 
 <?php
     include("connect.inc");
   
-    $query = "SELECT date_format(a.thidate,'%d/%m/%y'), a.ptname,a.hn,a.an,a.note,a.diag,a.patho,a.xray,a.phar,a.emer,a.surg,a.physi,a.denta,a.other,a.doctor,idcard FROM (Select  * From opday WHERE thidate LIKE '$today%' AND left(ptright,3) IN ('R09','R10','R11','R13','R17') ) as a  Order by thidate ASC ";
+    $query = "SELECT date_format(a.thidate,'%d/%m/%y'), a.ptname,a.hn,a.an,a.note,a.diag,a.patho,a.xray,a.phar,a.emer,a.surg,a.physi,a.denta,a.other,a.doctor,idcard,a.icd10,a.icd10_other
+    FROM ( 
+        Select * 
+        From opday 
+        WHERE thidate LIKE '$today%' 
+        AND left(ptright,3) IN ('R09','R10','R11','R13','R17') 
+    ) as a  
+    Order by thidate ASC ";
 
     $result = mysql_query($query)
         or die("Query failed");
     $n=0;
  $totalpri=0;
-    while (list ($thidate,$ptname,$hn,$an,$note,$diag,$patho,$xray,$phar,$emer,$surg,$physi,$denta,$other,$doctor,$idcard) = mysql_fetch_row ($result)) {
+    while (list ($thidate,$ptname,$hn,$an,$note,$diag,$patho,$xray,$phar,$emer,$surg,$physi,$denta,$other,$doctor,$idcard,$icd10,$icd10Other) = mysql_fetch_row ($result)) {
         $n++;	
 
 //$free=0;
@@ -60,7 +69,9 @@ $totalpri=$phar+$patho+$xray+$emer+$other+$denta;
 					"  <td BGCOLOR=66CDAA align='right'><font face='Angsana New'>".($emer+$other)."</td>\n".
 					"  <td BGCOLOR=66CDAA align='right'><font face='Angsana New'>$denta</td>\n".
 					"  <td BGCOLOR=66CDAA align='right'><font face='Angsana New'>$totalpri</td>\n".
-					"  <td BGCOLOR=66CDAA><font face='Angsana New'>$diag</td>\n".
+                    "  <td BGCOLOR=66CDAA><font face='Angsana New'>$diag</td>\n".
+                    "  <td BGCOLOR=66CDAA><font face='Angsana New'>$icd10</td>\n".
+                    "  <td BGCOLOR=66CDAA><font face='Angsana New'>$icd10Other</td>\n".
 					"  <td BGCOLOR=66CDAA><font face='Angsana New'>$note</td>\n".
 					
            " </tr>\n");
