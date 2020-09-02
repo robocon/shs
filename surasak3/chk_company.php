9fbfe9981065c746291f9bbdcf9d3afbb573fd95
kritsanasak kuntaros <roboconk@gmail.com>
2020-09-02 11:29:23 +0700

    ปรับ ให้แสดงผลในเมนูหลัก

--- surasak3/chk_company.php
+++ surasak3/chk_company.php
@@ -207,7 +207,6 @@ if ( $views == 'search' ) {
                         <li><a href="chk_report_all.php?camp=<?=$item['code'];?>" target="_blank">สรุปผลตรวจ</a></li>
                         <li><a href="chk_all_lab.php?part=<?=$item['code'];?>" target="_blank">ผล Lab ทั้งหมด</a></li>
                         <li><a href="chk_lab_sticker.php?part=<?=$item['code'];?>" target="_blank">พิมพ์สติกเกอร์ LAB</a></li>
-                        <li><a href="pm_shs.php" target="_blank">เข้าเมนูพี่สอง</a></li>
                         <li><a href="chk_report_all_money.php?camp=<?=$item['code'];?>" target="_blank">ทดสอบ ค่าใช้จ่ายจากรายการแลป (ตรวจนอกรพ.)</a></li>
                     </ol>
                 </td>
