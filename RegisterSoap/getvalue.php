046047bee150c52759fd8521af5ebeafbba842cc
kritsanasak kuntaros <roboconk@gmail.com>
2020-09-24 14:30:23 +0700

    ปรับ การเรียกใช้งาน getvalue ให้ใช้ผ่าน httprequest ได้

--- RegisterSoap/getvalue.php
+++ RegisterSoap/getvalue.php
@@ -5,5 +5,9 @@ $cont = file_get_contents("D:\\UCAuthentication4.x\\nhso_token.txt");
 $cont = trim($cont);
 list($user_person_id,$smctoken) = explode('#', $cont);
 
-var_dump($user_person_id);
-var_dump($smctoken);
+if($_REQUEST['action'] === 'dump'){
+    var_dump($user_person_id);
+    var_dump($smctoken);
+}else{
+    echo $cont;
+}
