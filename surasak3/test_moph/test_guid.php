3694e93657b09cb887eb3b7e0b38f81ba094c887
DESKTOP-9PGQ19T\kritsanasak <roboconk@gmail.com>
2021-04-06 21:38:20 +0700

    ลองปรับ uuid v4

--- surasak3/test_moph/test_guid.php
+++ surasak3/test_moph/test_guid.php
@@ -1,40 +1,26 @@
-<?php
-function dump($data = null){ 
-
-    $prefix = hash_hmac("sha256", uniqid(), uniqid());
-    $start = rand(1,20);
-    $data = substr($prefix,$start,32);
-    assert(strlen($data) == 16);
-    // Set version to 0100
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    // Set bits 6-7 to 10
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-
-    var_dump($data);
-
-    // $data = $data ?? random_bytes(16);
-    // assert(strlen($data) == 16);
-    
-    // Set version to 0100
-    // $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    // Set bits 6-7 to 10
-    // $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-    // $test1 = bin2hex($data);
-    // var_dump($test1);
-    // $test = str_split(bin2hex($data), 4);
-    // var_dump($test);
-
-    // preg_match("/(\w{0,8})(\w{0,4})(\w{0,4})(\w{0,4})(\w{0,12})/", $test1, $matchs);
-
-    // var_dump($matchs);
-
-    // $test = $matchs[1].'-'.$matchs[2].'-'.$matchs[3].'-'.$matchs[4].'-'.$matchs[5];
-    // var_dump($test);
-    // vsprintf("%04d%04d%04d%04d%04d%04d%04d%04d", $test);
-    // Output the 36 character UUID.
-    // vsprintf('%s%s-%s-%s-%s-%s%s%s', $test);
-    // return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+<?php 
+function getRandomInt($max){
+    $ii = false;
+    for($i = 1; $i <= $max; $i++)
+    {
+        $i1 = (string) rand(1, 255);
+        if($i == 7)
+        {
+            $i2 = chr(rand(64, 79));
+        }
+        else
+        {
+            $i2 = chr($i1);
+        }
+        $ii .= bin2hex($i2);
+    }
+    return $ii;
 }
-dump();
+
+function generate_uuidv4(){ 
+    $rand_int = getRandomInt(16);
+    $split4 = str_split($rand_int, 4);
+    $resVs = vsprintf("%04s%04s-%04s-%04s-%04s-%04s%04s%04s", $split4);
+     return $resVs;
+}
+echo strtoupper(generate_uuidv4());
