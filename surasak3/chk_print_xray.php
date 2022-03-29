fb946cc120f12b4a9cf75f6b90fdcd9a5c507025
Kritsanasak <roboconk@gmail.com>
2022-03-29 20:03:52 +0700

    Update chk_print_xray.php

--- surasak3/chk_print_xray.php
+++ surasak3/chk_print_xray.php
@@ -18,8 +18,8 @@ $id = $_REQUEST['id'];
     <iframe src="chk_print_xray_page.php?id=<?=$id;?>" frameborder="0" name="xrayPage" id="xrayPage" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
 	<script>
         window.onload = function(){ 
-            // window.frames["xrayPage"].focus();
-            // window.frames["xrayPage"].print();
+            window.frames["xrayPage"].focus();
+            window.frames["xrayPage"].print();
         }
     </script>
 </body>
