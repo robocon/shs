ff5b41b9f07936ee31e94783d95d1cf9f7598e05
Kritsanasak <roboconk@gmail.com>
2022-07-18 13:59:07 +0700

    ปรับฟอร์มใหม่

--- surasak3/dt_stat_opd.php
+++ surasak3/dt_stat_opd.php
@@ -1,8 +1,7 @@
 <?php 
 include 'bootstrap.php';
 
-// $db = new mysqli(HOST,USER,PASS,DB);
-$db = new mysqli('192.168.131.250','remoteuser','',DB);
+$db = new mysqli(HOST,USER,PASS,DB);
 
 $default_date = ( date('Y') + 543 ).date('-m-d');
 $date = input_post('date', $default_date);
@@ -42,6 +41,9 @@ $show = input_post('show');
 
 <form action="dt_stat_opd.php" method="post" id="userForm">
 	<div>
+		<h3>ระบบรายงานยอดผู้มารับบริการแยกตามแพทย์และสิทธิ</h3>
+	</div>
+	<div>
 		<label for="date">เลือกวันที่</label>
 		<input type="text" id="date" name="date" value="<?=$date;?>">
 	</div>
@@ -58,11 +60,12 @@ if( $show !== false ){
 
 	if(empty($d))
 	{
-		echo 'กรุณาใส่วันที่ <a href="dt_stat_opd.php">ย้อนกลับ</a>';
+		echo 'กรุณาใส่วันที่ให้ถูกต้อง<br>ตัวอย่างรูปแบบวันที่ ปี(พ.ศ.)-เดือน-วัน <br>เช่น 2565-07-01 เป็นต้น<br> <a href="dt_stat_opd.php">ย้อนกลับ</a>';
 		exit;
 	}
 
 	$pre_items = array();
+	$room_items = array();
 	// query เอาชื่อหมอ
 	$sql = "SELECT `doctor`,`room`,`hn` FROM `dt_logs` WHERE `thdatehn` LIKE '$d-$m-$y%' AND `doctor` NOT LIKE 'HD%' GROUP BY `doctor` ORDER BY `room` ASC";
 	$q = $db->query($sql);
@@ -70,8 +73,8 @@ if( $show !== false ){
 
 		$dt_name = $a['doctor'];
 
+		$room_items[$dt_name] = $a['room'];
 		
-
 		$sql_dt = "SELECT a.*, b.`ptname`,b.`goup`, b.`ptright` 
 		FROM ( 
 			SELECT `thdatehn`,`vn`,`hn` FROM `dt_logs` WHERE `doctor` = '$dt_name' 
@@ -81,30 +84,27 @@ if( $show !== false ){
 		while ($b = $qdt->fetch_assoc()) { 
 
 			$find_hn = $b['hn'];
-			// $goup_code = substr($b['goup'],0,3);
-			// dump($goup_code);
 
 			$q_opcard = $db->query("SELECT `yot` FROM `opcard` WHERE `hn` = '$find_hn' LIMIT 1 ");
 			$op = $q_opcard->fetch_assoc();
-			// $yot = trim($op['yot']);
 			$b['yot'] = trim($op['yot']);
 
 			$pre_items[$dt_name][] = $b;
-
 			
-
-
-
-			
-			// $doctor_list[$dt_name][] = $b;
 		}
 		
 	} // จบชื่อหมอ
 
+	if(empty($pre_items)){
+		?>
+		<p><b>ไม่พบข้อมูลการออกตรวจ</b></p>
+		<?php
+		exit;
+	}
+
 
 	$count_dt = array();
 	foreach ($pre_items as $dt_name => $items) { 
-		dump(count($items));
 
 		$count_dt[$dt_name]['officer'] = 0; //นายทหารสัญญาบัตร
 		$count_dt[$dt_name]['nco'] = 0; //นายทหารชั้นประทวน
@@ -118,28 +118,20 @@ if( $show !== false ){
 		$user_runno = 0;
 		foreach ($items as $key => $item) {
 			
-
 			$yot = trim($item['yot']);
 			
 			$test_yot = false;
 			if( $yot=='ร.ต.' || $yot=='ร.ท.' || $yot=='ร.อ.' || $yot=='พ.ต.' || $yot=='พ.ท.' || $yot=='พ.อ.' || $yot=='พลตรี' ){ 
 				$count_dt[$dt_name]['officer']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_yot = true;
 
 			}elseif( $yot=='ส.ต.' || $yot=='ส.ท.' || $yot=='ส.อ.' || $yot=='จ.ส.ต.' || $yot=='จ.ส.ท.' || $yot=='จ.ส.อ.' || $yot=='พลอาสา' || $yot=='พลอาสาสมัคร' ){ 
 				$count_dt[$dt_name]['nco']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_yot = true;
 
 			}elseif( $yot=='พลฯ' ){ 
 				$count_dt[$dt_name]['pvt']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_yot = true;
-
 			}
 
 			if($test_yot === true)
@@ -147,13 +139,11 @@ if( $show !== false ){
 				$user_runno++;
 				continue;
 			}
-
 			
 			$goup_code = trim(substr($item['goup'],0,3));
 			$test_goup = false;
 			if($goup_code=='G31'){
 				$count_dt[$dt_name]['family']++;
-				// unset($pre_items[$dt_name][$user_runno]);
 				$test_goup = true;
 				$user_runno++;
 				continue;
@@ -163,22 +153,15 @@ if( $show !== false ){
 			$test_ptright = false;
 			if($ptright_code=='R02' || $ptright_code=='R03' || $ptright_code=='R04'){
 				$count_dt[$dt_name]['r020304']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_ptright = true;
 
 			}elseif ($ptright_code=='R07') {
 				$count_dt[$dt_name]['r07']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_ptright = true;
 
 			}elseif ($ptright_code=='R06' || $ptright_code=='R08') {
 				$count_dt[$dt_name]['r0608']++;
-				// unset($pre_items[$dt_name][$user_runno]);
-				// continue;
 				$test_ptright = true;
-
 			}
 
 			if($test_ptright===true)
@@ -190,32 +173,95 @@ if( $show !== false ){
 			if($test_yot===false && $test_goup===false && $test_ptright===false){
 				$count_dt[$dt_name]['30bath']++;
 			}
-			
-			// unset($pre_items[$dt_name][$user_runno]);
-
-			
 		}
-		
-		
-		echo "<hr>";
-		# code...
 	}
 
-	dump($count_dt);
+	$pts = array(
+		'officer' => 'นายทหารสัญญาบัตร',
+		'nco' => 'นายทหารประทวน',
+		'pvt' => 'พลทหาร',
+		'family' => 'ครอบครัวทหาร',
+		'r020304' => 'สิทธิจ่ายตรง/ต้นฯ/รัฐฯ',
+		'r07' => 'ประกันสังคม',
+		'r0608' => 'พรบ./กท.44',
+		'30bath' => 'พลเรือน/30บาท'
+	);
+
+	$ptright_sum = array(
+		'officer' => 0,
+		'nco' => 0,
+		'pvt' => 0,
+		'family' => 0,
+		'r020304' => 0,
+		'r07' => 0,
+		'r0608' => 0,
+		'30bath' => 0,
+	);
 
 	?>
+	<div align="center"><h3>ยอดผู้มารับบริการแยกตามแพทย์และสิทธิ วันที่ <?=$d.' '.$def_fullm_th[$m].' '.$y;?></h3></div>
 	<table>
 		<tr>
-			<th></th>
+			<th>ห้องตรวจ</th>
+			<th>ชื่อแพทย์</th>
+			<?php
+			foreach ($pts as $key => $p) {
+				?>
+				<th><?=$p;?></th>
+				<?php
+			}
+			?>
+			<th>ยอดรวมตามแพทย์</th>
+		</tr>
+		<?php 
+		foreach ($count_dt as $key => $values) { 
+
+			$room_code = $room_items[$key];
+			$qr = $db->query("SELECT `detail` FROM `queue_room` WHERE `code` = '$room_code' ");
+			$room_name = '-';
+			if($qr->num_rows > 0){
+				$r = $qr->fetch_assoc();
+				$room_name = $r['detail'];
+			}
+			
+			?>
+			<tr>
+				<td><?=$room_name;?></td>
+				<td><?=$key;?></td>
+				<?php
+				$sum = 0;
+				foreach ($values as $key2 => $ptright) {
+					?>
+					<td align="right"><?=$ptright;?></td>
+					<?php
+					$sum += (int)$ptright;
+
+					$ptright_sum[$key2] += (int)$ptright;
+				}
+				?>
+				<td align="right"><b><?=$sum;?></b></td>
+
+			</tr>
+			<?php
+		}
+		?>
+		<tr>
+			<td colspan="2" align="center">ยอดรวมตามสิทธิ์</td>
+			<?php
+			$all = 0;
+			foreach ($pts as $key => $p) {
+
+				$sum_item = $ptright_sum[$key];
+				$all += $sum_item;
+				?>
+				<td align="right"><b><?=$sum_item;?></b></td>
+				<?php
+			}
+			?>
+			<td align="right"><b><?=$all;?></b></td>
 		</tr>
 	</table>
 	<?php
-
-
-
-
-
-
 
 	exit;
 
