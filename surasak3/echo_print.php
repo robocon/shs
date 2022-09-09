480cb6693c7ac53677f2144e011569ee71db9dfd
surasak11512 <surasakh12345@gmail.com>
2022-09-09 08:35:19 +0700

    Update echo_print.php

--- surasak3/echo_print.php
+++ surasak3/echo_print.php
@@ -42,15 +42,15 @@ list($h,$i,$s) = explode(':', $mTime);
 list($y,$m,$d) = ad_to_bc(explode('-', $mDate));
 $th_date = $d.' '.$def_month_th[$m].' '.$y;
 ?>
-<table border="1">
+<table width="100%">
     <tr style="border-bottom: 1px solid #000000;">
         <td colspan="2">
             <table width="100%">
                 <tr>
-                    <td align="center" width="30%" style="border-right: 1px solid #000000; padding:4px;">
+                    <td align="center" width="40%" style="border-right: 1px solid #000000; padding:4px;">
                         <table>
                             <tr>
-                                <td><img src="images/LogoFSH.jpg" alt="" style="width:50px;"></td>
+                                <td><img src="images/LogoFSH.jpg" alt="" style="height:60px;">&nbsp;&nbsp;</td>
                                 <td>
                                     <h3>โรงพยาบาลค่ายสุรศักดิ์มนตรี</h3>
                                     <h3>ECHOCARDIOGRAPHY REPORT</h3>
@@ -58,7 +58,7 @@ $th_date = $d.' '.$def_month_th[$m].' '.$y;
                             </tr>
                         </table>
                     </td>
-                    <td valign="top" width="70%" style="padding:4px;">
+                    <td valign="top" width="60%" style="padding:4px;">
                         <table width="100%">
                             <tr>
                                 <td>
@@ -77,21 +77,21 @@ $th_date = $d.' '.$def_month_th[$m].' '.$y;
         </td>
     </tr>
     <tr>
-        <td valign="top" width="40%">
-            <table border="1" width="100%">
+        <td valign="top" width="50%">
+            <table width="100%">
                 <tr>
                     <td colspan="8"><u>MEASUREMENT</u></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right" width="10%">Ao&nbsp;</td>
-                    <td ><?=$a['ao'];?></td>
-                    <td width="19%">mm. (20-32)</td>
+                    <td align="right"><?=$a['ao'];?></td>
+                    <td width="19%">&nbsp;mm. (20-32)</td>
 
                     <td>&nbsp;</td>
                     <td align="right" width="10%">LVEDV&nbsp;</td>
-                    <td ><?=$a['lvedv'];?></td>
-                    <td width="19%">ml. (90-140)</td>
+                    <td align="right"><?=$a['lvedv'];?></td>
+                    <td width="19%">&nbsp;ml. (90-140)</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
@@ -123,113 +123,210 @@ $th_date = $d.' '.$def_month_th[$m].' '.$y;
                     
                     <td>&nbsp;</td>
                     <td align="right">CO&nbsp;</td>
-                    <td><input type="text" name="co" id="co" size="8" value="<?=$a['co'];?>"></td>
+                    <td><?=$a['co'];?></td>
                     <td>L/min</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right">LVDd&nbsp;</td>
-                    <td><input type="text" name="lvdd" id="lvdd" size="8" value="<?=$a['lvdd'];?>"></td>
+                    <td><?=$a['lvdd'];?></td>
                     <td>mm. (37-55)</td>
                     
                     <td>&nbsp;</td>
                     <td align="right">EF&nbsp;</td>
-                    <td><input type="text" name="ed" id="ed" size="8" value="<?=$a['ed'];?>"></td>
+                    <td><?=$a['ed'];?></td>
                     <td>% (50-80)</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right">LVDs&nbsp;</td>
-                    <td><input type="text" name="lvds" id="lvds" size="8" value="<?=$a['lvds'];?>"></td>
+                    <td><?=$a['lvds'];?></td>
                     <td>mm. (22-45)</td>
                     
                     <td>&nbsp;</td>
                     <td align="right">peak E&nbsp;</td>
-                    <td><input type="text" name="peake" id="peake" size="8" value="<?=$a['peake'];?>"></td>
+                    <td><?=$a['peake'];?></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right">Pwd&nbsp;</td>
-                    <td><input type="text" name="pwd" id="pwd" size="8" value="<?=$a['pwd'];?>"></td>
+                    <td><?=$a['pwd'];?></td>
                     <td>mm. (7-11)</td>
                     
                     <td>&nbsp;</td>
                     <td align="right">peak A&nbsp;</td>
-                    <td><input type="text" name="peaka" id="peaka" size="8" value="<?=$a['peaka'];?>"></td>
+                    <td><?=$a['peaka'];?></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right">Pws&nbsp;</td>
-                    <td><input type="text" name="pws" id="pws" size="8" value="<?=$a['pws'];?>"></td>
+                    <td><?=$a['pws'];?></td>
                     <td>mm. (11-17)</td>
                     
                     <td>&nbsp;</td>
                     <td align="right">E/A&nbsp;</td>
-                    <td><input type="text" name="ea" id="ea" size="8" value="<?=$a['ea'];?>"></td>
+                    <td><?=$a['ea'];?></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td align="right">FS&nbsp;</td>
-                    <td><input type="text" name="fs" id="fs" size="8" value="<?=$a['fs'];?>"></td>
+                    <td><?=$a['fs'];?></td>
                     <td>% (25-44)</td>
                     
                     <td>&nbsp;</td>
                     <td align="right">DT&nbsp;</td>
-                    <td><input type="text" name="dt" id="dt" size="8" value="<?=$a['dt'];?>"></td>
+                    <td><?=$a['dt'];?></td>
                     <td></td>
                 </tr>
             </table>
-            <table border="1" width="100%">
+            <table width="100%">
                 <tr>
                     <td colspan="7"><u>DOPPLER</u></td>
                 </tr>
                 <tr>
                     <td>1.</td>
-                    <td>MS</td>
-                    <td>NO</td>
+                    <td align="right" width="16%">MS&nbsp;</td>
+                    <td style="width:96px;"><?=$a['ms'];?></td>
                     <td>&nbsp;</td>
-                    <td>MnGRAD</td>
-                    <td>&nbsp;</td>
+
+                    <td align="right" width="16%">MnGRAD&nbsp;</td>
+                    <td style="width:96px;"><?=$a['ms_mngrad'];?></td>
                     <td>mmHg</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td>MVA(PHT)</td>
-                    <td>&nbsp;</td>
+                    <td align="right">MVA(PHT)&nbsp;</td>
+                    <td><?=$a['ms_mvapht'];?></td>
                     <td>cm<sup>2</sup></td>
-                    <td>MVA(2-D)</td>
-                    <td>&nbsp;</td>
+
+                    <td align="right">MVA(2-D)&nbsp;</td>
+                    <td><?=$a['ms_mva2d'];?></td>
                     <td>cm<sup>2</sup></td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td>MR</td>
-                    <td>TRIVIAL</td>
+                    <td align="right">MR&nbsp;</td>
+                    <td><?=$a['ms_mr'];?></td>
                     <td>mmHg</td>
+
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>2.</td>
+                    <td align="right">AS&nbsp;</td>
+                    <td><?=$a['as'];?></td>
+                    <td>&nbsp;</td>
+
+                    <td align="right">PGRAD&nbsp;</td>
+                    <td><?=$a['as_pgrad'];?></td>
+                    <td>mmHg</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">MnGRAD&nbsp;</td>
+                    <td><?=$a['as_mngrad'];?></td>
+                    <td>mmHg</td>
+
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">AR&nbsp;</td>
+                    <td><?=$a['as_ar'];?></td>
+                    <td></td>
+                    
+                    <td align="right">AI PHT&nbsp;</td>
+                    <td><?=$a['as_aipht'];?></td>
+                    <td>ms.</td>
+                </tr>
+
+                <tr>
+                    <td>3.</td>
+                    <td align="right">PS&nbsp;</td>
+                    <td><?=$a['ps'];?></td>
+                    <td>&nbsp;</td>
+
+                    <td align="right">PGRAD&nbsp;</td>
+                    <td><?=$a['ps_pgrad'];?></td>
+                    <td>mmHg</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">MnGRAD&nbsp;</td>
+                    <td><?=$a['ps_mngrad'];?></td>
+                    <td>mmHg</td>
+
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">PR&nbsp;</td>
+                    <td><?=$a['ps_pr'];?></td>
+                    <td>&nbsp;</td>
+                    
+                    <td align="right">PGRAD&nbsp;</td>
+                    <td><?=$a['ps_pr_pgrad'];?></td>
+                    <td>mmHg</td>
+                </tr>
+                
+                <tr>
+                    <td>4.</td>
+                    <td align="right">TS&nbsp;</td>
+                    <td><?=$a['ts'];?></td>
+                    <td>&nbsp;</td>
+
+                    <td align="right">MnGRAD&nbsp;</td>
+                    <td><?=$a['ts_mngrad'];?></td>
+                    <td>mmHg</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">TVA(PHT)&nbsp;</td>
+                    <td><?=$a['ts_tvapht'];?></td>
+                    <td>cm<sup>2</sup></td>
+
+                    <td align="right">TVA(2-D)&nbsp;</td>
+                    <td><?=$a['ts_tva2d'];?></td>
+                    <td>cm<sup>2</sup></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td align="right">TR&nbsp;</td>
+                    <td><?=$a['ts_tr'];?></td>
+                    <td>&nbsp;</td>
+                    
+                    <td align="right">RVSP&nbsp;</td>
+                    <td><?=$a['ts_rvsp'];?></td>
+                    <td>mmHg</td>
                 </tr>
             </table>
         </td>
-        <td valign="top" width="60%">
-            <table border="1" width="100%">
+        <td valign="top" width="50%" style="padding-left: 4px;">
+            <table width="100%">
                 <tr>
                     <td><u>ECHOCARDIOGRAPHIC FINDING:</u></td>
                 </tr>
                 <tr>
-                    <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</td>
+                    <td><?=$a['cardio_finding'];?></td>
                 </tr>
                 <tr>
-                    <td>DIAGNOSIS: <b>Good MV prosthetic value and LV function</b></td>
+                    <td>DIAGNOSIS: <b><?=$a['diag'];?></b></td>
                 </tr>
                 <tr>
                     <td>
                         <p>Doctor: .........................</p>
-                        <p>พญ. ณัชญ์ระวี บุรีคำ</p>
+                        <!-- <p>พญ. ณัชญ์ระวี บุรีคำ</p> -->
+                        <p><?=$a['doctor'];?></p>
                     </td>
                 </tr>
             </table>
