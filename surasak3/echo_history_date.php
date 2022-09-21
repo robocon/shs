ceb970404cea396f3c7ad22bc4a7a5d264294cc6
Kritsanasak <roboconk@gmail.com>
2022-09-21 09:49:46 +0700

    ปรับเมนู echo ในหน้าจอแพทย์

--- surasak3/echo_history_date.php
+++ surasak3/echo_history_date.php
@@ -3,7 +3,7 @@ require_once 'bootstrap.php';
 
 $dbi = new mysqli(HOST,USER,PASS,DB);
 $hn = $dbi->escape_string($_GET['hn']);
-$q = $dbi->query("SELECT `id`,`date` FROM `echo_cardio` WHERE `hn` = '$hn' ");
+$q = $dbi->query("SELECT `id`,`date` FROM `echo_cardio` WHERE `hn` = '$hn' ORDER BY `id` DESC ");
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
@@ -23,12 +23,12 @@ a:active {color:#FF0000; text-decoration:underline;}
 a:hover {color:#FF0000; text-decoration:underline;}
 
 body,td,th {
-	font-family:  Angsana New;
+	font-family:"TH Sarabun New","TH SarabunPSK";
 	font-size: 20 px;
 }
 
 .font_title{
-	font-family:  Angsana New;
+	font-family:"TH Sarabun New","TH SarabunPSK";
 	font-size: 20 px;
 	color:#FFFFFF;
 	font-weight: bold;
@@ -36,25 +36,22 @@ body,td,th {
 }
 </style>
 </HEAD>
-
 <BODY>
-
-<h3>เลือกวันที่</h3>
-
-
-
-<?php
-while ($a=$q->fetch_assoc()) {
+<h3>เลือกวันที่เพื่อแสดงรายการ</h3>
+<ol style="margin: 0; padding: 4px 0 4px 12px;">
+<?php 
+$i = 1;
+while ($a=$q->fetch_assoc()) { 
+	$bg = 'style="background-color: #e5e5e5;"';
+	if($i%2===0){
+		$bg = 'style="background-color: #F0F4BF;"';
+	}
     ?>
-    <a href="echo_print.php?id=<?=$a['id'];?>&print=noprint" target="right"><?=$a['date'];?></a>
+    <li <?=$bg;?>><a href="echo_print.php?id=<?=$a['id'];?>&print=noprint" target="right"><?=$a['date'];?></a></li>
     <?php
+	$i++;
 }
-
-
 ?>
-
-
-
-
+</ol>
 </BODY>
 </HTML>
