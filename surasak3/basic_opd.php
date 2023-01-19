88f8f0764e694a8611ea27f4b118bb69781828f5
surasak11512 <surasakh12345@gmail.com>
2023-01-19 08:22:44 +0700

    ไปขี้แปร๊บเดี๋ยวลืม

--- surasak3/basic_opd.php
+++ surasak3/basic_opd.php
@@ -1701,16 +1701,76 @@ mmHg </td>
 			<td></td>
 			<td colspan="4">&nbsp;</td>
 		</tr>
+		<?php 
+		if($_SESSION['smenucode'] == 'ADMEYE')
+		{
+			$eye_thdatehn = $thidate.$cHn;
+			$sql = "SELECT * FROM `pt_opd_eye` WHERE `thdatehn` = '$eye_thdatehn' ";
+			$q = $dbi->query($sql);
+			$eye = array();
+			if($q->num_rows > 0)
+			{
+				$eye = $q->fetch_assoc();
+			}
+
+		?>
 		<tr>
 			<td colspan="5">
 				<div style="border: 1px solid red; width: 100%;">&nbsp;</div>
 				<fieldset>
 					<legend style="font-weight:bold;">ฟอร์มบันทึกห้องตา</legend>
+					<table>
+						<tr class="data_show" style="vertical-align: top;">
+							<td align="right">ยาต้านการแข็งตัว :<br>ของเกล็ดเลือด</td>
+							<td colspan="5">
+								<?php 
+								$antiplatelet_list = array(
+									'ไม่มี', 'มี'
+								);
 
+								if(empty($eye['antiplatelet_txt']))
+								{
+									$eye['antiplatelet'] = 'ไม่มี';
+								}
+
+								foreach ($antiplatelet_list as $key => $platelet) 
+								{ 
+									$default_platelet = ($platelet == $eye['antiplatelet']) ? 'checked="checked"' : '' ;
+									?>
+									<input type="radio" name="antiplatelet" id="antiplatelet<?=$key;?>" onclick="focus_antiplatelet<?=$key;?>()" value="<?=$platelet;?>" <?=$default_platelet;?> ><label for="antiplatelet<?=$key;?>"><?=$platelet;?></label>
+									<?php
+								}
+								?>
+								<input type="text" name="antiplatelet_txt" id="antiplatelet_txt" onfocus="focus_antiplate_txt()" onfocusout="unfocus_antiplate_txt()" value="<?=$eye['antiplatelet_txt'];?>">
+								<script type="text/javascript">
+
+									function focus_antiplatelet0(){
+										document.getElementById('antiplatelet_txt').value = '';
+									}
+									function focus_antiplatelet1(){
+										document.f2.antiplatelet_txt.focus();
+									}
+
+									function focus_antiplate_txt(){
+										document.f2.antiplatelet1.checked = true;
+									}
+									function unfocus_antiplate_txt(){
+										if(document.getElementById('antiplatelet_txt').value==''){
+											document.f2.antiplatelet1.checked = false;
+										}
+									}
+								</script>
+							</td>
+						</tr>
+					</table>
 
 				</fieldset>
 			</td>
 		</tr>
+		<?php
+		} // End form ห้องตา
+		?>
+		
 		<?php 
 		  if($_SESSION['smenucode'] == 'ADMEYE')
 		  {
@@ -1778,199 +1838,7 @@ mmHg </td>
 		if( $_SESSION['smenucode'] == 'ADMEYE')
 		{
 		?>
-		<tr>
-			<td align="left" colspan="6">
-				<table width="100%">
-					<tr>
-						<td><b style="font-weight:bold; font-size: 22px; text-decoration: underline;">EYE screening</b>&nbsp;&nbsp;VA</td>
-						<td>R <input type="text" name="esr" id="esr" value="<?=$eye['esr'];?>"></td>
-						<td>PH <input type="text" name="esr_ph" id="esr_ph" value="<?=$eye['esr_ph'];?>"></td>
-						<td>with glass <input type="text" name="esr_glass" id="esr_glass" value="<?=$eye['esr_glass'];?>"></td>
-						<td>NOT <input type="text" name="esr_not" id="esr_not" value="<?=$eye['esr_not'];?>"></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td>L <input type="text" name="esl" id="esl" value="<?=$eye['esl'];?>"></td>
-						<td>PH <input type="text" name="esl_ph" id="esl_ph" value="<?=$eye['esl_ph'];?>"></td>
-						<td>with glass <input type="text" name="esl_glass" id="esl_glass" value="<?=$eye['esl_glass'];?>"></td>
-						<td>NOT <input type="text" name="esl_not" id="esl_not" value="<?=$eye['esl_not'];?>"></td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-
-		<tr>
-			<td colspan="6" style="font-weight:bold; font-size: 22px; text-decoration: underline;">ข้อวินิจฉัยทางการพยาบาล Nursing DX:</td>
-		</tr>
-		<tr>
-			<td colspan="6">
-				<?php 
-				$nurse_dx1 = (!empty($eye['nurse_dx1'])) ? 'checked="checked"' : '' ;
-				$nurse_dx2 = (!empty($eye['nurse_dx2'])) ? 'checked="checked"' : '' ;
-				$nurse_dx3 = (!empty($eye['nurse_dx3'])) ? 'checked="checked"' : '' ;
-				$nurse_dx4 = (!empty($eye['nurse_dx4'])) ? 'checked="checked"' : '' ;
-				$nurse_dx5 = (!empty($eye['nurse_dx5'])) ? 'checked="checked"' : '' ;
-				?>
-				<table style="min-width: 800px;">
-					<tr>
-						<td><input type="checkbox" name="nurse_dx1" id="nurse_dx1" value="มีโอกาส/เสี่ยงต่อการเกิดภาวะแทรกซ้อนของโรค" <?=$nurse_dx1;?> > <label for="nurse_dx1">มีโอกาส/เสี่ยงต่อการเกิดภาวะแทรกซ้อนของโรค</label><input type="text" name="nurse_dx1_txt" id="nurse_dx1_txt" value="<?=$eye['nurse_dx1_txt'];?>" ></td>
-						<td><input type="checkbox" name="nurse_dx2" id="nurse_dx2" value="ต้องการข้อมูลเกี่ยวกับการให้บริการ" <?=$nurse_dx2;?>> <label for="nurse_dx2">ต้องการข้อมูลเกี่ยวกับการให้บริการ</label><input type="text" name="nurse_dx2_txt" id="nurse_dx2_txt" value="<?=$eye['nurse_dx2_txt'];?>" ></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="nurse_dx3" id="nurse_dx3" value="ต้องการความรู้/การปรึกษาเรื่อง" <?=$nurse_dx3;?>> <label for="nurse_dx3">ต้องการความรู้/การปรึกษาเรื่อง</label><input type="text" name="nurse_dx3_txt" id="nurse_dx3_txt" value="<?=$eye['nurse_dx3_txt'];?>" ></td>
-						<td><input type="checkbox" name="nurse_dx4" id="nurse_dx4" value="ไม่สุขสบาย: ปวด, เคืองตา" <?=$nurse_dx4;?>> <label for="nurse_dx4">ไม่สุขสบาย: ปวด, เคืองตา</label></td>
-					</tr>
-					<tr>
-						<td colspan="2"><input type="checkbox" name="nurse_dx5" id="nurse_dx5" value="เสี่ยงต่อการเกิดอุบัติเหตุ เนื่องจากการมองเห็นลดลง" <?=$nurse_dx5;?>> <label for="nurse_dx5">เสี่ยงต่อการเกิดอุบัติเหตุ เนื่องจากการมองเห็นลดลง</label></td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<script type="text/javascript">
-			document.getElementById('nurse_dx1').onclick = function(){
-				
-				if(this.checked==true)
-				{
-					document.getElementById('nurse_dx1_txt').focus();
-				}
-				else
-				{
-					document.getElementById('nurse_dx1_txt').value = '';
-				}
-			};
-
-			document.getElementById('nurse_dx2').onclick = function(){
-				if(this.checked==true)
-				{
-					document.getElementById('nurse_dx2_txt').focus();
-				}
-				else
-				{
-					document.getElementById('nurse_dx2_txt').value = '';
-				}
-			};
-
-			document.getElementById('nurse_dx3').onclick = function(){
-				if(this.checked==true)
-				{
-					document.getElementById('nurse_dx3_txt').focus();
-				}
-				else
-				{
-					document.getElementById('nurse_dx3_txt').value = '';
-				}
-			};
-		</script>
-
-		<tr>
-			<td colspan="6" style="font-weight:bold; font-size: 22px; text-decoration: underline;"><b>การพยาบาลและการประเมินผล Implementation</b></td>
-		</tr>
-		<tr>
-			<td colspan="6">
-				<?php 
-				$imp1 = (!empty($eye['imp1'])) ? 'checked="checked"' : '' ;
-				$imp2 = (!empty($eye['imp2'])) ? 'checked="checked"' : '' ;
-				$imp3 = (!empty($eye['imp3'])) ? 'checked="checked"' : '' ;
-				$imp4 = (!empty($eye['imp4'])) ? 'checked="checked"' : '' ;
-				$imp5 = (!empty($eye['imp5'])) ? 'checked="checked"' : '' ;
-				$imp6 = (!empty($eye['imp6'])) ? 'checked="checked"' : '' ;
-				?>
-				<table>
-					<tr>
-						<td colspan="2"><input type="checkbox" name="imp1" id="imp1" value="เฝ้าระวังการเกิด fall" <?=$imp1;?> ><label for="imp1">เฝ้าระวังการเกิด fall</label></td>
-					</tr>
-					<tr>
-						<td colspan="2"><input type="checkbox" name="imp2" id="imp2" value="ให้ความรู้/การปรึกษาเรื่อง" <?=$imp2;?>><label for="imp2">ให้ความรู้/การปรึกษาเรื่อง</label><input type="text" name="imp2_txt" id="imp2_txt" value="<?=$eye['imp2_txt'];?>"></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="imp3" id="imp3" value="แนะนำวิธีการใช้ยาตามแผนการรักษาของแพทย์" <?=$imp3;?>><label for="imp3">แนะนำวิธีการใช้ยาตามแผนการรักษาของแพทย์</label></td>
-						<td><input type="checkbox" name="imp4" id="imp4" value="เฝ้าระวังการเปลี่ยนแปลงขณะรอ Laser หลังหยอดตา" <?=$imp4;?>><label for="imp4">เฝ้าระวังการเปลี่ยนแปลงขณะรอ Laser หลังหยอดตา</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="imp5" id="imp5" value="ประเมินศักยภาพในการดูแลตนเอง" <?=$imp5;?>><label for="imp5">ประเมินศักยภาพในการดูแลตนเอง</label></td>
-						<td><input type="checkbox" name="imp6" id="imp6" value="บรรเทาอาการเจ็บปวด ดูแล" <?=$imp6;?>><label for="imp6">บรรเทาอาการเจ็บปวด ดูแล</label><input type="text" name="imp6_txt" id="imp6_txt" value="<?=$eye['imp6_txt'];?>"></td>
-					</tr>
-				</table>
-				<script type="text/javascript">
-					document.getElementById('imp2').addEventListener('click', function () {
-						if(this.checked==true)
-						{
-							document.getElementById('imp2_txt').focus();
-						}
-						else
-						{
-							document.getElementById('imp2_txt').value = '';
-						}
-					}, false);
-
-					document.getElementById('imp6').onclick = function(){
-						if(this.checked==true)
-						{
-							document.getElementById('imp6_txt').focus();
-						}
-						else
-						{
-							document.getElementById('imp6_txt').value = '';
-						}
-					};
-				</script>
-			</td>
-		</tr>
-
-		<tr>
-			<td colspan="6" style="font-weight:bold; font-size: 22px; text-decoration: underline;"><b>Evaluation</b></td>
-		</tr>
-		<tr>
-			<td colspan="6">
-				<?php 
-				$eva1 = (!empty($eye['eva1'])) ? 'checked="checked"' : '' ;
-				$eva2 = (!empty($eye['eva2'])) ? 'checked="checked"' : '' ;
-				$eva3 = (!empty($eye['eva3'])) ? 'checked="checked"' : '' ;
-				$eva4 = (!empty($eye['eva4'])) ? 'checked="checked"' : '' ;
-				$eva5 = (!empty($eye['eva5'])) ? 'checked="checked"' : '' ;
-				$eva6 = (!empty($eye['eva6'])) ? 'checked="checked"' : '' ;
-				$eva7 = (!empty($eye['eva7'])) ? 'checked="checked"' : '' ;
-				$eva8 = (!empty($eye['eva8'])) ? 'checked="checked"' : '' ;
-				$eva9 = (!empty($eye['eva9'])) ? 'checked="checked"' : '' ;
-				$eva10 = (!empty($eye['eva10'])) ? 'checked="checked"' : '' ;
-				?>
-				<table>
-					<tr>
-						<td>ให้คำแนะนำตาม D METHOD</td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva1" id="eva1" value="ผู้ป่วยมีความรู้เรื่องโรคที่เป็น" <?=$eva1;?> > <label for="eva1">ผู้ป่วยมีความรู้เรื่องโรคที่เป็น</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva2" id="eva2" value="แนะนำวิธีการใช้ยาตามแผนการรักษาของแพทย์" <?=$eva2;?>> <label for="eva2">แนะนำวิธีการใช้ยาตามแผนการรักษาของแพทย์</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva3" id="eva3" value="แนะนำการระมัดระวังพลัดตกหกล้ม" <?=$eva3;?>> <label for="eva3">แนะนำการระมัดระวังพลัดตกหกล้ม</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva4" id="eva4" value="สังเกตอาการผิดปกติ ถ้าตาแดงมากขึ้น ปวดตามาก น้ำตาไหล การมองเห็นลดลงให้มาพบแพทย์" <?=$eva4;?>> <label for="eva4">สังเกตอาการผิดปกติ ถ้าตาแดงมากขึ้น ปวดตามาก น้ำตาไหล การมองเห็นลดลงให้มาพบแพทย์</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva5" id="eva5" value="เน้นย้ำการมาตรวจตามนัด" <?=$eva5;?>> <label for="eva5">เน้นย้ำการมาตรวจตามนัด</label> <input type="checkbox" name="eva6" id="eva6" value="รักษาตามสิทธิ" <?=$eva6;?>><label for="eva6">รักษาตามสิทธิ</label> <input type="checkbox" name="eva7" id="eva7" value="ส่งตัวรักษาต่อ" <?=$eva7;?>><label for="eva7">ส่งตัวรักษาต่อ</label> <input type="checkbox" name="eva8" id="eva8" value="ไม่นัด" <?=$eva8;?>><label for="eva8">ไม่นัด</label> <input type="checkbox" name="eva9" id="eva9" value="ทานยาและหยอดยาตามแผนการักษา" <?=$eva9;?>><label for="eva9">ทานยาและหยอดยาตามแผนการักษา</label></td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="eva10" id="eva10" value="อื่นๆ" <?=$eva10;?>> <label for="eva10">อื่นๆ</label> <input type="text" name="eva10_txt" id="eva10_txt" value="<?=$eye['eva10_txt'];?>"></td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<script type="text/javascript">
-			document.getElementById('eva10').onclick = function(){
-				if(this.checked==true)
-				{
-					document.getElementById('eva10_txt').focus();
-				}
-				else
-				{
-					document.getElementById('eva10_txt').value = '';
-				}
-			};
-		</script>
+		
 		<?php 
 		}
 		?>
