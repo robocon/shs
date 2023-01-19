fc150f36ab07b5da8d3cbb629354c61eea7c94d8
kritsanasak kuntaros <roboconk@gmail.com>
2023-01-19 10:56:44 +0700

    ปรับเป็น utf8 จาก 6369252

--- surasak3/insert_basic_opd.php
+++ surasak3/insert_basic_opd.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // README! 
 // พิมพ์สติกเกอร์แบบ HTML สำหรับหน้าซักประวัติที่เป็นฟอร์มกรอกข้อมูล
 session_start();
@@ -42,18 +46,28 @@ function calcage($birth){
 	return $pAge;
 }
 
-$sql = "Select thidate, vn, hn, ptname , temperature , pause , rate , weight , height , bp1 , bp2 , drugreact , congenital_disease , type , organ , doctor, clinic, cigarette,alcohol,painscore,age,bp3,bp4,waist,`mens`,`mens_date`,`vaccine`,`parent_smoke`,`parent_smoke_amount`,`parent_drink`,`parent_drink_amount`,`smoke_amount`,`drink_amount`,`ht_amount`,`dm_amount`,`hpi`,`grade`,`mind`,`the_pill`,`cvriskscore`,`cvriskscore_lab` From opd where thdatehn = '".$_GET["dthn"]."' limit 1 ";
+$sql = "Select thidate, vn, hn, ptname , temperature , pause , rate , weight , height , bp1 , bp2 , drugreact , congenital_disease , type , organ , doctor, clinic, cigarette,alcohol,painscore,age,bp3,bp4,waist,`mens`,`mens_date`,`vaccine`,`parent_smoke`,`parent_smoke_amount`,`parent_drink`,`parent_drink_amount`,`smoke_amount`,`drink_amount`,`ht_amount`,`dm_amount`,`hpi`,`grade`,`mind`,`the_pill` From opd where thdatehn = '".$_GET["dthn"]."' limit 1 ";
 
 $result_dt_hn = Mysql_Query($sql);
-list($thidate, $vn, $hn, $ptname , $temperature , $pause , $rate , $weight , $height , $bp1 , $bp2 , $drugreact , $congenital_disease , $type , $organ , $doctor, $clinic, $cigarette, $alcohol,$painscore,$age,$bp3,$bp4,$waist,$mens,$mens_date,$vaccine,$parent_smoke,$parent_smoke_amount,$parent_drink,$parent_drink_amount,$smoke_amount,$drink_amount,$ht_amount,$dm_amount,$hpi,$grade,$mind,$the_pill,$cvriskscore,$cvriskscore_lab) = Mysql_fetch_row($result_dt_hn);
+list($thidate, $vn, $hn, $ptname , $temperature , $pause , $rate , $weight , $height , $bp1 , $bp2 , $drugreact , $congenital_disease , $type , $organ , $doctor, $clinic, $cigarette, $alcohol,$painscore,$age,$bp3,$bp4,$waist,$mens,$mens_date,$vaccine,$parent_smoke,$parent_smoke_amount,$parent_drink,$parent_drink_amount,$smoke_amount,$drink_amount,$ht_amount,$dm_amount,$hpi,$grade,$mind,$the_pill) = Mysql_fetch_row($result_dt_hn);
 $thidate = substr($thidate,8,2)."-".substr($thidate,5,2)."-".substr($thidate,0,4)." ".substr($thidate,10);
 if($cigarette==0){$cigarette='ไม่สูบ';}
 else if($cigarette==1){$cigarette='สูบ '.$smoke_amount.' มวน/สัปดาห์';}
 else {$cigarette='เคยสูบ';};
 
-if($alcohol==0){$alcohol='ไม่ดื่ม';}
-else if($alcohol==1){$alcohol='ดื่ม '.$drink_amount.' แก้ว/สัปดาห์';}
-else {$alcohol='เคยดื่ม';};
+if($alcohol==0){
+	$alcohol='ไม่ดื่ม';
+}else if($alcohol==1){
+
+	if(intval($drink_amount)===0){
+		$alcohol='ดื่ม '.$drink_amount.' แก้ว/สัปดาห์';
+	}else{
+		$alcohol='ไม่ดื่ม';
+	}
+	
+}else{
+	$alcohol='เคยดื่ม';
+}
 
 if($drugreact == 0){
 	$congenital_disease .=" , ผู้ป่วยไม่แพ้ยา";
@@ -83,21 +97,51 @@ if($drugreact == 0){
 
 ?>
 
-<script language="javascript">
+<script type="text/javascript">
 window.onload = function(){
 	window.print();
-	setTimeout(function(){ 
-		window.close();
-	},1000);
+    setTimeout(function () { 
+        window.close();
+		<?php 
+		// หน้า reprint (rp_basic_opd.php) จะส่ง $_GET reprint=yes ไม่ต้องให้ redirect ทำงาน
+		// ถ้ามาจากหน้าบันทึกซักประวัติ จะสั่งให้หน้า parent (basic_opd.php) redirect กลับไปที่ basic_opd.php ให้กรอก HN คนต่อไป
+		if(!empty($_GET['reprint']) && $_GET['reprint'] != 'yes')
+		{
+			?>
+			// force parent redirect to basic_opd.php
+			window.opener.location.href = "basic_opd.php";
+			<?php
+		}
+		?>
+    }, 1000);
 }
 </script>
-
+<style>
+body{
+	margin: 0;
+	padding: 0;
+}
+table td, .display-sticker>div, p{
+	font-family: "Angsana New","TH SarabunPSK","TH Sarabun New";
+	line-height: 18.897637795px;
+	font-size: 14pt;
+}
+.underline{
+	width: 50px;
+    display: inline-block;
+    border-bottom: 1px dashed #000;
+}
+.underline_notfix{
+	display: inline-block;
+    border-bottom: 1px dashed #000;
+}
+</style>
 <table cellpadding="0" cellspacing="0" border="0" style="font-size:9pt;">
 	<tr>
-		<td>HN : <?=$hn;?>, VN:<?=$vn;?>, <?=$thidate;?> <?=$cAge;?></td>
+		<td>HN : <?=$hn;?>, <?=$thidate;?> <?=$cAge;?></td>
 	</tr>
 	<tr>
-		<td>T : <?=$temperature;?> C, P : <?=$pause;?> ครั้ง/นาที , R : <?=$rate;?> ครั้ง/นาที </td>
+		<td>VN:<?=$vn;?>, T : <?=$temperature;?> C, P : <?=$pause;?> ครั้ง/นาที , R : <?=$rate;?> ครั้ง/นาที </td>
 	</tr>
 	<tr>
 		<td>BP : <?=$bp1;?> / <?=$bp2;?> mmHg, นน : <?=$weight;?> กก., สส : <?=$height;?> ซม.</td>
@@ -105,8 +149,12 @@ window.onload = function(){
 	
 	<tr>
 		<td>
-		รอบเอว : <?=$waist;?> ซม., 
 		<?php 
+		if(!empty($waist))
+		{
+			?>รอบเอว : <?=$waist;?> ซม., <?php
+		}
+
 		if( !empty($bp3) && !empty($bp4) ){
 			?>
 			Repeat BP : <?=$bp3;?> / <?=$bp4;?> mmHg
@@ -174,7 +222,7 @@ window.onload = function(){
 		<td>Triage Gr. : <?=$grade;?> สภาวะจิตใจ : <?=$mind;?></td>
 	</tr>
 	<tr>
-		<td>ลักษณะ : <?=$type;?>, คลินิก : <?=substr($clinic,3);?></td>
+		<td>ลักษณะ : <?=$type;?>, คลินิก : <?=$clinic;?></td>
 	</tr>
 	<tr>
 		<td>โรคประจำตัว : <?=trim($congenital_disease);?></td>
@@ -199,13 +247,14 @@ window.onload = function(){
 		</tr>
 		<?php
 	}
-	$organ = trim($organ);
-	if(!empty($organ)){
+
+	if(!empty($organ))
+	{
 		?>
 		<tr>
 			<td>อาการ : <?=trim($organ);?></td>
 		</tr>
-		<?php 
+		<?php
 	}
 
 	if ( !empty($hpi) ) {
@@ -215,19 +264,151 @@ window.onload = function(){
 		</tr>
 		<?php
 	}
-	if ( !empty($cvriskscore) ) {
-		?>
-		<tr>
-			<td>CV Risk Score: <?=$cvriskscore;?></td>
-		</tr>
-		<?php
-	}
-	if ( !empty($cvriskscore_lab) ) {
-		?>
-		<tr>
-			<td>CV Risk Score(LAB): <?=$cvriskscore_lab;?></td>
-		</tr>
-		<?php
-	}	
 	?>
 </table>
+
+<?php 
+
+
+if($_SESSION['smenucode'] == 'ADMEYE')
+{
+	$sql = "SELECT * FROM `pt_opd_eye` WHERE `thdatehn` = '$dthn' ";
+	$q = mysql_query($sql);
+	$item = mysql_fetch_assoc($q);
+
+	if(empty($item['esr_not']))
+	{
+		$item['esr_not'] = '';
+	}
+
+	?>
+	<!-- <div style="page-break-after: always;"></div> -->
+	<div style="line-height: 18.897637795px;">&nbsp;</div>
+	<div class="display-sticker">
+		<div><b>EYE Screening</b></div>
+		<table>
+			<tr>
+				<td>NOT</td>
+				<td>RE <span class="underline"><?=$item['esr_not'];?></span></td>
+				<td>LE <span class="underline"><?=$item['esl_not'];?></span></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>VA</td>
+				<td>RE <span class="underline"><?=$item['esr'];?></span></td>
+				<td>PH <span class="underline"><?=$item['esr_ph'];?></span></td>
+				<td>with glass <span class="underline"><?=$item['esr_glass'];?></span></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>LE <span class="underline"><?=$item['esl'];?></span></td>
+				<td>PH <span class="underline"><?=$item['esl_ph'];?></span></td>
+				<td>with glass <span class="underline"><?=$item['esl_glass'];?></span></td>
+			</tr>
+		</table>
+	</div>
+
+	<?php 
+	if(!empty($item['nurse_dx1']) OR !empty($item['nurse_dx2']) OR !empty($item['nurse_dx3']) OR !empty($item['nurse_dx4']) OR !empty($item['nurse_dx5']))
+	{
+	?>
+	<!-- <div style="page-break-after: always;"></div> -->
+	<div style="line-height: 18.897637795px;">&nbsp;</div>
+	<div class="display-sticker">
+		<div><b>Nursing DX</b></div>
+		<?php 
+		if(!empty($item['nurse_dx1'])){
+			?><div>- <?=$item['nurse_dx1'];?> <span class="underline_notfix"><?=$item['nurse_dx1_txt'];?></span></div><?php
+		}
+		if(!empty($item['nurse_dx2'])){
+			?><div>- <?=$item['nurse_dx2'];?> <span class="underline_notfix"><?=$item['nurse_dx2_txt'];?></span></div><?php
+		}
+		if(!empty($item['nurse_dx3'])){
+			?><div>- <?=$item['nurse_dx3'];?> <span class="underline_notfix"><?=$item['nurse_dx3_txt'];?></span></div><?php
+		}
+		if(!empty($item['nurse_dx4'])){
+			?><div>- <?=$item['nurse_dx4'];?></div><?php
+		}
+		if(!empty($item['nurse_dx5'])){
+			?><div>- <?=$item['nurse_dx5'];?></div><?php
+		}
+		?>
+	</div>
+	<?php 
+	}
+
+	if(!empty($item['imp1']) OR !empty($item['imp2']) OR !empty($item['imp3']) OR !empty($item['imp4']) OR !empty($item['imp5']) OR !empty($item['imp6']))
+	{
+	?>
+	<!-- <div style="page-break-after: always;"></div> -->
+	<div style="line-height: 18.897637795px;">&nbsp;</div>
+	<div class="display-sticker">
+		<div><b>Implementation</b></div>
+		<?php 
+		if(!empty($item['imp1'])){
+			?><div>- <?=$item['imp1'];?></div><?php
+		}
+		if(!empty($item['imp2'])){
+			?><div>- <?=$item['imp2'];?> <span class="underline_notfix"><?=$item['imp2_txt'];?></span></div><?php
+		}
+		if(!empty($item['imp3'])){
+			?><div>- <?=$item['imp3'];?></div><?php
+		}
+		if(!empty($item['imp5'])){
+			?><div>- <?=$item['imp5'];?></div><?php
+		}
+		if(!empty($item['imp6'])){
+			?><div>- <?=$item['imp6'];?> <span class="underline_notfix"><?=$item['imp6_txt'];?></span></div><?php
+		}
+		?>
+	</div>
+	<?php 
+	}
+
+	if(!empty($item['imp1']) OR !empty($item['imp2']) OR !empty($item['imp3']) OR !empty($item['imp4']) OR !empty($item['imp5']) OR !empty($item['imp6']))
+	{
+		?>
+		<!-- <div style="page-break-after: always;"></div> -->
+		<div style="line-height: 18.897637795px;">&nbsp;</div>
+		<div class="display-sticker">
+			<div><b>Evaluation</b></div>
+			<?php 
+			if(!empty($item['eva1'])){
+				?><div>- <?=$item['eva1'];?></div><?php
+			}
+			if(!empty($item['eva2'])){
+				?><div>- <?=$item['eva2'];?></div><?php
+			}
+			if(!empty($item['eva3'])){
+				?><div>- <?=$item['eva3'];?></div><?php
+			}
+			if(!empty($item['eva4'])){
+				?><div>- <?=$item['eva4'];?></div><?php
+			}
+			if(!empty($item['eva5'])){
+				?><div>- <?=$item['eva5'];?></div><?php
+			}
+			if(!empty($item['eva6'])){
+				?><div>- <?=$item['eva6'];?></div><?php
+			}
+			if(!empty($item['eva7'])){
+				?><div>- <?=$item['eva7'];?></div><?php
+			}
+			if(!empty($item['eva8'])){
+				?><div>- <?=$item['eva8'];?></div><?php
+			}
+			if(!empty($item['eva9'])){
+				?><div>- <?=$item['eva9'];?></div><?php
+			}
+			if(!empty($item['eva10'])){
+				?><div>- <?=$item['eva10'];?> <span class="underline_notfix"><?=$item['eva10_txt'];?></span></div><?php
+			}
+			?>
+		</div>
+		<?php
+	}
+	?>
+	<p class="display-sticker">ผู้ป่วยรับทราบ <span class="underline_notfix"><?=$_SESSION['sOfficer'];?></span><b> /RN,PN</b></p>
+	<?php
+}
+?>
