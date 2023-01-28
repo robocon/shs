6ad7c35d59bdd8602889de8e6fbb95b3c272637c
surasak11512 <surasakh12345@gmail.com>
2023-01-28 13:43:36 +0700

    ฟอร์มค้นหา

--- surasak3/authen_report.php
+++ surasak3/authen_report.php
@@ -23,24 +23,36 @@ $dbi->query("SET NAMES UTF8");
 </head>
 
 <body>
-	<div class=".container">
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">HOME</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-					</ul>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">HOME</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<!-- <div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+				</ul>
+			</div> -->
+		</div>
+	</nav>
+	<div class="container">
+		<h3>รายงาน New AuthenCode API</h3>
+		<form>
+			<div class="row mb-3">
+				<label for="inputDate" class="col-sm-1 col-form-label">เลือกวันที่</label>
+				<div class="col-sm-3">
+					<input type="date" class="form-control" id="inputDate" value="2023-01-01">
 				</div>
 			</div>
-		</nav>
+			<div class="col-md-3">
+				<button type="submit" class="btn btn-primary mb-3">ค้นหาข้อมูล</button>
+			</div>
+		</form>
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+	
 </body>
-
 </html>
