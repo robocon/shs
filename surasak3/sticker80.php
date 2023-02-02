3ad144b06416912037b35cb9c1a72f41f0565e96
surasak11512 <surasakh12345@gmail.com>
2023-02-02 20:55:58 +0700

    sticker 80 กับ 50 แบบ minimal

--- surasak3/sticker80.php
+++ surasak3/sticker80.php
@@ -1,58 +1,38 @@
 <?php 
 require_once 'bootstrap.php';
-// exit;
+
 $dbi = new mysqli(HOST, USER, PASS, DB);
 $dbi->query("SET NAMES UTF8");
 
 $hn = sprintf("%s", $_REQUEST['hn']);
-$show = sprintf("%s", $_REQUEST['showvn']);
+$size = sprintf("%s", $_REQUEST['stickersize']);
 $thdateHn = sprintf("%s", $_REQUEST['thdatehn']);
 
 ?>
 <style>
-    body{
-        margin: 0;
-        padding: 0;
-    }
-    p {
-        margin: 0;
-        padding: 0;
-    }
+    *{font-family: "TH SarabunPSK";font-weight: bold;}
+    body{margin: 0;padding: 0;}
+    p {margin: 0;padding: 0;}
 </style>
 <?php
-
-if (empty($show)) {
-    ?>
-    <div style="width: 80mm; height: 50mm;border: 1px solid red; text-align:left;">
-        <div style="float:left; text-align:center;">
-            <img src="printQrCode.php?hn=<?=$hn;?>&size=5&margin=1" alt="">
-            <p><?=$hn;?></p>
-        </div>
-    </div>
-    
-    <?php
-    
-}elseif ($show==='vn') {
-    
-    $sql = "SELECT `thidate`,`hn`,`vn`,`ptname`,`age`,`toborow` FROM `opday` WHERE `thdatehn` = '$thdateHn' ";
-    $q = $dbi->query($sql);
-    $a = $q->fetch_assoc();
-    ?>
-    
-    <div style="width: 80mm; height: 50mm; border: 1px solid red;">
-        <div style="float:left; text-align:center;">
-            <img src="printQrCode.php?hn=<?=$hn;?>&size=5&margin=1" alt="">
-            <p>Digitalcard</p>
-        </div>
-        <div style="margin-left: 4px;">
-            <p>HN: <?=$a['hn'];?></p>
-            <p><?=$a['ptname'];?></p>
-            <p>VN: <?=$a['vn'];?> <?=$a['vn'];?> <?=$a['age'];?></p>
-            <p><?=$a['thidate'];?></p>
-            <p><?=$a['toborow'];?></p>
-        </div>
-    </div>
-    
-    <?php
+$thdateHn = date('d-m-').(date('Y')+543).$hn;
+$sql = "SELECT `thidate`,`hn`,`vn`,`ptname`,`age`,`toborow`,`ptright` FROM `opday` WHERE `thdatehn` = '$thdateHn' ";
+$q = $dbi->query($sql);
+$a = $q->fetch_assoc();
+if (empty($size) OR $size==80) { 
+    $urlSize = 'size=5';
+    $width = '80mm';
+    $height = '50mm';
+}elseif ($size==30) {
+    $urlSize = 'size=3';
+    $width = '50mm';
+    $height = '30mm';
 }
 ?>
+<div style="width: <?=$width;?>; height: <?=$height;?>; text-align:left;">
+    <div style="float:left; text-align:center;">
+        <img src="printQrCode.php?hn=<?=$hn;?>&<?=$urlSize;?>&margin=1" alt="">
+        <p><?=$hn;?></p>
+        <p><?=$a['ptname'];?></p>
+    </div>
+</div>
