62fdf1f32545ab73f673a2df6bf4b8ebbaf11e6a
kritsanasak kuntaros <roboconk@gmail.com>
2023-04-18 15:19:33 +0700

    ฟอร์ม fresh wound ให้แพทย์บันทึก

---
+++ surasak3/er_form_fresh_wound.php
@@ -0,0 +1,100 @@
+<style>
+    *{
+        font-family: "TH Sarabun New","TH SarabunPSK";
+        font-size: 18px;
+    }
+    p, ol{
+        margin:0;
+        padding:0;
+    }
+    li{
+        margin-left: 16px;
+    }
+    input[type=checkbox]:hover{
+        cursor: pointer;
+    }
+</style>
+<div>
+    <h3>แบบฟอร์มคัดกรองบาดแผลสด : Fresh Traumatic Wound (FTW)*</h3>
+    <p>*FTW = บาดแผลสดจาก อุบัติเหตุที่เกิด<u>ภายใน 6 ชั่วโมง</u>ก่อนได้รับการรักษา</p>
+</div>
+<div>
+    <h3><u>กรุณาเลือกลักษณะอาการของผู้ป่วยด้านล่างนี้ โดยลักษณะอาการด้านล่างเป็นลักษณะอาการของผู้ป่วย Fresh Traumatic Wound ที่สมเหตุสมผลที่จะได้รับยาปฏิชีวนะ</u></h3>
+</div>
+<div>
+    <table border="1">
+        <tr>
+            <td>ลักษณะของบาดแผล</td>
+            <td>การประเมิน</td>
+            <td></td>
+        </tr>
+        <tr style="background-color: #d1d1d1;">
+            <td>บาดแผลที่มีโอกาสติดเชื้อและควรใช้ยาปฏิชีวนะ</td>
+            <td></td>
+            <td align="center">ยาปฏิชีวนะที่แนะนำ</td>
+        </tr>
+        <tr>
+            <td>1. แผลขอบไม่เรียบ เย็บแผลให้ขอบชนกันได้ไม่สนิท</td>
+            <td align="center"><input type="checkbox" name="ftw" id=""></td>
+            <td rowspan="5">
+                <p>1<sup>st</sup>-line : Dicloxacillin 250-500 mg QID ขณะท้องว่าง</p>
+                <p><u>กรณีแพ้ Penicillin</u></p>
+                <ol>
+                    <li><b>Clindamycin</b> 300mg TID</li>
+                    <li><b>Erythromycin dry sry.</b> 20-50 mg/kg/day แบ่งให้3-4ครั้งขณะท้องว่าง</li>
+                    <li><b>Roxithromycin</b> 150mg BID</li>
+                </ol>
+            </td>
+        </tr>
+        <tr>
+            <td>2.แผลลึกถึงกล้ามเนื้อ เอ็น หรือกระดูก</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>3.แผลยาวกว่า 5 ซม.</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>4.แผลจากการบดอัด (เช่น โดนประตูหนีบอย่างแรง)</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>5.ผู้ป่วยภูมิต้านทานต่ำ (เช่น อายุ >65ปี เบาหวาน ตับแข็ง โรคพิษสุราเรื้อรัง หลอดเลือดส่วนปลายตีบ มะเร็ง ได้รับยากดภูมิ)</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr style="background-color: #d1d1d1;">
+            <td>บาดแผลที่มีสิ่งปนเปื้อนและควรได้ยาปฏิชีวนะ</td>
+            <td></td>
+            <td align="center">ยาปฏิชีวนะที่แนะนำ พิจารณาให้ tetanus toxoid ร่วมด้วย</td>
+        </tr>
+        <tr>
+            <td>1.แผลสัตว์กัด/คนกัด (อาจให้ยาปฏิชีวนะนาน 3-5วัน และพิจารณาใช้ rabies vaccine, rabies immunoglobulin ร่วมด้วย)</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+            <td rowspan="4">
+                <p>1<sup>st</sup>-line : <b>Amoxycillin-Clavulanic acid</b></p>
+                <p>เด็ก = Amox-Clav โดยให้ Amoxy 20-50 mg/kg/day แบ่งให้ 3ครั้ง<br>(MAX: Amoxy 250mg/dose)</p>
+                <p>ผู้ใหญ่ = 625 mg BID</p>
+                <p><u>กรณีแพ้ Penicillin</u></p>
+                <p><b>Ciprofloxacin</b> 500 mg BID + <b>Clindamycin</b> 300 mg TID</p>
+                <p><b>Ciprofloxacin</b> 500 mg BID + <b>Metronidazole</b> 400-500 mg TID</p>
+            </td>
+        </tr>
+        <tr>
+            <td>2.มีเนื้อตายบริเวณกว้าง</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>3.มีสิ่งสกปรกติดอยู่ในแผลล้างไม่ออก</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>4.ปนเปื้อนสิ่งที่มีแบคทีเรียมาก (เช่น อจจาระ น้ำสกปรก)</td>
+            <td align="center"><input type="checkbox" name="" id=""></td>
+        </tr>
+    </table>
+</div>
+<div>
+    <p><input type="checkbox" name="" id=""> <b><u>ไม่เข้าเกณฑ์การได้รับยาปฏิชีวนะ</u></b></p>
+    <p>คณะอนุกรรมการส่งเสริมการใช้ยาอย่างสมเหตุผล</p>
+    <p>Reference: แนวทางการใช้ยาปฏิชีวนะอย่างสมเหตุผล. 2554 [ออนไลน์]: http://newsser.fda.moph.go.th/rumthai/userfiledownload/asu173dl.pdf</p>
+</div>
