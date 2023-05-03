1e8d8b564372369df089fc14cdb5f321992ea3f0
kritsanasak <roboconk@gmail.com>
2023-05-03 11:31:39 +0700

    fix

--- surasak3/ha_menu.php
+++ surasak3/ha_menu.php
@@ -35,6 +35,6 @@
     <ul>
         <li><a href="ha_index.php">หน้าหลัก</a></li>
         <li><a href="ha_main.php">จัดการตัวชี้วัด</a></li>
-        <li><a href="#">รายงาน</a></li>
+        <li><a href="ha_report.php">รายงาน</a></li>
     </ul>
 </div>
