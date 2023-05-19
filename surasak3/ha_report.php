fe30757635d2dfb7abd771ce73fec7fe2b0e792c
kritsanasak kuntaros <roboconk@gmail.com>
2023-05-19 14:44:57 +0700

    ปรับการแสดงผลตัวชี้วัด รายเดือน รายปี

--- surasak3/ha_report.php
+++ surasak3/ha_report.php
@@ -18,8 +18,8 @@ $dbi->query("SET NAMES UTF8");
 include_once 'ha_menu.php';
 $page = sprintf("%s", $_REQUEST['page']);
 
-$month_start = sprintf("%s", empty($_POST['month_start']) ? date('m') : $_POST['month_start'] );
-$month_end = sprintf("%s", empty($_POST['month_end']) ? date('m') : $_POST['month_end'] );
+$month_start = sprintf("%s", empty($_POST['month_start']) ? '' : $_POST['month_start'] );
+$month_end = sprintf("%s", empty($_POST['month_end']) ? '' : $_POST['month_end'] );
 
 $year_start = sprintf("%s", empty($_POST['year_start']) ? date('Y') : $_POST['year_start'] );
 $year_end = sprintf("%s", empty($_POST['year_end']) ? date('Y') : $_POST['year_end'] );
@@ -131,79 +131,175 @@ if ($page==='search') {
     /**
      * เดี๋ยวต้องปรับให้ค้นหาแค่ ปี / ปี+เดือน ได้
      */
-
-    
-    
-    $qm = $dbi->query("SELECT * FROM `indicator_main` WHERE `id` = '$main_id'");
-    $main = $qm->fetch_assoc();
-
-    // $year_txt='';
-    // if(!empty($year)){
-    //     $year_txt='ปี'.($year+543);
-    // }
-
-    // $month_txt='';
-    // if(!empty($month)){
-    //     $month_txt='เดือน'.$def_fullm_th[$month];
-    // }
-
-    $where_month = " AND `month` = '$month_start' ";
-    if($month_start != $month_end){
-        $where_month = " AND ( `month`>= '$month_start' AND `month`<='$month_end' ) ";
-    }
-
-    $where_year = " AND `year` = '$year_start' ";
-    if($year_start != $year_end){
-        $where_year = " AND ( `year`>= '$year_start' AND `year`<='$year_end' ) ";
-    }
-
-    ?>
-    <div>
-        <h1><?=$main['name'];?> <?=$month_txt;?> <?=$year_txt;?></h1>
-        <?php 
-        $sql = "SELECT a.`main_id`,a.`field_id`,a.`value`,a.`year`,a.`month`, b.`name` 
-        FROM ( 
-            SELECT * FROM `indicator_data` WHERE `main_id` = '$main_id' $where_year $where_month  
-        ) AS a RIGHT JOIN ( 
-            SELECT * FROM `indicator_field` WHERE `main_id` = '$main_id' 
-        ) AS b ON a.`field_id` = b.`id` 
-        WHERE b.`status` = 'y' 
-        ORDER BY a.`year`,a.`month`,a.`field_id` ASC ";
-
-        dump($sql);
-
-        $q=$dbi->query($sql);
-        if ($q->num_rows>0) {
+    if(empty($main_id)){
+        
         ?>
-        <table class="chk_table">
-            <tr>
-                <th>#</th>
-                <th>ตัวชี้วัด</th>
-                <th></th>
-            </tr>
-        <?php 
-        $i=1;
-        while ($a=$q->fetch_assoc()) {
-            ?>
-            <tr>
-                <td><?=$i;?></td>
-                <td><?=$a['name'];?></td>
-                <td align="right"><span style="padding: 0 6px;"><?=$a['value'];?></span></td>
-            </tr>
-            <?php
-            $i++;
-        }
-        ?>
-        </table>
+        <div>
+            <p><b>กรุณาเลือกตัวชี้วัดที่ต้องการ</b></p>
+        </div>
         <?php
-        }else{
-            ?>
-            <p>ยังไม่พบการลงข้อมูล <?=$main['name'];?></p>
-            <?php
+        
+    }else{
+
+        $qm = $dbi->query("SELECT * FROM `indicator_main` WHERE `id` = '$main_id'");
+        $main = $qm->fetch_assoc();
+
+        $q_field = $dbi->query("SELECT `id`,`name` FROM `indicator_field` WHERE `main_id` = '$main_id' AND `status` = 'y' ");
+        $field_items = array();
+        while ($f = $q_field->fetch_assoc()) {
+            $fid = $f['id'];
+            $field_items[$fid] = $f['name'];
         }
-    ?>
-    </div>
-    <?php
+
+        $range_year = range($year_start, $year_end);
+        $range_month = range($month_start, $month_end);
+
+        $where = "";
+        if(count($range_year) == 1){ 
+
+            $where .= " AND `year` = '$year_start' ";
+            if($month_start != $month_end){
+                $where .= " AND ( `month`>= '$month_start' AND `month`<='$month_end' ) ";
+
+            }else{
+                $where .= " AND `month` = '$month_start' ";
+
+            }
+
+        }elseif ($range_year > 1) { 
+
+            if(empty($month_end)){
+
+                $where .= " AND `month` = '' AND ( `year` >= '$year_start' AND `year` <= '$year_end' ) ";
+
+            }else{
+                $where .= " AND ( 
+                    ( `year` >= '$year_start' AND `month` >= '$month_start' ) 
+                    AND 
+                    ( `year` <= '$year_end' AND `month` <= '$month_end' ) 
+                ) ";
+            }
+            
+        }
+        
+        ?>
+        <div>
+            <h1><?=$main['name'];?> <?=$month_txt;?> <?=$year_txt;?></h1>
+            <?php 
+            $sql = "SELECT a.`main_id`,a.`field_id`,a.`value`,a.`year`,a.`month`, b.`name` 
+            FROM ( 
+                SELECT * FROM `indicator_data` WHERE `main_id` = '$main_id' $where  
+            ) AS a RIGHT JOIN ( 
+                SELECT * FROM `indicator_field` WHERE `main_id` = '$main_id' 
+            ) AS b ON a.`field_id` = b.`id` 
+            WHERE b.`status` = 'y' 
+            ORDER BY a.`year`,a.`month`,a.`field_id` ASC ";
+            $q=$dbi->query($sql);
+            if ($q->num_rows>0) {
+
+                if(count($range_year) == 1){ 
+
+                    $data_items = array();
+                    while ($a=$q->fetch_assoc()) {
+                        $field_id = $a['field_id'];
+                        $m = $a['month'];
+
+                        $data_items[$field_id][$m] = array(
+                            'value' => $a['value'],
+                            'name' => $a['name']
+                        );
+                    }
+
+                    ?>
+                    <table class="chk_table">
+                        <tr>
+                            <th>ตัวชี้วัด</th>
+                            <?php 
+                            foreach ($range_month as $key => $value) {
+                                $m = sprintf("%02d", $value);
+                                ?>
+                                <th><?=$def_fullm_th[$m];?></th>
+                                <?php
+                            }
+                            ?>
+                        </tr>
+                        <?php 
+                        foreach ($field_items as $key => $value) {
+                            ?>
+                            <tr>
+                                <td><?=$value;?></td>
+                                <?php 
+                                foreach ($range_month as $dkey => $dvalue) {
+                                    $m = sprintf("%02d", $dvalue);
+
+                                    ?>
+                                    <td><?=$data_items[$key][$m]['value'];?></td>
+                                    <?php
+                                }
+                                ?>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                    </table>
+                    <?php
+
+                }elseif ($range_year > 1) { 
+
+                    $data_items = array();
+                    while ($a=$q->fetch_assoc()) {
+                        $year = $a['year'];
+                        $field_id = $a['field_id'];
+                        $data_items[$year][$field_id] = array(
+                            'value' => $a['value'],
+                            'title' => $a['name']
+                        );
+                    }
+
+                    ?>
+                    <table class="chk_table">
+                        <tr>
+                            <th>ตัวชี้วัด</th>
+                            <?php 
+                            foreach ($data_items as $key => $value) {
+                                ?>
+                                <th><?=$key;?></th>
+                                <?php
+                            }
+                            ?>
+                        </tr>
+                        <?php 
+                        foreach ($field_items as $key => $v) {
+                            ?>
+                            <tr>
+                                <td><?=$v['value'];?></td>
+                                <?php 
+                                foreach ($data_items as $fkey => $fv) {
+                                    $real_value = $fv[$key]['value'];
+                                    ?>
+                                    <td><?=$real_value;?></td>
+                                    <?php
+                                }
+                                ?>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                    <?php 
+
+                }
+
+
+
+            }else{
+                ?>
+                <p>ยังไม่พบการลงข้อมูล <?=$main['name'];?></p>
+                <?php
+            }
+        ?>
+        </div>
+        <?php
+    }
 }
 ?>
 </div>
