8f5e547cf86df9bc208ef77f88a317cf318a3a3a
kritsanasak kuntaros <roboconk@gmail.com>
2023-06-01 11:01:36 +0700

    เพิ่มการหมุนรูปให้คูมหมอ

--- surasak3/dt_paperLessFullPage.php
+++ surasak3/dt_paperLessFullPage.php
@@ -16,9 +16,53 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec( $ch );
 curl_close($ch);
 ?>
-<img src="<?=$file;?>" width="100%">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+	#rotateImageBtn{
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	#rotateImageBtn:hover{
+		cursor: pointer;
+	}
+	.button {
+		padding: 8px 8px;
+		font-size: 24px;
+		text-align: center;
+		cursor: pointer;
+		outline: none;
+		color: #fff;
+		background-color: #04AA6D;
+		border: none;
+		border-radius: 8px;
+		box-shadow: 0 9px #999;
+	}
 
-<script language="JavaScript">
+	.button:hover {background-color: #3e8e41}
+
+	.button:active {
+		background-color: #3e8e41;
+		box-shadow: 0 5px #666;
+		transform: translateY(4px);
+	}
+</style>
+
+<div style="position:relative;">
+	<img src="<?=$file;?>" id="showImage" width="100%">
+	<div id="rotateImageBtn" class="button">Rotate <i class="fa fa-rotate-right" style="font-size:24px"></i></div>
+</div>
+
+<script type="text/javascript">
+	let rotation = 0;
+	const angle = 90;
+	document.getElementById('rotateImageBtn').onclick = function(){
+		var img = document.getElementById('showImage');
+		rotation = (rotation + angle) % 360;
+		img.style.transform = 'rotate('+rotation+'deg)';
+	}
+  
+
 var isNS = (navigator.appName == "Netscape") ? 1 : 0;
  
 if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
