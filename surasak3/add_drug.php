5e3655d9ca9d657cee8463f4ae811595abbc2b45
kritsanasak kuntaros <roboconk@gmail.com>
2023-08-21 10:01:39 +0700

    just update

--- surasak3/add_drug.php
+++ surasak3/add_drug.php
@@ -24,7 +24,12 @@ if($num > 0){
 $sql = "Select an, hn, ptname, bedcode, ptright, doctor From bed where an = '$my_an' limit 0,1 ";
 $result = Mysql_Query($sql);
 $bed = Mysql_fetch_assoc($result);
-// Mysql_free_result($result);
+if(!$bed){
+	?>
+	<h1 style="color:red;">คำเตือน ไม่พบข้อมูล HN: <?=$my_an;?> กรุณาตรวจสอบ AN อีกครั้ง</h1>
+	<?php
+}
+// $bed['hn'] = '47-18782';
 
 // session_register("hn_now");
 $my_hn = $_SESSION["hn_now"] = $bed["hn"];
@@ -35,6 +40,7 @@ $_SESSION["ptright_now"] = $bed["ptright"];
 // รายการยาที่แพ้ทั้งหมดของคนไข้
 $drugreact_list = array();
 $drugreact_list_js = array();
+dump("SELECT * FROM `drugreact` WHERE hn = '$my_hn' GROUP BY drugcode");
 $res_drugreact = $dbi->query("SELECT * FROM `drugreact` WHERE hn = '$my_hn' GROUP BY drugcode");
 $rowdg1 = $res_drugreact->num_rows;
 if($rowdg1 > 0){
