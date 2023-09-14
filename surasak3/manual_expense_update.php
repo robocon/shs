ed0236495ff2a2cce4978d09328d42ef32f1eb5c
kritsanasak kuntaros <roboconk@gmail.com>
2023-09-14 16:26:31 +0700

    นักเลงพอโว้ย

--- surasak3/manual_expense_update.php
+++ surasak3/manual_expense_update.php
@@ -15,6 +15,43 @@ dump($depart_id);
 dump($new_lab);
 dump($vn);
 
+$labItems = explode(',', $new_lab);
+
+
 $dep = new ClassDepart();
 $item = $dep->getDepartFromId($depart_id);
 dump($item);
+$hn = $item['hn'];
+
+foreach ($labItems AS $key => $labCode) { 
+    $sqlLabcare = sprintf("SELECT `code`,`oldcode`,`detail`,`price`,`yprice`,`nprice`,`depart`,`part` FROM `labcare` WHERE `code` = '%s' ", $labCode);
+    $q = $dbi->query($sqlLabcare);
+    if ($q->num_rows > 0) { 
+        $lab = $q->fetch_assoc();
+        $code = $lab['code'];
+        $detail = $lab['detail'];
+        $price = $lab['price'];
+        $nprice = $lab['nprice'];
+        $yprice = $lab['yprice'];
+        $depart = $lab['depart'];
+        $part = $lab['part'];
+
+        // $sql = "UPDATE patdata SET ";
+        dump($code);
+        $sql_patdata = "SELECT row_id FROM patdata WHERE date LIKE '2566-09-13%' AND hn = '$hn' AND code = '$code' AND depart = 'PATHO'";
+        $qPat = $dbi->query($sql_patdata);
+        if($qPat->num_rows>0){
+            $pat = $qPat->fetch_assoc();
+            
+            $patdata_id = $pat['row_id'];
+            $sql_pat_update = "UPDATE patdata SET 
+            price = '$price',
+            yprice = '$yprice',
+            nprice = '',
+            paid = '$price' 
+            WHERE row_id = '$patdata_id' ";
+            dump($sql_pat_update);
+        }
+        
+    }
+}
