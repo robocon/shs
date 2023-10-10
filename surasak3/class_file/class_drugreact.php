fe8f3f3884db81d7554d5f1399c3eb8954dffca8
kritsanasak <roboconk@gmail.com>
2023-10-10 09:02:46 +0700

    ปรับ getDrugreactFromHn ให้เลือกตาม field where group ได้ เพิ่ม แสดงรายการยาตามกลุ่มที่แพ้ยา

--- surasak3/class_file/class_drugreact.php
+++ surasak3/class_file/class_drugreact.php
@@ -7,13 +7,27 @@ class Drugreact extends DbConnect{
         parent::__construct();
     }
 
-    public function getDrugreactFromHn($hn=null){
+    /**
+     * @param string $hn        เลขที่ HN ของผู้มารับบริการ
+     * @param string $fields    Default เป็น * คือการเลือกทุกฟิลด์ แต่ถ้าต้องการเลือกเฉพาะฟิลด์ให้ใส่มาเป็น Array
+     * @param string $where     คำสั่ง where เงื่อนไขเพิ่มเติม
+     * @param string $group     คำสั่ง group by xxx กรณีที่ต้องการ group ตามกลุ่ม
+     */
+    public function getDrugreactFromHn($hn=null, $fields=array(), $where=null, $group=null){
         if (empty($hn)) {
             return "HN is Required";
         }
 
         $hn = sprintf("%s", $hn);
-        $q = $this->dbi->query("SELECT * FROM drugreact WHERE hn = '$hn' ");
+        $where = sprintf("%s", $where);
+        $group = sprintf("%s", $group);
+
+        $field = '*';
+        if(!empty($fields)){
+            $field = implode(',', $fields);
+        }
+
+        $q = $this->dbi->query("SELECT $field FROM drugreact WHERE hn = '$hn' $where $group");
         if ($q->num_rows>0) {
             $items = array();
             while ($a = $q->fetch_assoc()) {
@@ -25,6 +39,50 @@ class Drugreact extends DbConnect{
 
         }
         
+        return $res;
+    }
+
+    public function getGroupNameFromHn($hn=null){
+        if(empty($hn)){
+            return "HN is Required";
+        }
+        $q = $this->dbi->query("SELECT groupname FROM drugreact WHERE hn = '$hn' AND groupname <> '' GROUP BY groupname ");
+        if ($q->num_rows>0) {
+            $items = array();
+            while ($a = $q->fetch_assoc()) {
+                
+                $items[] = $a;
+            }
+            $res = $items;
+            
+        }else{
+            $res = array("error"=>"400", "message"=>"Data not found ".$this->dbi->error);
+        }
+        return $res;
+    }
+
+    public function getDrugreactInGroupRelation($hn=null){
+
+        if (empty($hn)) {
+            return "HN is Required";
+        }
+
+        $sql="SELECT b.* FROM ( 
+            SELECT `groupname` FROM `drugreact` WHERE `hn` = '$hn' AND `groupname` != '' GROUP BY `groupname`
+        ) AS a 
+        LEFT JOIN `drugreact_group` AS c ON c.`name` = a.`groupname`
+        LEFT JOIN `drugreact_group_list` AS b ON c.`id` = b.`drugreact_group` 
+        WHERE b.drugcode NOT IN (SELECT `drugcode` FROM `drugreact` WHERE `hn` = '$hn' AND drugcode != '' GROUP BY drugcode)";
+        $q = $this->dbi->query($sql);
+        if($q->num_rows>0){ 
+            $items = array();
+            while ($a = $q->fetch_assoc()) {
+                $items[] = $a;
+            }
+            $res = $items;
+        }else{
+            $res = array("error"=>"400", "message"=>"Data not found ".$this->dbi->error);
+        }
         return $res;
     }
 }
--- surasak3/test_drugreact.php
+++ surasak3/test_drugreact.php
@@ -3,8 +3,17 @@ require_once 'bootstrap.php';
 require_once 'class_file/class_drugreact.php';
 
 $hn = '47-4661';
-$hn = '47-1';
+// $hn = '47-1';
 
 $d = new Drugreact();
-$res = $d->getDrugreactFromHn($hn);
+// $res = $d->getDrugreactFromHn($hn);
+
+// $res = $d->getGroupNameFromHn($hn);
+
+// $fields = array('groupname');
+// $where = "AND groupname <> ''";
+// $group = 'GROUP BY groupname';
+// $res = $d->getDrugreactFromHn($hn, $fields, $where, $group);
+
+$res = $d->getDrugreactInGroupRelation($hn);
 var_dump($res);
