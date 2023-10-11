058e2ba05830b3564f7ab62f62d0ca5ccceca5c6
kritsanasak kuntaros <roboconk@gmail.com>
2023-10-11 09:39:27 +0700

    แยก folder เทสเคส

--- surasak3/test_appoint.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'bootstrap.php';
-require_once dirname(__FILE__).'/class_file/class_appoint.php';
-
-$app = new Appoint();
-$date = '2566-10-10';
-// $date = '';
-$res = $app->getDisAppoint($date);
-dump($res);
