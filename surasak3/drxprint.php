6507d62fc1850afb640aca0d8a27d1d2fd3e26ef
kritsanasak kuntaros <roboconk@gmail.com>
2023-10-16 15:11:22 +0700

    ปรับเงื่อนไขในหน้าใบสั่งยาผู้ป่วยนอกต้องมี drugcode tradname หรือ genname และ advreact ไม่เป็นค่าว่าง

--- surasak3/drxprint.php
+++ surasak3/drxprint.php
@@ -162,7 +162,10 @@ if(!empty($cStkcutdate)) {
 	}
 	
 	$num1='0';
-	$query = "SELECT genname,tradname,advreact,asses,sideeffects FROM drugreact WHERE  hn = '$rxHn' and ( drugcode!='' and advreact!='')";
+	$query = "SELECT genname,tradname,advreact,asses,sideeffects 
+	FROM drugreact 
+	WHERE  hn = '$rxHn' 
+	and ( drugcode !='' or tradname!='' or genname!='') and advreact !='' ";
 	$result = mysql_query($query) or die("Query drugreact failed");
 
 	if(mysql_num_rows($result)){
