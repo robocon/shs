0bf9cf5c1a9da3928f41b581a952cd59a1b1a9b1
kritsanasak kuntaros <roboconk@gmail.com>
2023-12-28 12:29:08 +0700

    เพิ่ม class สำหรับ xray แล้วแยก opday กับ opcard จากตัวเดิมออกมาเป็นไฟล์ใหม่

---
+++ surasak3/class_file/class_opcard.php
@@ -0,0 +1,65 @@
+<?php
+require_once dirname(__FILE__).'/database.php';
+class Opcard extends DbConnect
+{
+    public $dbi = null;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param string $hn HN ผู้มารับบริการ
+     * @param array $fields ชื่อฟิลด์ที่ต้องการ select
+     * 
+     * @return array $item รายการตามที่ $fields ได้เลือกไว้หรือทั้งหมด
+     */
+    public function getByHn($hn=null, $fields=null)
+    {
+        $field = '*';
+        if(!empty($fields)){
+            $field = implode(',', $fields);
+        }
+        $query = sprintf("SELECT $field FROM `opcard` WHERE `hn`='%s'", $this->dbi->escape_string($hn));
+        $result = $this->dbi->query($query);
+        $item = false;
+        if($result->num_rows > 0){
+            $item = $result->fetch_assoc();
+        }
+        return $item;
+    }
+
+    public function update($hn, $items=array()){ 
+
+        if(count($items) > 1){
+            $update = array();
+            foreach ($items as $key => $value) {
+                $update[] = sprintf("`$key` = '%s'", $value);
+            }
+            $update_txt = implode(', ', $update);
+        }elseif (count($items)===1) { 
+            $key = key($items);
+            $value = $items[$key];
+            $update_txt = sprintf("`$key` = '%s'", $value);
+        }
+        
+
+        $sql = sprintf("UPDATE `opcard` SET ".$update_txt." WHERE `hn` = '%s'", $hn);
+        $this->dbi->query($sql);
+
+        return $hn;
+    }
+
+    public function getAge($dbirth){
+        list($y, $m, $d) = explode('-', $dbirth);
+        $date2 = date('Y-m-d');
+        $diff = abs(strtotime($date2) - strtotime(($y-543)."-$m-$d"));
+        $years = floor($diff / (365*60*60*24));
+        return $years;
+    }
+
+    public function dbirthThaiToEng($dbirth){
+        list($y, $m, $d) = explode('-', $dbirth);
+        return ($y-543).'-'.$m.'-'.$d;
+    }
+}
---
+++ surasak3/class_file/class_xray.php
@@ -0,0 +1,142 @@
+<?php
+require_once dirname(__FILE__).'/database.php';
+require_once dirname(__FILE__).'/class_opday.php';
+// require_once dirname(__FILE__).'/class_opcard.php';
+
+class Xray extends DbConnect
+{ 
+    public $dbi = null;
+    public $doctor = 'MD022 (ไม่ทราบแพทย์)';
+    public $typeDiag = 'ตรวจสุขภาพ';
+    // public $thaiDateFull = (date('Y')+543).date('-m-d H:i:s');
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getThaiDateFull(){
+        return (date('Y')+543).date('-m-d H:i:s');
+    }
+
+    public function mapXrayList($i, $a){
+        $key = sprintf("%s", $i)+1;
+        $value = sprintf("%s", $a);
+        return "$key.$value";
+    }
+    
+    /**
+     * 
+     */
+    public function addXrayOnlyItem($hn=null, $stanceList=array()){ 
+
+        if(empty($hn) OR empty($stanceList)){
+            return "HN and Xray item is required";
+            exit;
+        }
+        
+        $opdayClass = new Opday();
+        $opday = $opdayClass->getThisDay($hn);
+        if($opday===false){
+            return "Can not find VN";
+            exit;
+        }
+
+        $opcardClass = new Opcard();
+        $opcard = $opcardClass->getByHn($hn);
+        $thaiDateFull = $this->getThaiDateFull();
+        $runno = $this->getXrayRunno();
+        $xray_no = $runno++;
+
+        $preSQL = array_map(array($this, 'mapXrayList'), array_keys($stanceList), array_values($stanceList));
+        $detailAll = implode(' ', $preSQL);
+
+        $data = array(
+            'date' => $thaiDateFull,
+            'hn' => $hn,
+            'vn' => $opday['vn'],
+            'yot' => $opcard['yot'],
+            'name' => $opcard['name'],
+            'sname' => $opcard['surname'],
+            'detail' => $detailAll,
+            'doctor' => $this->doctor,
+            'xrayno' => $xray_no,
+            'type_diag' => $this->typeDiag,
+            'detail_all' => $detailAll,
+            'dbirth' => $opcard['dbirth'],
+        );
+        $resInsertXrayDoctor = $this->insertXrayDoctor($data);
+        dump($resInsertXrayDoctor);
+
+        // $sql_xray_detail = "INSERT INTO `xray_doctor_detail` (
+        //     `date` ,`hn` ,`xrayno` ,`doctor_detail`,`detail_all`
+        // )VALUES (
+        //     '$this->thaiDateFull','$this->hn','$xray_no','1. CHEST CHECK UP','1. CHEST CHECK UP'
+        // );";
+
+        // $sql_xray_stat = "INSERT INTO `xray_stat` (
+        //     `date` ,`hn` ,`xn` ,`xn_new` ,`ptname` ,`age` ,
+        //     `ptright` ,`patient_from` ,`detail` ,`doctor` ,`digital` ,`10_12` ,
+        //     `14_14` ,`NONE` ,`office` ,`idno`,`remark` 
+        // )VALUES ( 
+        //     '$this->thaiDateFull', '$this->hn', '', '', '$ptname', '$age', 
+        //     '$ptright', 'OPD', '1.CHEST CHECK UP', 'MD022 (ไม่ทราบแพทย์)', '1', '0', 
+        //     '0', '0', '$this->sOfficer', '$depart_id', '$sumPrice'
+        // );";
+
+    }
+
+    public function getXrayRunno(){
+        $q_runno = $this->dbi->query("SELECT `title`,`prefix`,`runno` FROM `runno` WHERE `title` = 'xrayno'");
+        $runno_row = $q_runno->fetch_assoc();
+        $xray_no = $runno_row['runno'];
+        return $xray_no;
+    }
+
+    public function nextRunno(){
+        // $this->dbi->query("UPDATE `runno` SET `runno` = '$xray_no' WHERE `title`='xrayno'");
+    }
+
+    public function insertXrayDoctor($data = array()){
+        
+        $sqlXrayDoctor = "INSERT INTO `xray_doctor` (
+            `date` ,`hn` ,`vn` ,`yot` ,`name` ,`sname` ,
+            `detail` ,`doctor` ,`status` ,`xrayno` ,`film` ,`type_diag`,
+            `detail_all`,`dbirth`,`orderby`
+        )VALUES (
+            '".$data['date']."', '".$data['hn']."', '".$data['vn']."', '".$data['yot']."', '".$data['name']."', '".$data['sname']."', 
+            '".$data['detail']."', '".$data['doctor']."', 'N', '".$data['xrayno']."', 'digital', '".$data['type_diag']."', 
+            '".$data['detail_all']."', '".$data['dbirth']."', 'XRAY'
+        );";
+
+        if($this->dbi->query($sqlXrayDoctor)===true){
+            $res = true;
+        }else{
+            $res = $this->dbi->error;
+        }
+
+        return $res;
+    }
+
+    public function getXrayItem($code){
+        $code = sprintf("%s", $code);
+        $sql = "SELECT * FROM labcare WHERE depart = 'XRAY' AND part = 'XRAY' and code = '$code' LIMIT 1 ";
+        $q = $this->dbi->query($sql);
+        if($q->num_rows>0){
+            $res = $q->fetch_assoc();
+        }
+        return $res;
+    }
+
+    public function getXrayItems($code){
+        $code = sprintf("%s", $code);
+        $sql = "SELECT * FROM labcare WHERE depart = 'XRAY' AND part = 'XRAY' and code = '$code' ";
+        $q = $this->dbi->query($sql);
+        $res = array();
+        if($q->num_rows>0){
+            while ($a = $q->fetch_assoc()) {
+                $res[] = $a;
+            }
+        }
+        return $res;
+    }
+}
