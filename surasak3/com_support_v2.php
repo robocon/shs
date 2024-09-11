4850dce0253b9c1103f8ff5868140feab1152608
kritsanasak kuntaros <roboconk@gmail.com>
2024-09-11 14:38:19 +0700

    ปรับการเรียกใช้เวลา enddate

--- surasak3/com_support_v2.php
+++ surasak3/com_support_v2.php
@@ -109,6 +109,7 @@ if($page==='load25page'){
             $q = $dbi->query($sql);
             $i = $q->fetch_assoc();
 
+            $is['row'] = $i['row'];
             $is['depart'] = $i['depart'];
             $is['head'] = $i['head'];
             $is['detail'] = strip_tags(html_entity_decode($s['detail']));
@@ -118,6 +119,7 @@ if($page==='load25page'){
             $is['user1'] = $i['user1'];
             $is['p_edit'] = '';
             $is['date'] = $i['date'];
+            $is['dateend'] = $i['dateend'];
             $items[] = $is;
         }
     }
