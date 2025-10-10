33ff2a2c74b46bfdcf9c01ce0e4919a29ad4618f
roboconk <roboconk@gmail.com>
2025-10-10 21:33:41 +0700

    ว่าจะทำให้พี่อิ๋ว

---
+++ surasak3/doctor_lab.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'bootstrap.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TITLE</title>
+    <link rel="icon" href="images/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
+</head>
+<body>
+    <?php
+    /**
+     * @todo
+     * - แสดงรายการแลปภายในวันเท่านั้น
+     * - อำนวยความสะดวกให้พยาบาลในการดูผลแลป
+     * - แยกเป็นชื่อหมอติ๊กได้ว่าจะให้หมออะไรแสดงรายการบ้าง หรือ หน้าห้องตรวจดี กำลังคิดอยู่
+     * 
+     */
+    ?>
+</body>
+</html>
