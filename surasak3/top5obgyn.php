1fa65345e8fc8634cb42899d16aeec840523406e
Kitsanasak Kantharot <roboconk@gmail.com>
2025-11-14 15:27:15 +0700

    เมนู ICD10 กุมาร ปรับ แสดงทุกแพทย์ และปรับให้แยกตามเดือนที่เลือกตามช่วงเวลา

--- surasak3/top5obgyn.php
+++ surasak3/top5obgyn.php
@@ -1,21 +1,26 @@
 <?php
-include 'bootstrap.php';
-
-include 'templates/classic/header.php';
-
-$doctors = array(
-	'87' => 'ขชล รวมทรัพย์', 
-	'20' => 'วรวิทย์ วงษ์มณี',
-	'143' => 'รัตนเกียรติ พงษ์รัตนกุล',
-	'179' => 'สัมมา เลิศธีรกุล',
-	'100' => 'สาริศ เฉลิมพงศ์ชัย'
-
-);
+include dirname(__FILE__).'/bootstrap.php';
+include dirname(__FILE__).'/templates/classic/header.php';
 
 $th_date = !empty($_POST['year_select']) ? trim($_POST['year_select']) : date('Y') + 543 ;
 $doctor_id = isset($_POST['doctor']) ? trim($_POST['doctor']) : false ;
-?>
 
+// 14/11/68 พี่หมีไม่คีย์ใบงานเข้ามา ไม่เปิดให้ใช้งาน
+?>
+<h1>ไม่พบข้อมูล</h1>
+<?php
+exit;
+?>
+<style>
+	h1{
+		font-size: 2em;
+		font-weight: bold;
+	}
+	h2{
+		font-size: 1.5em;
+		font-weight: bold;
+	}
+</style>
 <div class="site-center">
 	<div class="site-body panel">
 		<div class="body">
@@ -35,18 +40,7 @@ $doctor_id = isset($_POST['doctor']) ? trim($_POST['doctor']) : false ;
 				<form action="top5obgyn.php" method="post" class="no-print">
 					<div class="col">
 						<div class="cell">
-							ปี-เดือน <input type="text" name="year_select" value="<?php echo $th_date;?>">
-							<span>* สามารถเลือกการแสดงผลตามเดือนได้ เช่น 2558-12</span>
-						</div>
-					</div>
-					<div class="col">
-						<div class="cell">
-							เลือกแพทย์ <select name="doctor" id="">
-								<?php foreach($doctors as $key => $item){ ?>
-								<?php $selected = ( $key == $doctor_id ) ? 'selected="selected"' : '' ;?>
-								<option value="<?=$key;?>" <?=$selected;?>><?=$item;?></option>
-								<?php } ?>
-							</select>
+							ตั้งแต่ <input type="month" name="year_select" value=""> ถึง <input type="month" name="year_select2" value="">
 						</div>
 					</div>
 					<div class="col">
@@ -62,171 +56,174 @@ $doctor_id = isset($_POST['doctor']) ? trim($_POST['doctor']) : false ;
 
 					$db = Mysql::load();
 
+					if(empty($_POST['year_select']) OR empty($_POST['year_select'])){
+						?><h3>กรุณาเลือกข้อมูลให้ครบถ้วน</h3><?php
+						exit;
+					}
+
 					$sql = "SELECT `name`,`doctorcode` FROM `doctor` WHERE `row_id` = '$doctor_id';";
 					$db->select($sql);
 					$doctor = $db->get_item();
 					
 					// ลบช่องว่างสองช่องให้เหลือช่องเดียว
 					list($md, $name, $surname) = explode(' ', str_replace('  ', ' ', $doctor['name']));
-					
-					$where = "AND a.`doctor` LIKE '%$name%'";
-					if( $name == 'กฤษฎิ์พงษ์' ){
-						$where = "AND ( a.`doctor` LIKE '%$name%' OR a.`doctor` LIKE '%จรรยวรรธน์%' ) ";
+
+					$year_select = ad_to_bc($_POST['year_select'].'-01');
+					$year_select2 = ad_to_bc($_POST['year_select2'].'-'.date('t', $_POST['year_select2']));
+
+					if($year_select > $year_select2){
+						?><h3>มันจะไปเลือกเดือนย้อนหลังได้ยังไง</h3><?php
+						exit;
 					}
 
-					$sql = "SELECT a.`thidate`,a.`diag`,a.`doctor`,CONCAT('in') AS `patient`,COUNT(a.`diag`) AS `diag_row`,a.`icd10`,
-					TIMESTAMPDIFF( 
-						YEAR, 
-						b.`dbirth`, 
-						CONCAT( ( YEAR( NOW() ) + 543 ), DATE_FORMAT( NOW(), '-%m-%d' ) ) 
-					)  AS `age`,
-					SUBSTRING(a.`age`, 1, 2) AS `short_age` 
-					FROM `opday` AS a 
-					LEFT JOIN `opcard` AS b ON b.`hn` = a.`hn` 
-					WHERE a.`thidate` LIKE '$th_date%' 
-					$where 
-					AND ( a.`diag` IS NOT NULL AND a.`diag` NOT LIKE '' )
-					GROUP BY a.`diag`
-					
-					ORDER BY `diag_row` DESC";
-					dump($sql);
-					exit;
+$sql = "SELECT SUBSTRING(a.`thidate`,1,7) AS `date`,a.`thidate`,a.`hn`,a.`diag`,a.`doctor`,COUNT(a.`icd10`) AS `diag_row`,a.`icd10`,b.`dbirth`,a.`age` AS `opday_age`,
+TIMESTAMPDIFF( YEAR, b.`dbirth`, CONCAT( ( YEAR( NOW() ) + 543 ), DATE_FORMAT( NOW(), '-%m-%d' ) ) ) AS `age`,
+SUBSTRING(a.`age`, 1, 2) AS `short_age` 
+FROM `opday` AS a 
+LEFT JOIN `opcard` AS b ON b.`hn` = a.`hn` 
+WHERE ( a.`thidate` >= '$year_select' AND a.`thidate` <= '$year_select2' ) 
+AND a.`an` IS NULL AND a.`doctor` <> '' 
+AND ( a.`icd10` IS NOT NULL AND a.`icd10` != '' )
+GROUP BY SUBSTRING(a.`thidate`,1,7),a.`doctor`, a.`icd10`
+ORDER BY SUBSTRING(a.`thidate`,1,7) ASC, `diag_row` DESC";
+
 					$db->select($sql);
 					$items = $db->get_items();
 					
-					$all_rows = 0;
-					foreach ($items as $key => $item) {
-						if( $doctor_id == 20 && $item['age'] > 15 ){ continue; }
-
-						$all_rows += (int) $item['diag_row'];
+					$newItems = array();
+					foreach ($items as $item) {
+						$key = $item['doctor'];
+						$subKey = $item['date'];
+						$newItems[$key][$subKey][] = $item;
 					}
 					
+					list($yStart, $mStart) = explode('-', $_POST['year_select']);
+					list($yEnd, $mEnd) = explode('-', $_POST['year_select2']);
 					?>
-					<div class="col">
-						<div class="cell">
+					<div style="margin-top: 1em;">&nbsp;</div>
+					<h1>ระยะเวลา ตั้งแต่ <?=$def_fullm_th[$mStart].' '.($yStart+543);?> ถึง <?=$def_fullm_th[$mEnd].' '.($yEnd+543);?></h1>
+					
+					<table>
+						<tr valign="top">
+							<td width="50%" style="vertical-align:top">
+
+					<h1 style="text-align:center;">ผู้ป่วยนอก</h1>
+					<?php 
+					foreach ($newItems as $doctor => $itemsInMonth) {
+						?>
+						<div style="margin-top:1em;">&nbsp;</div>
+						<h2>แพทย์ <?=$doctor;?></h2>
+							<?php
+							foreach ($itemsInMonth as $yearMonth => $items) {
+
+								list($y, $m) = explode('-', $yearMonth);
+								?>
+								<h3><?=$def_fullm_th[$m];?> <?=$y;?></h3>
+								<table>
+									<tr>
+										<th width="5%">#</th>
+										<th width="8%">ICD 10</th>
+										<th>Diag</th>
+										<th width="8%">จำนวน(คน)</th>
+									</tr>
+								<?php
+								$i = 1;
+								foreach ($items as $item) {
+
+									if($item['date']!==$lastMonth){
+										$i = 1;
+									}
+									$lastMonth = $item['date'];
+									// หมอวรวิทย์ จะคัดเอาเฉพาะคนที่อายุไม่เกิน 15ปี
+									// if( $doctor_id == 20 && $item['short_age'] > 15 ){ continue; }
+									?>
+									<tr>
+										<td><?=$i;?></td>
+										<td><?=$item['icd10'];?></td>
+										<td><?=$item['diag'];?></td>
+										<td><?=$item['diag_row'];?></td>
+									</tr>
+									<?php 
+									$i++;
+								} // End each item
+								?>
+								</table>
+								<?php
+							}// End each month
+							?>
+					<?php
+					} // End doctor
+					?>
+
+
+							</td>
+							<td valign="top" style="vertical-align:top">
+					
+					<h1 style="text-align:center;">ผู้ป่วยใน</h1>
+					<?php
+
+					$sql = "SELECT SUBSTRING(a.`date`,1,7) AS `year_month`,a.`date`,a.`icd10`,a.`diag`,a.`doctor`,CONCAT('out') AS `patient`,COUNT(a.`icd10`) AS `diag_row`,a.`icd10`,
+					TIMESTAMPDIFF( YEAR, b.`dbirth`, CONCAT( ( YEAR( NOW() ) + 543 ), DATE_FORMAT( NOW(), '-%m-%d' ) ) )  AS `age`
+					FROM `ipcard` AS a 
+					LEFT JOIN `opcard` AS b ON b.`hn` = a.`hn` 
+					WHERE ( a.`date` >= '$year_select' AND a.`date` <= '$year_select2' ) 
+					AND a.`doctor` <> '' 
+					AND ( a.`icd10` IS NOT NULL AND a.`icd10` != '' )
+					GROUP BY SUBSTRING(a.`date`,1,7),a.`doctor`, a.`icd10`
+					ORDER BY SUBSTRING(a.`date`,1,7) ASC, COUNT(a.`icd10`) DESC";
+					$db->select($sql);
+					$items = $db->get_items();
+					$newItems2 = array();
+					foreach ($items as $item) {
+						$key = $item['doctor'];
+						$subKey = $item['year_month'];
+						$newItems2[$key][$subKey][] = $item;
+					}
+
+
+					foreach ($newItems2 as $doctor => $itemsInMonth) {
+						?>
+						<div style="margin-top:1em;">&nbsp;</div>
+						<h2>แพทย์ <?=$doctor;?></h2>
+						<?php
+						foreach ($itemsInMonth as $yearMonth => $items) {
+							list($y, $m) = explode('-', $yearMonth);
+							?>
+							<h3><?=$def_fullm_th[$m];?> <?=$y;?></h3>
 							<table>
 								<tr>
-									<td style="vertical-align: top!important;">
-										<h3>ผู้ป่วยนอก</h3>
-										<table>
-								<thead>
-									<tr>
-										<th>#</th>
-										<th>ICD 10</th>
-										<th>Diag</th>
-										<th>จำนวน(คน)</th>
-										<th>%</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php
-									$i = 1;
-									foreach ($items as $key => $item) {
-										
-										// หมอวรวิทย์ จะคัดเอาเฉพาะคนที่อายุไม่เกิน 15ปี
-										if( $doctor_id == 20 && $item['short_age'] > 15 ){ continue; }
-									?>
-									<tr>
-										<td><?=$i;?></td>
-										<td><?=$item['icd10'];?></td>
-										<td><?=$item['diag'];?></td>
-										<td><?=$item['diag_row'];?></td>
-										<td>
-											<?php 
-											echo round( ( $item['diag_row'] * 100 ) / $all_rows, 2 );
-											?>
-										</td>
-									</tr>
-									<?php 
-										$i++;
-									}
-									?>
-									<tr>
-										<td colspan="3">รวม</td>
-										<td><?=$all_rows;?></td>
-										<td></td>
-									</tr>
-								</tbody>
-							</table>
-									</td>
-									<td style="vertical-align: top!important;">
-										
-										<?php
-										$sql = "SELECT a.`date`,a.`diag`,a.`doctor`,CONCAT('out') AS `patient`,COUNT(a.`diag`) AS `diag_row`,a.`icd10`,
-										TIMESTAMPDIFF( 
-											YEAR, 
-											b.`dbirth`, 
-											CONCAT( ( YEAR( NOW() ) + 543 ), DATE_FORMAT( NOW(), '-%m-%d' ) ) 
-										)  AS `age`, 
-										SUBSTRING(a.`age`, 1, 2) AS `short_age` 
-										FROM `ipcard` AS a 
-										LEFT JOIN `opcard` AS b ON b.`hn` = a.`hn` 
-										WHERE a.`date` LIKE '$th_date%' 
-										$where 
-										AND ( a.`diag` IS NOT NULL AND a.`diag` NOT LIKE '' )
-										GROUP BY a.`diag`
-										ORDER BY `diag_row` DESC";
-										$db->select($sql);
-										$items = $db->get_items();
-
-										$all_rows = 0;
-										foreach ($items as $key => $item) {
-											if( $doctor_id == 20 && $item['age'] > 15 ){ continue; }
-											
-											$all_rows += (int) $item['diag_row'];
-										}
-										?>
-										
-										
-										
-										<h3>ผู้ป่วยใน</h3>
-										<table>
-								<thead>
-									<tr>
-										<th>#</th>
-										<th>ICD 10</th>
-										<th>Diag</th>
-										<th>จำนวน(คน)</th>
-										<th>%</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php
-									$i = 1;
-									foreach ($items as $key => $item) {
-										
-										// หมอวรวิทย์ จะคัดเอาเฉพาะคนที่อายุไม่เกิน 15ปี
-										if( $doctor_id == 20 && $item['short_age'] > 15 ){ continue; }
-									?>
-									<tr>
-										<td><?=$i;?></td>
-										<td><?=$item['icd10'];?></td>
-										<td><?=$item['diag'];?></td>
-										<td><?=$item['diag_row'];?></td>
-										<td>
-											<?php 
-											echo round( ( $item['diag_row'] * 100 ) / $all_rows, 2 );
-											?>
-										</td>
-									</tr>
-									<?php 
-										$i++;
-									}
-									?>
-									<tr>
-										<td colspan="3">รวม</td>
-										<td><?=$all_rows;?></td>
-										<td></td>
-									</tr>
-								</tbody>
-							</table>
-									</td>
+									<th width="5%">#</th>
+									<th width="8%">ICD 10</th>
+									<th>Diag</th>
+									<th width="8%">จำนวน(คน)</th>
 								</tr>
+								<?php
+								$i = 1;
+								foreach ($items as $key => $item) {
+									?>
+									<tr>
+										<td><?=$i;?></td>
+										<td><?=$item['icd10'];?></td>
+										<td><?=$item['diag'];?></td>
+										<td><?=$item['diag_row'];?></td>
+									</tr>
+									<?php
+									$i++;
+								}
+							?>
 							</table>
-							
-							
-						</div>
-					</div>
+						<?php
+						}
+					}
+					?>
+
+
+
+							</td>
+						</tr>
+					</table>
+					
+					
 					<?php
 				}
 				?>
@@ -235,4 +232,4 @@ $doctor_id = isset($_POST['doctor']) ? trim($_POST['doctor']) : false ;
 	</div>
 </div>
 <?php
-include 'templates/classic/footer.php';
+include dirname(__FILE__).'/templates/classic/footer.php';
