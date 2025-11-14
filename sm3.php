2a7c1f040c0f36becf2993979bd527604f34ed36
Kitsanasak Kantharot <roboconk@gmail.com>
2025-11-14 15:29:03 +0700

    หน้า Landing ปรับ ให้ล้างค่า Session

--- sm3.php
+++ sm3.php
@@ -1,48 +1,40 @@
-<script language="JavaScript1.2">
-<!--
-window.moveTo(0,0);
-if (document.all) {
-top.window.resizeTo(screen.availWidth,screen.availHeight);
-}
-else if (document.layers||document.getElementById) {
-if (top.window.outerHeight<screen.availHeight||top.window.outerWidth<screen.availWidth){
-top.window.outerHeight = screen.availHeight;
-top.window.outerWidth = screen.availWidth;
-}
-}
-//-->
+<?php
+session_start();
+session_destroy();
+include("connect.inc");
 
-<script>
+$_SESSION = array();
+
+setcookie('shsLogin', '', -1, '/');
+setcookie('shsLoginUser', '', -1, '/');
+?>
+<script language="JavaScript1.2">
+  window.moveTo(0, 0);
+  if (document.all) {
+    top.window.resizeTo(screen.availWidth, screen.availHeight);
+  } else if (document.layers || document.getElementById) {
+    if (top.window.outerHeight < screen.availHeight || top.window.outerWidth < screen.availWidth) {
+      top.window.outerHeight = screen.availHeight;
+      top.window.outerWidth = screen.availWidth;
+    }
+  }
   if (top && top.frames["main"]) {
     top.frames["main"].location.reload(); // รีเฟรชเฉพาะ frame 'main'
   }
 </script>
-</script>
-
 <?php
-include("connect.inc");
-$sRowid=$_GET["user_id"];
+$sRowid = $_GET["user_id"];
 mysql_query("UPDATE `inputm` SET `login_status`=0 WHERE (`row_id`='$sRowid');");
-
-    session_start();
-    session_destroy();
-
 ?>
-
-
-
-
 <frameset framespacing="0" border="0" cols="270,*" frameborder="0">
   <frameset rows="120,*">
     <frame name="contents" target="main" src="banner.htm" marginwidth="0" marginheight="0" scrolling="no" noresize>
-    <frame name="contents1" src="surasak3/login.php" marginwidth="0" marginheight="0" scrolling="yes" target="_self" noresize>
+      <frame name="contents1" src="surasak3/login.php" marginwidth="0" marginheight="0" scrolling="yes" target="_self" noresize>
   </frameset>
   <frame name="main" src="display.php" id="mainDisplayPage" marginwidth="0" marginheight="0" scrolling="yes" target="_self">
-  <noframes>
-  <body>
-
-  <p>This page uses frames, but your browser doesn't support them.</p>
-
-  </body>
-  </noframes>
+    <noframes>
+      <body>
+        <p>This page uses frames, but your browser doesn't support them.</p>
+      </body>
+    </noframes>
 </frameset>
