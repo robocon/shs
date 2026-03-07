a14ba5873a0f415d7dc90260a87151493123d851
kritsanasak <roboconk@gmail.com>
2026-03-07 14:43:34 +0700

    จ่ายยาผู้ป่วยใน ปรับ การแจ้งเตือนการเล็อกและการปลดล็อค

--- surasak3/phardc.php
+++ surasak3/phardc.php
@@ -1,27 +1,37 @@
 <?php
-    session_start();
-    if (!isset($sIdname)){die;}
-	include("connect.inc");
-	$date2 = (date("Y")+543).date("-m-d");
-	
-	$str="select lock_dc from ipcard WHERE an = '".$_GET['an']."' ";
-	$strresult=mysql_query($str);
-	$arr=mysql_fetch_array($strresult);
-	
-	if($arr['lock_dc']=='' || $arr['lock_dc']==NULL){
-		
-		$sql = "update ipcard set lock_dc = '$date2' where an = '".$_GET['an']."' ";
-		
-	}else{
-		
-		$sql = "update ipcard set lock_dc =null where an = '".$_GET['an']."' ";
-	}
-	
-	
-	//echo $sql;
-	$result=mysql_query($sql);
-	if($result){
-		echo "ทำการปลดล็อคเรียบร้อยแล้ว กรุณารอสักครู่....";
-		echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"2;URL=enddrugprofile.php\">";
-	}
-?>
+session_start();
+include_once dirname(__FILE__) . '/connect.php';
+include_once dirname(__FILE__) . '/includes/functions.php';
+if (!isset($_SESSION['sIdname'])) {
+	endSession();
+	exit;
+}
+
+$date2 = (date("Y") + 543) . date("-m-d");
+$sql = sprintf("SELECT `lock_dc` FROM `ipcard` WHERE `an` = '%s'", $dbi->real_escape_string($_GET['an']));
+$q = $dbi->query($sql);
+$arr= $q->fetch_assoc($q);
+
+/**
+ * ถ้า lock_dc เป็นค่าว่าง แสดงว่าตอนนี้กำลัง lock
+ */
+if ($arr['lock_dc'] == '' || $arr['lock_dc'] == NULL) {
+	$sql = sprintf("UPDATE `ipcard` SET `lock_dc` = '%s' WHERE `an` = '%s' ", 
+		$dbi->real_escape_string($date), 
+		$dbi->real_escape_string($_GET['an'])
+	);
+	$lockMsg = "ทำการปลดล็อคเรียบร้อยแล้ว กรุณารอสักครู่....";
+
+} else {
+	$sql = sprintf("UPDATE `ipcard` SET `lock_dc` = '' WHERE `an` = '%s' ", 
+		$dbi->real_escape_string($_GET['an'])
+	);
+	$lockMsg = "ทำการล็อคเรียบร้อย กรุณารอสักครู่....";
+
+}
+
+$result = $dbi->query($sql);
+if ($result) {
+	echo $lockMsg;
+	echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"2;URL=enddrugprofile.php\">";
+}
