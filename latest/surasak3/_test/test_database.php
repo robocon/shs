7e847c1fe37606029141dc49ed8969c711ef7951
Kitsanasak Kantharot <roboconk@gmail.com>
2026-04-19 14:07:56 +0700

    remove latest folder

--- latest/surasak3/_test/test_database.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once dirname(__FILE__).'/../newBootstrap.php';
-
-$db = new Database();
-$tbname = '43epi';
-$data = array('HOSPCODE'=>'11512','PID'=>'59-1718');
-$db->insertData($tbname, $data);
